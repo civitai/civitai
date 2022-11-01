5291464b2757c3018d8df66af7cf7775c1c8fded
Justin Maier <Zipp425@gmail.com>
2022-11-01 17:06:31 -0600

    Add training data download

---
+++ src/pages/api/download/training-data/[modelVersionId].ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getGetUrl } from '~/utils/s3-utils';
+import { UserActivityType } from '@prisma/client';
+import { getServerAuthSession } from '~/server/common/get-server-auth-session';
+
+export default async function downloadModel(req: NextApiRequest, res: NextApiResponse) {
+  const modelVersionId = req.query.modelVersionId as string;
+  if (!modelVersionId) {
+    res.status(400).json({ error: 'Missing modelVersionId' });
+    return;
+  }
+
+  const modelVersion = await prisma?.modelVersion.findFirst({
+    where: { id: parseInt(modelVersionId) },
+    select: { model: { select: { id: true, name: true } }, name: true, trainingDataUrl: true },
+  });
+  if (!modelVersion || !modelVersion.trainingDataUrl) {
+    res.status(404).json({ error: 'Model not found' });
+    return;
+  }
+
+  // Track activity
+  const session = await getServerAuthSession({ req, res });
+  const userId = session?.user?.id;
+  await prisma?.userActivity.create({
+    data: {
+      userId,
+      activity: UserActivityType.TrainingDataDownload,
+      details: JSON.stringify({ modelId: modelVersion.model.id, modelVersionId }),
+    },
+  });
+
+  const { url } = await getGetUrl(modelVersion.trainingDataUrl);
+
+  res.redirect(url);
+}
