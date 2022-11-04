e420f6e9de437c2df0157543c9a555fb72818f66
Justin Maier <Zipp425@gmail.com>
2022-11-04 12:04:39 -0600

    fix tag creation invalidation

--- src/components/Model/ModelForm/ModelForm.tsx
+++ src/components/Model/ModelForm/ModelForm.tsx
@@ -44,34 +44,34 @@ export function ModelForm({ model }: Props) {
   const editing = !!model;
   const initialFormData = editing
     ? ({
-        ...model,
-        tagsOnModels: model?.tagsOnModels.map(({ tag }) => tag) ?? [],
-        modelVersions:
-          model?.modelVersions.map((version) => ({
-            ...version,
-            images: version.images.map(({ image }) => image),
-          })) ?? [],
-      } as CreateModelProps)
+      ...model,
+      tagsOnModels: model?.tagsOnModels.map(({ tag }) => tag) ?? [],
+      modelVersions:
+        model?.modelVersions.map((version) => ({
+          ...version,
+          images: version.images.map(({ image }) => image),
+        })) ?? [],
+    } as CreateModelProps)
     : {
-        name: '',
-        description: '',
-        trainedWords: [],
-        type: ModelType.Checkpoint,
-        tagsOnModels: [],
-        nsfw: false,
-        modelVersions: [
-          {
-            name: '',
-            description: '',
-            url: '',
-            epochs: null,
-            steps: null,
-            sizeKB: 0,
-            trainingDataUrl: '',
-            images: [],
-          },
-        ],
-      };
+      name: '',
+      description: '',
+      trainedWords: [],
+      type: ModelType.Checkpoint,
+      tagsOnModels: [],
+      nsfw: false,
+      modelVersions: [
+        {
+          name: '',
+          description: '',
+          url: '',
+          epochs: null,
+          steps: null,
+          sizeKB: 0,
+          trainingDataUrl: '',
+          images: [],
+        },
+      ],
+    };
   const form = useForm<CreateModelProps>({
     validate: zodResolver(modelSchema.passthrough()),
     initialValues: initialFormData,
@@ -95,6 +95,7 @@ export function ModelForm({ model }: Props) {
           icon: <IconCheck size={18} />,
         });
         queryUtils.model.invalidate();
+        queryUtils.tag.invalidate();
         router.push(`/models/${response.id}`);
       },
       onError(error: TRPCClientErrorBase<DefaultErrorShape>) {
