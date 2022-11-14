da1a1e493544b9750e1129e37f90504ecca03aa7
Justin Maier <Zipp425@gmail.com>
2022-11-13 21:40:07 -0700

    Add generalized other downloads

---
+++ src/pages/api/download/[...key].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getGetUrl } from '~/utils/s3-utils';
+import { getServerAuthSession } from '~/server/common/get-server-auth-session';
+
+export default async function downloadTrainingData(req: NextApiRequest, res: NextApiResponse) {
+  const keyParts = req.query.key as string[];
+  const key = keyParts.join('/');
+  console.log({ key });
+  if (!key) return res.status(400).json({ error: 'Missing key' });
+
+  const session = await getServerAuthSession({ req, res });
+  const userId = session?.user?.id;
+  if (!userId) {
+    if (req.headers['content-type'] === 'application/json')
+      return res.status(401).json({ error: 'Unauthorized' });
+    else return res.redirect(`/login?returnUrl=/api/download/${key}`);
+  }
+
+  // Track activity
+  // TODO Tracking: Add a new activity type for downloading other keys...
+
+  const { url } = await getGetUrl(key);
+
+  res.redirect(url);
+}
