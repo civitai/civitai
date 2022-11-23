fae40f3ecc2a6d44c18c9654adbaceaeb9528c8d
Briant Diehl <bkdiehl@gmail.com>
2022-11-23 11:44:33 -0700

    fix defaults on schema

--- src/server/schema/model.schema.ts
+++ src/server/schema/model.schema.ts
@@ -6,23 +6,25 @@ import { modelVersionUpsertSchema } from '~/server/schema/model-version.schema';
 import { tagSchema } from '~/server/schema/tag.schema';
 import { sanitizedStringSchema } from '~/server/schema/utils.schema';
 
-export const getAllModelsSchema = z
-  .object({
-    limit: z.number().min(1).max(200),
-    cursor: z.number(),
-    page: z.number(),
-    query: z.string(),
-    tag: z.string(),
-    username: z.string(),
-    types: z
-      .union([z.nativeEnum(ModelType), z.nativeEnum(ModelType).array()])
-      .optional()
-      .transform((rel) => (!rel ? undefined : Array.isArray(rel) ? rel : [rel])),
-    sort: z.nativeEnum(ModelSort).default(ModelSort.HighestRated),
-    period: z.nativeEnum(MetricTimeframe).default(MetricTimeframe.AllTime),
-    rating: z.preprocess((val) => Number(val), z.number()).transform((val) => Math.floor(val)),
-  })
-  .partial();
+export const getAllModelsSchema = z.object({
+  limit: z.number().min(1).max(200).optional(),
+  cursor: z.number().optional(),
+  page: z.number().optional(),
+  query: z.string().optional(),
+  tag: z.string().optional(),
+  username: z.string().optional(),
+  types: z
+    .union([z.nativeEnum(ModelType), z.nativeEnum(ModelType).array()])
+    .optional()
+    .transform((rel) => (!rel ? undefined : Array.isArray(rel) ? rel : [rel]))
+    .optional(),
+  sort: z.nativeEnum(ModelSort).default(ModelSort.HighestRated),
+  period: z.nativeEnum(MetricTimeframe).default(MetricTimeframe.AllTime),
+  rating: z
+    .preprocess((val) => Number(val), z.number())
+    .transform((val) => Math.floor(val))
+    .optional(),
+});
 
 export type GetAllModelsInput = z.input<typeof getAllModelsSchema>;
 export type GetAllModelsOutput = z.infer<typeof getAllModelsSchema>;
