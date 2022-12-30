c3d9f9b8186c93eddeb9cd5bd210bea651935cad
Briant Diehl <bkdiehl@gmail.com>
2022-12-29 17:24:40 -0700

    add todo comments

--- src/server/selectors/commentv2.selector.ts
+++ src/server/selectors/commentv2.selector.ts
@@ -19,6 +19,8 @@ export const commentV2Select = Prisma.validator<Prisma.CommentV2Select>()({
   // },
 });
 
+//TODO - come up with a better way of prefetching data and communicating the limits of that prefetched data to the client component
+// When I  prefetch relational messages and `take` a number of messages, the client Comments component needs to know the `take` number so that it knows when to display a show more message
 export const getRelationalComments = () => {
   return;
 };
