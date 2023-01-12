d80b8227b8163fd9cbe23c235621cc5de0d6f617
Justin Maier <Zipp425@gmail.com>
2023-01-11 17:15:37 -0700

    Add image unique constraint

--- prisma/migrations/20230111235844_images_on_reviews_one_to_many/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[imageId]` on the table `ImagesOnReviews` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ImagesOnReviews_imageId_key" ON "ImagesOnReviews"("imageId");
