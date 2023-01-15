ca03ad562f909ab86bdc9bb40fa1a94c1e03a21d
Briant Diehl <bkdiehl@gmail.com>
2023-01-15 13:36:34 -0700

    merge schemas

--- src/server/schema/model-version.schema.ts
+++ src/server/schema/model-version.schema.ts
@@ -1,9 +1,27 @@
 import { z } from 'zod';
 import { constants } from '~/server/common/constants';
+import { v4 as uuidv4 } from 'uuid';
 
 import { imageSchema } from '~/server/schema/image.schema';
 import { modelFileSchema } from '~/server/schema/model-file.schema';
 import { getSanitizedStringSchema } from '~/server/schema/utils.schema';
+
+export type RecipeModelInput = z.infer<typeof recipeModelSchema>;
+export const recipeModelSchema = z.object({
+  id: z.union([z.number(), z.string()]).optional(),
+  name: z.string(),
+  type: z.enum(['version', 'result', 'unknown']),
+});
+
+export type RecipeInput = z.infer<typeof recipeSchema>;
+export const recipeSchema = z.object({
+  id: z.string().default(uuidv4()),
+  type: z.enum(['sum', 'diff']),
+  modelA: recipeModelSchema,
+  modelB: recipeModelSchema,
+  modelC: recipeModelSchema.optional(),
+  multiplier: z.number(),
+});
 
 export const modelVersionUpsertSchema = z.object({
   id: z.number().optional(),
@@ -20,4 +38,5 @@ export const modelVersionUpsertSchema = z.object({
     .max(20, 'You can only upload up to 20 images'),
   trainedWords: z.array(z.string()),
   files: z.array(modelFileSchema),
+  recipe: z.array(recipeSchema).optional(),
 });
