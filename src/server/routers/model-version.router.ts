712e832628b94acd50f152d4f26998b9eb62394e
Manuel E. Urena <manuel.ureh@hotmail.com>
2023-01-19 17:19:02 -0400

    Adds middleware with feature flag protection

--- src/server/routers/model-version.router.ts
+++ src/server/routers/model-version.router.ts
@@ -1,13 +1,24 @@
+import { TRPCError } from '@trpc/server';
+import { SessionUser } from 'next-auth';
 import {
   getModelVersionRunStrategiesHandler,
   toggleNotifyEarlyAccessHandler,
 } from '~/server/controllers/model-version.controller';
 import { getByIdSchema } from '~/server/schema/base.schema';
-import { protectedProcedure, publicProcedure, router } from '~/server/trpc';
+import { getFeatureFlags } from '~/server/services/feature-flags.service';
+import { middleware, protectedProcedure, publicProcedure, router } from '~/server/trpc';
+
+const isFlagProtected = middleware(({ ctx, next }) => {
+  const { earlyAccessModel } = getFeatureFlags({ user: ctx.user });
+  if (!earlyAccessModel) throw new TRPCError({ code: 'FORBIDDEN' });
+
+  return next({ ctx: { user: ctx.user as SessionUser } });
+});
 
 export const modelVersionRouter = router({
   getRunStrategies: publicProcedure.input(getByIdSchema).query(getModelVersionRunStrategiesHandler),
   toggleNotifyEarlyAccess: protectedProcedure
     .input(getByIdSchema)
+    .use(isFlagProtected)
     .mutation(toggleNotifyEarlyAccessHandler),
 });
