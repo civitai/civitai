d9189e4c12b2e9d9c0faa159b07fd5dd53742a05
Briant Diehl <bkdiehl@gmail.com>
2023-01-27 11:10:44 -0700

    quick push to gallery index

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,6 +1,12 @@
 import { Container, Center, Paper, Title } from '@mantine/core';
+import { useGalleryFilters } from '~/components/Gallery/GalleryFilters';
+import { trpc } from '~/utils/trpc';
 
 export default function Gallery() {
+  const filters = useGalleryFilters();
+
+  const { data, isLoading } = trpc.image.getGalleryImagesInfinite.useInfiniteQuery(filters);
+
   return (
     <Container>
       <Center py="xl">
