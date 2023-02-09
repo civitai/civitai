7d0e323fdbc33f24f2d754dfa9492fe15d9f6c70
Justin Maier <Zipp425@gmail.com>
2023-02-09 13:54:24 -0700

    Fix trpc url

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -26,7 +26,7 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `/api/trpc`,
           maxURLLength: 2083,
         }),
       ],
