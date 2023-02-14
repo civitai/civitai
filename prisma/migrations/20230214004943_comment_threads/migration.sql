fca0346fe3a210c130de68581e016798d3cefb19
Briant Diehl <bkdiehl@gmail.com>
2023-02-13 17:55:06 -0700

    add justin todo

--- prisma/migrations/20230214004943_comment_threads/migration.sql
+++ prisma/migrations/20230214004943_comment_threads/migration.sql
@@ -1,39 +1,49 @@
 /*
-  Warnings:
-
-  - You are about to drop the column `locked` on the `CommentV2` table. All the data in the column will be lost.
-  - You are about to drop the column `parentId` on the `CommentV2` table. All the data in the column will be lost.
-  - You are about to drop the `AnswerComment` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `ImageComment` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `QuestionComment` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `threadId` to the `CommentV2` table without a default value. This is not possible if the table is not empty.
-
-*/
+ Warnings:
+ 
+ TODO.Justin - resolve warnings
+ - You are about to drop the column `locked` on the `CommentV2` table. All the data in the column will be lost.
+ - You are about to drop the column `parentId` on the `CommentV2` table. All the data in the column will be lost.
+ - You are about to drop the `AnswerComment` table. If the table is not empty, all the data it contains will be lost.
+ - You are about to drop the `ImageComment` table. If the table is not empty, all the data it contains will be lost.
+ - You are about to drop the `QuestionComment` table. If the table is not empty, all the data it contains will be lost.
+ - Added the required column `threadId` to the `CommentV2` table without a default value. This is not possible if the table is not empty.
+ 
+ */
 -- DropForeignKey
-ALTER TABLE "AnswerComment" DROP CONSTRAINT "AnswerComment_answerId_fkey";
-
--- DropForeignKey
-ALTER TABLE "AnswerComment" DROP CONSTRAINT "AnswerComment_commentId_fkey";
-
--- DropForeignKey
-ALTER TABLE "CommentV2" DROP CONSTRAINT "CommentV2_parentId_fkey";
+ALTER TABLE
+  "AnswerComment" DROP CONSTRAINT "AnswerComment_answerId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "ImageComment" DROP CONSTRAINT "ImageComment_commentId_fkey";
+ALTER TABLE
+  "AnswerComment" DROP CONSTRAINT "AnswerComment_commentId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "ImageComment" DROP CONSTRAINT "ImageComment_imageId_fkey";
+ALTER TABLE
+  "CommentV2" DROP CONSTRAINT "CommentV2_parentId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "QuestionComment" DROP CONSTRAINT "QuestionComment_commentId_fkey";
+ALTER TABLE
+  "ImageComment" DROP CONSTRAINT "ImageComment_commentId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "QuestionComment" DROP CONSTRAINT "QuestionComment_questionId_fkey";
+ALTER TABLE
+  "ImageComment" DROP CONSTRAINT "ImageComment_imageId_fkey";
+
+-- DropForeignKey
+ALTER TABLE
+  "QuestionComment" DROP CONSTRAINT "QuestionComment_commentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE
+  "QuestionComment" DROP CONSTRAINT "QuestionComment_questionId_fkey";
 
 -- AlterTable
-ALTER TABLE "CommentV2" DROP COLUMN "locked",
-DROP COLUMN "parentId",
-ADD COLUMN     "threadId" INTEGER NOT NULL;
+ALTER TABLE
+  "CommentV2" DROP COLUMN "locked",
+  DROP COLUMN "parentId",
+ADD
+  COLUMN "threadId" INTEGER NOT NULL;
 
 -- DropTable
 DROP TABLE "AnswerComment";
@@ -46,13 +56,12 @@ DROP TABLE "QuestionComment";
 
 -- CreateTable
 CREATE TABLE "Thread" (
-    "id" SERIAL NOT NULL,
-    "locked" BOOLEAN NOT NULL DEFAULT false,
-    "questionId" INTEGER,
-    "answerId" INTEGER,
-    "imageId" INTEGER,
-
-    CONSTRAINT "Thread_pkey" PRIMARY KEY ("id")
+  "id" SERIAL NOT NULL,
+  "locked" BOOLEAN NOT NULL DEFAULT false,
+  "questionId" INTEGER,
+  "answerId" INTEGER,
+  "imageId" INTEGER,
+  CONSTRAINT "Thread_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -65,13 +74,31 @@ CREATE UNIQUE INDEX "Thread_answerId_key" ON "Thread"("answerId");
 CREATE UNIQUE INDEX "Thread_imageId_key" ON "Thread"("imageId");
 
 -- AddForeignKey
-ALTER TABLE "CommentV2" ADD CONSTRAINT "CommentV2_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  "CommentV2"
+ADD
+  CONSTRAINT "CommentV2_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE
+  "Thread"
+ADD
+  CONSTRAINT "Thread_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE
+SET
+  NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "Answer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE
+  "Thread"
+ADD
+  CONSTRAINT "Thread_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "Answer"("id") ON DELETE
+SET
+  NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "Image"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE
+  "Thread"
+ADD
+  CONSTRAINT "Thread_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "Image"("id") ON DELETE
+SET
+  NULL ON UPDATE CASCADE;
