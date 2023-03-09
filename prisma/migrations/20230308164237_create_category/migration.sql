bec15be2f3aa1bac9b0855cf51016ae35a20821b
Héctor Echiburú <black.shot@live.cl>
2023-03-08 22:58:44 -0300

    remove unnecesary migrations

--- prisma/migrations/20230308164237_create_category/migration.sql
+++ prisma/migrations/20230308164237_create_category/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "CategoryType" AS ENUM ('Models', 'Characters', 'Places', 'Concepts', 'Clothings', 'Styles', 'Poses', 'QualityEnhancements', 'Others');
+CREATE TYPE "CategoryType" AS ENUM ('RealisticModels', 'SemiRealisticModels', 'AnimeModels', 'Models', 'Characters', 'Places', 'Concepts', 'Clothings', 'Styles', 'Poses', 'QualityEnhancements', 'Others');
 
 -- AlterTable
 ALTER TABLE "Model" ADD COLUMN     "category" "CategoryType" NOT NULL DEFAULT 'Models';
