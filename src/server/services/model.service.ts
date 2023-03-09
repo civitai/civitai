0e2b5aab9fccfaea0244286c97f8b7e476904af1
Justin Maier <Zipp425@gmail.com>
2023-03-08 21:40:10 -0700

    Fix type issue causing db validation issues

--- src/server/services/model.service.ts
+++ src/server/services/model.service.ts
@@ -438,8 +438,8 @@ export const updateModel = async ({
               image: {
                 create: {
                   ...prepareCreateImage(image),
-                  user: { connect: { id: userId } },
-                },
+                  userId,
+                } as Prisma.ImageUncheckedCreateWithoutImagesOnReviewsInput,
               },
             })),
           },
@@ -505,8 +505,8 @@ export const updateModel = async ({
                   image: {
                     create: {
                       ...prepareCreateImage(image),
-                      user: { connect: { id: userId } },
-                    },
+                      userId,
+                    } as Prisma.ImageUncheckedCreateWithoutImagesOnReviewsInput,
                   },
                 })),
                 update: imagesToUpdate.map(({ index, ...image }) => ({
