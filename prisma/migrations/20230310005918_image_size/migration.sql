152838d2638008bb23be086aea5cef1dde5a0100
Briant Diehl <bkdiehl@gmail.com>
2023-03-09 18:11:34 -0700

    record image mimeType and size on upload

---
+++ prisma/migrations/20230310005918_image_size/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Image" ADD COLUMN     "sizeKB" INTEGER;
--- src/server/schema/image.schema.ts
+++ src/server/schema/image.schema.ts
@@ -57,8 +57,8 @@ export const imageSchema = z.object({
   analysis: imageAnalysisSchema.optional(),
   tags: z.array(tagSchema).optional(),
   needsReview: z.boolean().optional(),
-  mimeTye: z.string().optional(),
-  size: z.string().optional(),
+  mimeType: z.string().optional(),
+  sizeKB: z.number().optional(),
 });
 
 export type ImageUploadProps = z.infer<typeof imageSchema>;
