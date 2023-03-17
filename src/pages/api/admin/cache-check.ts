3a203f54769611ebfcc36a07ffb7877fabc4517e
Justin Maier <Zipp425@gmail.com>
2023-03-16 21:15:51 -0600

    Add cache check handling

---
+++ src/pages/api/admin/cache-check.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
+import { redis } from '~/server/redis/client';
+import { WebhookEndpoint } from '~/server/utils/endpoint-helpers';
+import { invalidateSession } from '~/server/utils/session-helpers';
+import { zc } from '~/utils/schema-helpers';
+
+const schema = z.object({
+  userId: zc.numberString,
+  reset: zc.booleanString.optional(),
+});
+
+export default WebhookEndpoint(async (req: NextApiRequest, res: NextApiResponse) => {
+  const { userId, reset } = schema.parse(req.query);
+  if (reset) await invalidateSession(userId);
+  const sessionCache = await redis.get(`session:${userId}`);
+
+  return res.status(200).json({
+    sessionCache,
+    reset,
+  });
+});
