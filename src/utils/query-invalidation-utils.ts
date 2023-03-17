974c3c09ba739f21fedcb2cffa17e0c9b45fa9c4
Justin Maier <Zipp425@gmail.com>
2023-03-16 20:48:59 -0600

    Adjust moderation invalidations to cover posts

--- src/utils/query-invalidation-utils.ts
+++ src/utils/query-invalidation-utils.ts
@@ -1,11 +1,8 @@
 import { trpc } from '~/utils/trpc';
 
 export async function invalidateModeratedContent(queryUtils: ReturnType<typeof trpc.useContext>) {
-  await queryUtils.model.getAll.invalidate();
-  await queryUtils.image.getGalleryImages.invalidate();
-  await queryUtils.image.getGalleryImageDetail.invalidate();
-  await queryUtils.image.getGalleryImagesInfinite.invalidate();
-  await queryUtils.tag.getAll.invalidate();
-  await queryUtils.tag.getTrending.invalidate();
-  // TODO Briant: Invalidate post queries
+  await queryUtils.model.invalidate();
+  await queryUtils.tag.invalidate();
+  await queryUtils.post.invalidate();
+  await queryUtils.image.invalidate();
 }
