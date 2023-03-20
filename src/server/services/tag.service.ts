e2c2f92acf8976a6a7625e88655341dca36cbfbc
Justin Maier <Zipp425@gmail.com>
2023-03-20 09:41:05 -0600

    Improve tag count query

--- src/server/services/tag.service.ts
+++ src/server/services/tag.service.ts
@@ -5,20 +5,24 @@ import { dbWrite, dbRead } from '~/server/db/client';
 import { GetTagsInput } from '~/server/schema/tag.schema';
 
 export const getTagWithModelCount = async ({ name }: { name: string }) => {
-  return await dbRead.tag.findFirst({
-    where: {
-      name: { equals: name, mode: 'insensitive' },
-    },
-    select: {
-      id: true,
-      name: true,
-      _count: {
-        select: {
-          tagsOnModels: { where: { model: { status: ModelStatus.Published } } },
-        },
-      },
-    },
-  });
+  return await dbRead.$queryRaw<{ id: number; name: string; count: number }>`
+    SELECT "public"."Tag"."id",
+    "public"."Tag"."name",
+    (
+      SELECT COUNT(*) AS "_aggr_count_tagsOnModels"
+      FROM "public"."TagsOnModels"
+      WHERE ("public"."TagsOnModels"."modelId", "public"."TagsOnModels"."tagId") IN (
+        SELECT "t0"."modelId", "t0"."tagId"
+        FROM "public"."TagsOnModels" AS "t0" INNER JOIN "public"."Model" AS "j0" ON ("j0"."id") = ("t0"."modelId")
+        WHERE "j0"."status" = 'Published'
+          AND "t0"."modelId" IS NOT NULL
+          AND "t0"."tagId" = "public"."Tag"."id"
+      )
+    ) as count
+    FROM "public"."Tag"
+    WHERE "public"."Tag"."name" ILIKE ${name}
+    LIMIT 1 OFFSET 0
+  `;
 };
 
 export const getTags = async <TSelect extends Prisma.TagSelect = Prisma.TagSelect>({
