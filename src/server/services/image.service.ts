76814e3d6edaf74b7084ab1c529992833632fe64
Briant Diehl <bkdiehl@gmail.com>
2023-03-21 14:42:41 -0600

    quick fix to image ordering when querying images on post

--- src/server/services/image.service.ts
+++ src/server/services/image.service.ts
@@ -432,7 +432,8 @@ export const getAllImages = async ({
   generation,
 }: GetInfiniteImagesInput & { userId?: number }) => {
   const AND: Prisma.Enumerable<Prisma.ImageWhereInput> = [];
-  if (postId) AND.push({ postId });
+  const orderBy: Prisma.Enumerable<Prisma.ImageOrderByWithRelationInput> = [];
+
   if (modelId || modelVersionId)
     AND.push({ resourceHelper: { some: { modelVersionId, modelId } } });
   if (username) AND.push({ user: { username: { equals: username, mode: 'insensitive' } } });
@@ -450,10 +451,10 @@ export const getAllImages = async ({
   }
   if (!!tags?.length) AND.push({ tags: { some: { tagId: { in: tags } } } });
   if (!!generation?.length) AND.push({ generationProcess: { in: generation } });
-
-  const orderBy: Prisma.Enumerable<Prisma.ImageOrderByWithRelationInput> = [];
-  if (postId) orderBy.push({ index: 'asc' });
-  else {
+  if (postId) {
+    AND.push({ postId });
+    orderBy.push({ index: 'asc' });
+  } else {
     if (sort === ImageSort.MostComments)
       orderBy.push({ rank: { [`commentCount${period}Rank`]: 'asc' } });
     else if (sort === ImageSort.MostReactions)
