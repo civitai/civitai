46434651e527a31a3a60d0b12961b6b39bb4390e
Justin Maier <Zipp425@gmail.com>
2023-03-22 17:10:11 -0600

    Migration of reviews, comments, resources, and posts

--- prisma/migrations/20230322180123_discussion_item/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Thread" ADD COLUMN     "modelId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_modelId_fkey" FOREIGN KEY ("modelId") REFERENCES "Model"("id") ON DELETE SET NULL ON UPDATE CASCADE;
