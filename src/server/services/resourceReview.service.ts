00758bb3dcc7eb3c9da38ac828e38d6036bcf13e
Justin Maier <Zipp425@gmail.com>
2023-03-23 23:31:35 -0600

    Parameterize some queries

--- src/server/services/resourceReview.service.ts
+++ src/server/services/resourceReview.service.ts
@@ -74,14 +74,14 @@ export const getResourceReviewsInfinite = async ({
 
 export type RatingTotalsModel = { '1': number; '2': number; '3': number; '4': number; '5': number };
 export const getRatingTotals = async ({ modelId }: GetRatingTotalsInput) => {
-  const result = await dbRead.$queryRawUnsafe<{ rating: number; count: number }[]>(`
+  const result = await dbRead.$queryRaw<{ rating: number; count: number }[]>`
     SELECT
       rr.rating,
       COUNT(*) count
     FROM "ResourceReview" rr
     WHERE rr."modelId" = ${modelId}
     GROUP BY rr.rating
-  `);
+  `;
 
   const transformed = result.reduce(
     (acc, { rating, count }) => {
