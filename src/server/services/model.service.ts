917fde7fbb8443fdb83b119111c065c923ae0a5a
Justin Maier <Zipp425@gmail.com>
2023-03-28 14:24:38 -0600

    Add filter by commercial use in API

--- src/server/services/model.service.ts
+++ src/server/services/model.service.ts
@@ -1,4 +1,11 @@
-import { MetricTimeframe, ModelStatus, ModelType, Prisma, TagTarget } from '@prisma/client';
+import {
+  CommercialUse,
+  MetricTimeframe,
+  ModelStatus,
+  ModelType,
+  Prisma,
+  TagTarget,
+} from '@prisma/client';
 import { TRPCError } from '@trpc/server';
 import isEqual from 'lodash/isEqual';
 import { SessionUser } from 'next-auth';
@@ -61,6 +68,10 @@ export const getModels = async <TSelect extends Prisma.ModelSelect>({
     excludedIds,
     checkpointType,
     status,
+    allowNoCredit,
+    allowDifferentLicense,
+    allowDerivatives,
+    allowCommercialUse,
   },
   select,
   user: sessionUser,
@@ -90,6 +101,27 @@ export const getModels = async <TSelect extends Prisma.ModelSelect>({
   if (sessionUser?.isModerator && !(username || user)) {
     AND.push({ status: status && status.length > 0 ? { in: status } : ModelStatus.Published });
   }
+
+  // Filter by model permissions
+  if (allowCommercialUse !== undefined) {
+    const commercialUseOr: CommercialUse[] = [];
+    switch (allowCommercialUse) {
+      case CommercialUse.None:
+        commercialUseOr.push(CommercialUse.None);
+        break;
+      case CommercialUse.Image:
+        commercialUseOr.push(CommercialUse.Image);
+      case CommercialUse.Rent:
+        commercialUseOr.push(CommercialUse.Rent);
+      case CommercialUse.Sell:
+        commercialUseOr.push(CommercialUse.Sell);
+    }
+    AND.push({ allowCommercialUse: { in: commercialUseOr } });
+  }
+  if (allowDerivatives !== undefined) AND.push({ allowDerivatives });
+  if (allowDifferentLicense !== undefined) AND.push({ allowDifferentLicense });
+  if (allowNoCredit !== undefined) AND.push({ allowNoCredit });
+
   if (query) {
     AND.push({
       OR: [
