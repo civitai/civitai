c74b14ddc9d297b847a705438bb1af6a77271123
Justin Maier <Zipp425@gmail.com>
2023-03-30 16:16:13 -0600

    Adjust permissions on user images and posts pages

--- src/pages/user/[username]/images.tsx
+++ src/pages/user/[username]/images.tsx
@@ -6,12 +6,15 @@ import { PeriodFilter, SortFilter } from '~/components/Filters';
 import { ImageCategories } from '~/components/Image/Infinite/ImageCategories';
 import { useCurrentUser } from '~/hooks/useCurrentUser';
 import { BrowsingMode } from '~/server/common/enums';
+import { NotFound } from '~/components/AppLayout/NotFound';
 
 export default function UserImages() {
   const router = useRouter();
   const username = router.query.username as string;
   const currentUser = useCurrentUser();
   const browsingMode = currentUser?.username === username ? BrowsingMode.All : undefined;
+
+  if (!currentUser?.isModerator && username !== currentUser?.username) return <NotFound />;
 
   return (
     <Container fluid style={{ maxWidth: 2500 }}>
