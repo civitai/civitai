a64e74d4426c3484b21a1e12323ad1707832eae6
Briant Diehl <bkdiehl@gmail.com>
2023-04-04 15:16:18 -0600

    added support for post slug

--- src/components/Post/Detail/PostDetail.tsx
+++ src/components/Post/Detail/PostDetail.tsx
@@ -9,9 +9,15 @@ import { PostImages } from '~/components/Post/Detail/PostImages';
 import Router from 'next/router';
 import { useEffect } from 'react';
 import { QS } from '~/utils/qs';
+import { Meta } from '~/components/Meta/Meta';
+import { truncate } from 'lodash-es';
+import { removeTags } from '~/utils/string-helpers';
+import { getEdgeUrl } from '~/client-utils/cf-images-utils';
 
 export function PostDetail({ postId }: { postId: number }) {
   const { data: post, isLoading: postLoading } = trpc.post.get.useQuery({ id: postId });
+  const { data: { items: images } = { items: [] }, isLoading: imagesLoading } =
+    trpc.image.getInfinite.useQuery({ postId });
 
   // when a user navigates back in their browser, set the previous url with the query string post={postId}
   useEffect(() => {
@@ -38,35 +44,42 @@ export function PostDetail({ postId }: { postId: number }) {
   if (!post) return <NotFound />;
 
   return (
-    <Container size="sm">
-      <Stack>
-        <Group position="apart" noWrap align="top">
-          {post.title ? (
-            <Title sx={{ lineHeight: 1 }} order={2}>
-              {post.title}
-            </Title>
-          ) : (
-            <span></span>
-          )}
-          <PostControls postId={post.id} userId={post.user.id} />
-        </Group>
+    <>
+      <Meta
+        title={post.title ?? `Image post by ${post.user.username}`}
+        description={truncate(removeTags(post.detail ?? ''), { length: 150 })}
+        image={images[0]?.url == null ? undefined : getEdgeUrl(images[0].url, { width: 1200 })}
+      />
+      <Container size="sm">
+        <Stack>
+          <Group position="apart" noWrap align="top">
+            {post.title ? (
+              <Title sx={{ lineHeight: 1 }} order={2}>
+                {post.title}
+              </Title>
+            ) : (
+              <span></span>
+            )}
+            <PostControls postId={post.id} userId={post.user.id} />
+          </Group>
 
-        <PostImages postId={postId} />
-        <Stack spacing="xl">
-          {!!post.tags.length && (
-            <Group spacing="xs">
-              {post.tags.map((tag) => (
-                <Badge key={tag.id} size="lg">
-                  {tag.name}
-                </Badge>
-              ))}
-            </Group>
-          )}
-          {post.detail && <RenderHtml html={post.detail} withMentions />}
-          <a id="comments" />
-          <PostComments postId={postId} userId={post.user.id} />
+          <PostImages postId={post.id} images={images} isLoading={imagesLoading} />
+          <Stack spacing="xl">
+            {!!post.tags.length && (
+              <Group spacing="xs">
+                {post.tags.map((tag) => (
+                  <Badge key={tag.id} size="lg">
+                    {tag.name}
+                  </Badge>
+                ))}
+              </Group>
+            )}
+            {post.detail && <RenderHtml html={post.detail} withMentions />}
+            <a id="comments" />
+            <PostComments postId={postId} userId={post.user.id} />
+          </Stack>
         </Stack>
-      </Stack>
-    </Container>
+      </Container>
+    </>
   );
 }
