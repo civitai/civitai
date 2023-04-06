ad95b6f922a1b655ad73567cdc13d6e1610ae82b
Koen <koen@linker.io>
2023-04-06 23:55:48 +0100

    added metrics page

---
+++ src/pages/api/metrics.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse } from "next";
+import client from "prom-client";
+import { WebhookEndpoint } from '~/server/utils/endpoint-helpers';
+
+client.collectDefaultMetrics({
+  prefix: "civitai_node_",
+  register: client.register
+});
+
+const handler = WebhookEndpoint(async (_, res: NextApiResponse) => {
+  const metrics = await client.register.metrics();
+
+  res.setHeader("Content-type", client.register.contentType);
+  res.send(metrics);
+});
+
+export default handler;
