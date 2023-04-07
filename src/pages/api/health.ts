493c30f5be20cc341c4a5c5297c5b1293118b355
Koen <koen@linker.io>
2023-04-07 22:36:56 +0100

    Updated healthcheck

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -10,15 +10,22 @@ const handler = WebhookEndpoint(async (req: NextApiRequest, res: NextApiResponse
     where: { id: -1 },
     data: { username: 'civitai' },
     select: { id: true },
-  });
-  const readDbCheck = await dbRead.user.findUnique({ where: { id: 1 }, select: { id: true } });
+  }).then(x => x !== null, _ => false);
+  const readDbCheck = await dbRead.user.findUnique({ where: { id: 1 }, select: { id: true } }).then(x => x !== null, _ => false);
+  
+  // redis and session fail silenty (no exception)
   const redisCheck = await redis.get('user:-1:hidden-tags');
   const session = await getServerSession(req, res, createAuthOptions(req as NextApiRequest));
 
-  return res.status(200).json({
-    healthy: true,
-    writeDb: writeDbCheck !== null,
-    readDb: readDbCheck !== null,
+  // as we're forwarding the request to authenticate with, we may not always have a server session
+  // for that reason, do NOT include the server session as a factor in determining valid status
+  const healthy = writeDbCheck && readDbCheck && redisCheck;
+
+  return res.status(healthy ? 200 : 500).json({
+    podname: process.env.PODNAME,
+    healthy: healthy,
+    writeDb: writeDbCheck,
+    readDb: readDbCheck,
     redis: redisCheck !== null,
     session: session !== null,
   });
