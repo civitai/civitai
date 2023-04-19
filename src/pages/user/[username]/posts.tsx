9f1e8b78acc844bffdb686a46e5979a55a698ce7
Manuel E. Urena <manuel.ureh@hotmail.com>
2023-04-19 10:42:23 -0400

    Fixes issue where users were not able to see their own posts page

--- src/pages/user/[username]/posts.tsx
+++ src/pages/user/[username]/posts.tsx
@@ -1,17 +1,22 @@
-import { useRouter } from 'next/router';
-import { useCurrentUser } from '~/hooks/useCurrentUser';
-
 import { Container } from '@mantine/core';
+import { useRouter } from 'next/router';
+
 import { NotFound } from '~/components/AppLayout/NotFound';
 import PostsInfinite from '~/components/Post/Infinite/PostsInfinite';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
 import { userPageQuerySchema } from '~/server/schema/user.schema';
+import { postgresSlugify } from '~/utils/string-helpers';
 
 export default function UserPosts() {
   const router = useRouter();
   const { id, username } = userPageQuerySchema.parse(router.query);
   const currentUser = useCurrentUser();
 
-  if (!username || (!currentUser?.isModerator && username !== currentUser?.username))
+  if (
+    !username ||
+    !currentUser?.username ||
+    (!currentUser.isModerator && username !== postgresSlugify(currentUser.username))
+  )
     return <NotFound />;
 
   return (
