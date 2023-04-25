64089dbda8138020e6218d7f3fffe45af0a544e5
Justin Maier <Zipp425@gmail.com>
2023-04-25 15:11:48 -0600

    Revise PlayFab integration to use server api

--- src/server/playfab/client-wrapper.ts
+++ src/server/playfab/client-wrapper.ts
@@ -1,7 +1,7 @@
-import { PlayFab, PlayFabClient } from 'playfab-sdk';
+import { PlayFab, PlayFabClient, PlayFabServer } from 'playfab-sdk';
 import { promisify } from '~/utils/callback-helpers';
 
-export async function LoginWithCustomID(userId: number) {
+async function ClientLoginWithCustomID(userId: number) {
   return promisify<PlayFabModule.IPlayFabSuccessContainer<PlayFabClientModels.LoginResult>>(
     PlayFabClient.LoginWithCustomID,
     {
@@ -12,7 +12,43 @@ export async function LoginWithCustomID(userId: number) {
   );
 }
 
-export async function WritePlayerEvent(
+async function ServerLinkCustomID(userId: number, playFabId: string) {
+  return promisify<
+    PlayFabModule.IPlayFabSuccessContainer<PlayFabServerModels.LinkServerCustomIdResult>
+  >(PlayFabServer.LinkServerCustomId, {
+    ServerCustomId: userId.toString(),
+    PlayFabId: playFabId,
+    ForceLink: true,
+  });
+}
+
+async function ServerLoginWithCustomID(userId: number) {
+  return promisify<PlayFabModule.IPlayFabSuccessContainer<PlayFabServerModels.ServerLoginResult>>(
+    PlayFabServer.LoginWithServerCustomId,
+    {
+      ServerCustomId: userId.toString(),
+      CreateAccount: false,
+    }
+  );
+}
+
+export async function LoginWithCustomID(userId: number) {
+  let serverLogin = await ServerLoginWithCustomID(userId);
+  if ((serverLogin.errorCode = 1001)) {
+    // If server login fails, login with the client and link the accounts
+    const clientLogin = await ClientLoginWithCustomID(userId);
+    if (clientLogin.errorCode == 1001 || !clientLogin.data.PlayFabId)
+      throw new Error('Playfab login failed');
+
+    const serverLink = await ServerLinkCustomID(userId, clientLogin.data.PlayFabId);
+    if (serverLink.errorCode) throw new Error('Playfab link failed');
+    serverLogin = await ServerLoginWithCustomID(userId);
+  }
+
+  return serverLogin;
+}
+
+export async function WritePlayerEventClient(
   sessionTicket: string,
   event: PlayFabClientModels.WriteClientPlayerEventRequest
 ) {
@@ -20,5 +56,15 @@ export async function WritePlayerEvent(
   return promisify<PlayFabModule.IPlayFabSuccessContainer<PlayFabClientModels.WriteEventResponse>>(
     PlayFabClient.WritePlayerEvent,
     { ...event }
+  );
+}
+
+export async function WritePlayerEvent(
+  playFabId: string,
+  event: Omit<PlayFabServerModels.WriteServerPlayerEventRequest, 'PlayFabId'>
+) {
+  return promisify<PlayFabModule.IPlayFabSuccessContainer<PlayFabServerModels.WriteEventResponse>>(
+    PlayFabServer.WritePlayerEvent,
+    { ...event, PlayFabId: playFabId }
   );
 }
