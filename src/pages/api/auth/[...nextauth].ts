23191049dbbb9d7ae9f1d36ba6fcebafe084aaaa
Koen <koen@linker.io>
2023-04-26 13:54:51 +0100

    User registration tacking

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,8 @@ import { getRandomInt } from '~/utils/number-helpers';
 import { sendVerificationRequest } from '~/server/auth/verificationEmail';
 import { refreshToken, invalidateSession } from '~/server/utils/session-helpers';
 import { getSessionUser, updateAccountScope } from '~/server/services/user.service';
+import { Tracker } from '~/server/clickhouse/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const setUserName = async (id: number, setTo: string) => {
   try {
@@ -37,104 +39,121 @@ const cookiePrefix = useSecureCookies ? '__Secure-' : '';
 const { hostname } = new URL(env.NEXTAUTH_URL);
 const cookieName = `${cookiePrefix}civitai-token`;
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(dbWrite),
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  events: {
-    createUser: async ({ user }) => {
-      const startingUsername = user.email?.trim() ?? user.name?.trim() ?? `civ_`;
-      if (startingUsername) {
-        let username: string | undefined = undefined;
-        while (!username) username = await setUserName(Number(user.id), startingUsername);
-      }
+export function createAuthOptions(): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(dbWrite),
+    session: {
+      strategy: 'jwt',
+      maxAge: 30 * 24 * 60 * 60, // 30 days
     },
-  },
-  callbacks: {
-    async signIn({ account }) {
-      if (account?.provider === 'discord' && !!account.scope) await updateAccountScope(account);
-
-      return true;
-    },
-    async jwt({ token, user, trigger }) {
-      if (trigger === 'update') {
-        await invalidateSession(Number(token.sub));
-        const user = await getSessionUser({ userId: Number(token.sub) });
-        token.user = user;
-      } else {
-        token.sub = Number(token.sub) as any; //eslint-disable-line
-        if (user) token.user = user;
-        const { deletedAt, ...restUser } = token.user as User;
-        token.user = { ...restUser };
-      }
-
-      return token;
-    },
-    async session({ session, token }) {
-      token = await refreshToken(token);
-      session.user = (token.user ? token.user : session.user) as Session['user'];
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-      authorization: {
-        params: { scope: 'identify email role_connections.write' },
+    events: {
+      createUser: async ({ user }) => {
+        const startingUsername = user.email?.trim() ?? user.name?.trim() ?? `civ_`;
+        if (startingUsername) {
+          let username: string | undefined = undefined;
+          while (!username) username = await setUserName(Number(user.id), startingUsername);
+        }
       },
-    }),
-    GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    RedditProvider({
-      clientId: env.REDDIT_CLIENT_ID,
-      clientSecret: env.REDDIT_CLIENT_SECRET,
-      authorization: {
-        params: {
-          duration: 'permanent',
+    },
+    callbacks: {
+      async signIn({ account }) {
+        if (account?.provider === 'discord' && !!account.scope) await updateAccountScope(account);
+
+        return true;
+      },
+      async jwt({ token, user, trigger }) {
+        if (trigger === 'update') {
+          await invalidateSession(Number(token.sub));
+          const user = await getSessionUser({ userId: Number(token.sub) });
+          token.user = user;
+        } else {
+          token.sub = Number(token.sub) as any; //eslint-disable-line
+          if (user) token.user = user;
+          const { deletedAt, ...restUser } = token.user as User;
+          token.user = { ...restUser };
+        }
+
+        return token;
+      },
+      async session({ session, token }) {
+        token = await refreshToken(token);
+        session.user = (token.user ? token.user : session.user) as Session['user'];
+        return session;
+      },
+    },
+    // Configure one or more authentication providers
+    providers: [
+      DiscordProvider({
+        clientId: env.DISCORD_CLIENT_ID,
+        clientSecret: env.DISCORD_CLIENT_SECRET,
+        authorization: {
+          params: { scope: 'identify email role_connections.write' },
+        },
+      }),
+      GithubProvider({
+        clientId: env.GITHUB_CLIENT_ID,
+        clientSecret: env.GITHUB_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true,
+      }),
+      GoogleProvider({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true,
+      }),
+      RedditProvider({
+        clientId: env.REDDIT_CLIENT_ID,
+        clientSecret: env.REDDIT_CLIENT_SECRET,
+        authorization: {
+          params: {
+            duration: 'permanent',
+          },
+        },
+      }),
+      EmailProvider({
+        server: {
+          host: env.EMAIL_HOST,
+          port: env.EMAIL_PORT,
+          auth: {
+            user: env.EMAIL_USER,
+            pass: env.EMAIL_PASS,
+          },
+        },
+        sendVerificationRequest,
+        from: env.EMAIL_FROM,
+      }),
+    ],
+    cookies: {
+      sessionToken: {
+        name: cookieName,
+        options: {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+          secure: useSecureCookies,
+          domain: hostname == 'localhost' ? hostname : '.' + hostname, // add a . in front so that subdomains are included
         },
       },
-    }),
-    EmailProvider({
-      server: {
-        host: env.EMAIL_HOST,
-        port: env.EMAIL_PORT,
-        auth: {
-          user: env.EMAIL_USER,
-          pass: env.EMAIL_PASS,
-        },
-      },
-      sendVerificationRequest,
-      from: env.EMAIL_FROM,
-    }),
-  ],
-  cookies: {
-    sessionToken: {
-      name: cookieName,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: useSecureCookies,
-        domain: hostname == 'localhost' ? hostname : '.' + hostname, // add a . in front so that subdomains are included
-      },
     },
-  },
-  pages: {
-    signIn: '/login',
-    error: '/login',
-  },
-};
+    pages: {
+      signIn: '/login',
+      error: '/login',
+    },
+  };
+}
 
-export default NextAuth(authOptions);
+export const authOptions = createAuthOptions();
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  const customAuthOptions = createAuthOptions();
+  customAuthOptions.events ??= {};
+  customAuthOptions.events.signIn = async (context) => {
+    if (context.isNewUser) {
+      const tracker = new Tracker(req, res);
+      await tracker.userActivity({
+        type: 'Registration',
+        targetUserId: parseInt(context.user.id),
+      });
+    }
+  };
+  return await NextAuth(req, res, customAuthOptions);
+}
