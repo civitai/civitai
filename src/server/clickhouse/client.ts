a5050f2e73b0a25a22b630eaf3cd3a3186e27875
Koen <koen@linker.io>
2023-04-26 14:02:04 +0100

    Make req and res optional

--- src/server/clickhouse/client.ts
+++ src/server/clickhouse/client.ts
@@ -64,17 +64,26 @@ export type TrackRequest = {
 };
 
 export class Tracker {
-  constructor(private req: NextApiRequest, private res: NextApiResponse) {}
+  constructor(private req?: NextApiRequest, private res?: NextApiResponse) {}
 
   private async track(table: string, custom: object) {
-    const values = {
-      ip: requestIp.getClientIp(this.req),
-      userAgent: this.req.headers['user-agent'],
-      userId: (await getServerAuthSession({ req: this.req, res: this.res }))?.user?.id,
-      ...custom,
-    };
+    const values =
+      this.req && this.res
+        ? {
+            ip: requestIp.getClientIp(this.req),
+            userAgent: this.req.headers['user-agent'],
+            userId: (await getServerAuthSession({ req: this.req, res: this.res }))?.user?.id,
+            ...custom,
+          }
+        : {
+            ip: 'unknown',
+            userEngagement: 'unknown',
+            userId: 0,
+            ...custom,
+          };
+
     // do not await as we do not want to fail on tracker issues
-    await clickhouse.insert({
+    clickhouse.insert({
       table: table,
       values: [values],
       format: 'JSONEachRow',
