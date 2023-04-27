d0c2b79c26372016c5f20c50bfa4964899ef3a83
Justin Maier <Zipp425@gmail.com>
2023-04-27 11:08:49 -0600

    Simplify tracking

--- src/server/controllers/track.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from '~/server/createContext';
-import { AddViewSchema } from '../schema/track.schema';
-
-export async function addViewHandler({ input, ctx }: { input: AddViewSchema; ctx: Context }) {
-  await ctx.track.view(input);
-}
--- src/server/routers/track.router.ts
+++ src/server/routers/track.router.ts
@@ -1,7 +1,6 @@
-import { addViewHandler } from '~/server/controllers/track.controller';
 import { addViewSchema } from '~/server/schema/track.schema';
 import { publicProcedure, router } from '~/server/trpc';
 
 export const trackRouter = router({
-  addView: publicProcedure.input(addViewSchema).mutation(addViewHandler),
+  addView: publicProcedure.input(addViewSchema).mutation(({ input, ctx }) => ctx.track.view(input)),
 });
