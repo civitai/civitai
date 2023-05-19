2a8a71f0261257fbd13ce5bc5e9b604788cd9da0
Briant Diehl <bkdiehl@gmail.com>
2023-05-19 16:39:54 -0600

    virtualized leaderboard list

---
+++ src/components/Leaderboard/CreatorList2.tsx
@@ -0,0 +1,27 @@
+import { useMantineTheme } from '@mantine/core';
+import { List } from 'masonic';
+
+import { useRouter } from 'next/router';
+import { LeaderboardGetModel } from '~/types/router';
+import { CreatorCard } from '~/components/Leaderboard/CreatorCard';
+
+export function CreatorList2({ data }: { data: LeaderboardGetModel[] }) {
+  const theme = useMantineTheme();
+  const router = useRouter();
+  const { position } = router.query;
+
+  return (
+    <List
+      items={data}
+      render={CreatorCard}
+      scrollToIndex={
+        position
+          ? { index: data.findIndex((x) => x.position === Number(position)), align: 'center' }
+          : undefined
+      }
+      rowGutter={theme.spacing.md}
+      overscanBy={3}
+      itemHeightEstimate={98}
+    />
+  );
+}
