efe3378e8e5dabdd429a9c1a775a9f78c1deb419
Justin Maier <Zipp425@gmail.com>
2023-06-09 16:32:32 -0600

    Adjust RankBadge

--- src/components/Leaderboard/RankBadge.tsx
+++ src/components/Leaderboard/RankBadge.tsx
@@ -1,35 +1,59 @@
-import { BadgeProps, Group, MantineSize, Text } from '@mantine/core';
+import { BadgeProps, Box, Group, MantineColor, MantineSize, Text, Tooltip } from '@mantine/core';
 import { IconCrown } from '@tabler/icons-react';
 import { EdgeImage } from '~/components/EdgeImage/EdgeImage';
 import { IconBadge } from '~/components/IconBadge/IconBadge';
 
+const rankColors: Record<number, MantineColor> = {
+  1: 'blue',
+  3: 'yellow',
+  10: 'silver',
+  100: 'orange',
+};
+
 export const RankBadge = ({ rank, size, textSize = 'sm', iconSize = 18, ...props }: Props) => {
   if (!rank || !rank.leaderboardRank || rank.leaderboardRank > 100) return null;
+
+  let badgeColor: MantineColor = 'gray';
+  for (const [rankLimit, rankColor] of Object.entries(rankColors)) {
+    if (rank.leaderboardRank <= parseInt(rankLimit)) {
+      badgeColor = rankColor;
+      break;
+    }
+  }
 
   const hasLeaderboardCosmetic = !!rank.leaderboardCosmetic;
 
   return (
-    <Group spacing={0} noWrap>
-      {rank.leaderboardCosmetic ? <EdgeImage src={rank.leaderboardCosmetic} width={32} /> : null}
-      <IconBadge
-        size={size}
-        tooltip={`${rank.leaderboardTitle} Rank`}
-        color="yellow"
-        // variant="outline"
-        href={`/leaderboard/${rank.leaderboardId}?position=${rank.leaderboardRank}`}
-        icon={!hasLeaderboardCosmetic ? <IconCrown size={iconSize} /> : undefined}
-        sx={
-          hasLeaderboardCosmetic
-            ? { padding: '2px 8px', marginLeft: '-2px', height: 'auto' }
-            : undefined
-        }
-        {...props}
-      >
-        <Text size={textSize} inline>
-          #{rank.leaderboardRank}
-        </Text>
-      </IconBadge>
-    </Group>
+    <Tooltip label={`${rank.leaderboardTitle} Rank`} position="top" color="dark" withArrow>
+      <Group spacing={0} noWrap sx={{ position: 'relative' }}>
+        {rank.leaderboardCosmetic ? (
+          <Box pos="relative" sx={{ zIndex: 2 }}>
+            <EdgeImage src={rank.leaderboardCosmetic} width={32} />
+          </Box>
+        ) : null}
+        <IconBadge
+          size={size}
+          color={badgeColor}
+          href={`/leaderboard/${rank.leaderboardId}?position=${rank.leaderboardRank}`}
+          icon={!hasLeaderboardCosmetic ? <IconCrown size={iconSize} /> : undefined}
+          sx={
+            hasLeaderboardCosmetic
+              ? {
+                  paddingLeft: 16,
+                  marginLeft: -14,
+                  borderTopLeftRadius: 0,
+                  borderBottomLeftRadius: 0,
+                }
+              : undefined
+          }
+          {...props}
+        >
+          <Text size={textSize} inline>
+            #{rank.leaderboardRank}
+          </Text>
+        </IconBadge>
+      </Group>
+    </Tooltip>
   );
 };
 
