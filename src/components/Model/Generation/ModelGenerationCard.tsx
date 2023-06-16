fd0fbce7f729bcf9a5a104c9d026910217476664
Manuel E. Urena <manuel.ureh@hotmail.com>
2023-06-16 19:01:06 -0400

    Adds previous button in model generation card

--- src/components/Model/Generation/ModelGenerationCard.tsx
+++ src/components/Model/Generation/ModelGenerationCard.tsx
@@ -15,11 +15,19 @@ import {
   Text,
   createStyles,
 } from '@mantine/core';
+import { getHotkeyHandler } from '@mantine/hooks';
 import { openConfirmModal } from '@mantine/modals';
 import { ModelVersionExploration } from '@prisma/client';
-import { IconChevronRight, IconDotsVertical, IconEdit, IconTrash } from '@tabler/icons-react';
+import {
+  IconChevronLeft,
+  IconChevronRight,
+  IconDotsVertical,
+  IconEdit,
+  IconTrash,
+} from '@tabler/icons-react';
 import { IconInfoCircle, IconPlus } from '@tabler/icons-react';
 import { useEffect, useRef, useState } from 'react';
+import { DismissibleAlert } from '~/components/DismissibleAlert/DismissibleAlert';
 
 import { GenerationPromptModal } from '~/components/Model/Generation/GenerationPromptModal';
 import { useCurrentUser } from '~/hooks/useCurrentUser';
@@ -46,6 +54,8 @@ const useStyles = createStyles((theme, _params, getRef) => ({
   iconWrapper: {
     ref: getRef('iconWrapper'),
   },
+
+  root: { display: 'flex' },
 
   nextButton: {
     backgroundColor: theme.colors.gray[0],
@@ -164,49 +174,61 @@ export function ModelGenerationCard({ columnWidth, height, versionId, modelId }:
           withBorder
         >
           <Card.Section py="xs" inheritPadding withBorder>
-            <Group spacing="xs" position="apart">
-              <Group spacing={8}>
-                <Image
-                  src="https://downloads.intercomcdn.com/i/o/415875/17821df0928378c5e14e54e6/17c1c63527031e39c565ab2c57308471.png"
-                  width={32}
-                  height={32}
-                  alt="some alt"
-                  radius="sm"
-                  withPlaceholder
-                />
-                <Stack spacing={0}>
-                  <Text size="sm" weight="bold">
-                    Generated Exploration
-                  </Text>
-                  <Text size="xs" color="dimmed">
-                    A service provided by{' '}
-                    <Anchor
-                      href="https://picfinder.ai"
-                      target="_blank"
-                      rel="noopened noreferrer"
-                      inherit
-                      span
-                    >
-                      PicFinder
-                    </Anchor>
-                  </Text>
-                </Stack>
+            <Stack spacing={4}>
+              <Group spacing="xs" position="apart">
+                <Group spacing={8}>
+                  <Image
+                    src="https://downloads.intercomcdn.com/i/o/415875/17821df0928378c5e14e54e6/17c1c63527031e39c565ab2c57308471.png"
+                    width={32}
+                    height={32}
+                    alt="some alt"
+                    radius="sm"
+                    withPlaceholder
+                  />
+                  <Stack spacing={0}>
+                    <Text size="sm" weight="bold">
+                      Generated Exploration
+                    </Text>
+                    <Text size="xs" color="dimmed">
+                      A service provided by{' '}
+                      <Anchor
+                        href="https://picfinder.ai"
+                        target="_blank"
+                        rel="noopened noreferrer"
+                        inherit
+                        span
+                      >
+                        PicFinder
+                      </Anchor>
+                    </Text>
+                  </Stack>
+                </Group>
+                <Popover width={300} withArrow withinPortal>
+                  <Popover.Target>
+                    <ActionIcon radius="xl" variant="transparent">
+                      <IconInfoCircle />
+                    </ActionIcon>
+                  </Popover.Target>
+                  <Popover.Dropdown>
+                    The images you see here are being generated on demand by the PicFinder service.
+                    Select one of the pre-defined prompts from the creator below to start exploring
+                    the unlimited possibilities.
+                  </Popover.Dropdown>
+                </Popover>
               </Group>
-              <Popover width={300} withArrow withinPortal>
-                <Popover.Target>
-                  <ActionIcon radius="xl" variant="transparent">
-                    <IconInfoCircle />
-                  </ActionIcon>
-                </Popover.Target>
-                <Popover.Dropdown>
-                  The images you see here are being generated on demand by the PicFinder service.
-                  Select one of the pre-defined prompts from the creator below to start exploring
-                  the unlimited possibilities.
-                </Popover.Dropdown>
-              </Popover>
-            </Group>
+              <DismissibleAlert
+                id="generated-exploration"
+                content="These images are generated on demand. Press the next button to generate another image or select a different preset prompt below."
+              />
+            </Stack>
           </Card.Section>
-          <Card.Section sx={{ position: 'relative', height: height, overflow: 'hidden' }}>
+          <Card.Section
+            sx={{ position: 'relative', height, overflow: 'hidden' }}
+            onKeyDown={getHotkeyHandler([
+              ['ArrowLeft', (e) => e.preventDefault()],
+              ['ArrowRight', (e) => e.preventDefault()],
+            ])}
+          >
             {loading && !images.length ? (
               <Center h="100%">
                 <Loader size="md" variant="bars" />
@@ -235,6 +257,33 @@ export function ModelGenerationCard({ columnWidth, height, versionId, modelId }:
                     />
                   ))}
                 </div>
+                {!!data.length && !!images.length && currentIndex > 0 && (
+                  <ActionIcon
+                    className={classes.nextButton}
+                    radius="xl"
+                    size="md"
+                    color="gray"
+                    p={4}
+                    sx={{ position: 'absolute', top: '50%', left: 10 }}
+                    onClick={() => {
+                      viewportRef.current?.scrollBy({
+                        left: imageContainerWidth * -1,
+                        behavior: 'smooth',
+                      });
+
+                      if (selectedPrompt) {
+                        setAvailablePrompts((current) => ({
+                          ...current,
+                          [selectedPrompt.name]: {
+                            imageIndex: (current[selectedPrompt.name]?.imageIndex ?? 0) - 1,
+                          },
+                        }));
+                      }
+                    }}
+                  >
+                    <IconChevronLeft />
+                  </ActionIcon>
+                )}
                 {!!data.length && !!images.length && (
                   <ActionIcon
                     className={classes.nextButton}
@@ -265,8 +314,8 @@ export function ModelGenerationCard({ columnWidth, height, versionId, modelId }:
               </>
             )}
           </Card.Section>
-          <Card.Section py="xs" inheritPadding withBorder>
-            <Group spacing={8} noWrap>
+          <Card.Section pt="xs" inheritPadding withBorder>
+            <Group spacing={8} align="flex-start" noWrap>
               {isModerator && (
                 <ActionIcon
                   variant="outline"
@@ -282,7 +331,7 @@ export function ModelGenerationCard({ columnWidth, height, versionId, modelId }:
                   <IconPlus />
                 </ActionIcon>
               )}
-              <ScrollArea type="never">
+              <ScrollArea styles={{ viewport: { overflowY: 'hidden' } }} offsetScrollbars>
                 <Chip.Group
                   spacing={4}
                   value={prompt}
