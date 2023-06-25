e19d6aed3233dd3847257d8bc2c8e4b05db0889a
Justin Maier <Zipp425@gmail.com>
2023-06-25 15:30:36 -0600

    simplify on-demand run strat updating

--- src/server/jobs/add-on-demand-run-strategies.ts
+++ src/server/jobs/add-on-demand-run-strategies.ts
@@ -5,35 +5,29 @@ export const addOnDemandRunStrategiesJob = createJob(
   'add-on-demand-run-strategies',
   '33 * * * *',
   async () => {
-    await dbWrite.$transaction(async (tx) => {
-      // Add new
-      await tx.$executeRawUnsafe(`
+    await dbWrite.$transaction(
+      async (tx) => {
+        // Upsert new
+        await tx.$executeRaw`
         INSERT INTO "RunStrategy" ("partnerId", "modelVersionId", "url")
         SELECT "partnerId", "modelVersionId", "url"
         FROM "OnDemandRunStrategy" s
-        WHERE NOT EXISTS (
-          SELECT 1 FROM "RunStrategy"
-          WHERE "partnerId" = s."partnerId" AND "modelVersionId" = s."modelVersionId"
-        );
-      `);
+        ON CONFLICT("partnerId", "modelVersionId") DO UPDATE SET "url" = excluded."url";
+      `;
 
-      // Update existing
-      await tx.$executeRawUnsafe(`
-        UPDATE "RunStrategy" t
-        SET "url" = s."url"
-        FROM "OnDemandRunStrategy" s
-        WHERE t."partnerId" = s."partnerId" AND t."modelVersionId" = s."modelVersionId";
-      `);
-
-      // Delete old
-      await tx.$executeRawUnsafe(`
+        // Delete old
+        await tx.$executeRaw`
         DELETE FROM "RunStrategy" t
         USING "Partner" p
         WHERE
           p.id = t."partnerId"
           AND p."onDemand" = TRUE
           AND NOT EXISTS (SELECT 1 FROM "OnDemandRunStrategy" WHERE "modelVersionId" = t."modelVersionId" AND "partnerId" = t."partnerId");
-      `);
-    });
+      `;
+      },
+      {
+        timeout: 3 * 60 * 1000,
+      }
+    );
   }
 );
