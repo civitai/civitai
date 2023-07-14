86b4ca407eb7e6d276f5f55b9e9d0bb7df0c7d29
Briant Diehl <bkdiehl@gmail.com>
2023-07-14 13:54:32 -0600

    remove dead code

--- src/components/ImageGeneration/GenerationForm/BaseModelProvider.tsx
+++ src/components/ImageGeneration/GenerationForm/BaseModelProvider.tsx
@@ -1,6 +1,6 @@
 import { isEqual } from 'lodash';
 import { createContext, useContext, useEffect, useState } from 'react';
-import { FieldValues, UseFormReturn, DeepPartial, useFormContext, Path } from 'react-hook-form';
+import { FieldValues, DeepPartial, useFormContext } from 'react-hook-form';
 
 type State = {
   baseModels: string[];
@@ -12,35 +12,6 @@ export const useBaseModelsContext = () => {
   if (!context) throw new Error('BaseModelsContext not in tree');
   return context;
 };
-
-export function BaseModelProviderOld<T extends FieldValues>({
-  children,
-  form,
-  getBaseModels,
-}: {
-  children: React.ReactNode;
-  form: UseFormReturn<T>;
-  getBaseModels: (data: DeepPartial<T>) => string[];
-}) {
-  const [baseModels, setBaseModels] = useState<string[]>([]);
-
-  useEffect(() => {
-    const subscription = form.watch((value, { name, type }) => {
-      if (name === 'model' || name === 'resources' || name === 'vae') {
-        const baseModels = getBaseModels(value);
-        setBaseModels((state) => {
-          return isEqual(state, baseModels) ? state : baseModels;
-        });
-        //TODO - alert user if there are incompatible basemodels
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, []); //eslint-disable-line
-
-  // useEffect(() => console.log({ baseModels }), [baseModels]);
-
-  return <BaseModelsContext.Provider value={{ baseModels }}>{children}</BaseModelsContext.Provider>;
-}
 
 export function BaseModelProvider<T extends FieldValues>({
   children,
