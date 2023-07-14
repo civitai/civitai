abae317f7a954eba07470eb7bfe99c0bff65b565
Briant Diehl <bkdiehl@gmail.com>
2023-07-14 13:56:49 -0600

    remove dead code

--- src/server/routers/generation.router.ts
+++ src/server/routers/generation.router.ts
@@ -50,13 +50,7 @@ export const generationRouter = router({
   getResources: publicProcedure
     .input(getGenerationResourcesSchema)
     .query(({ ctx, input }) => getGenerationResources({ ...input, user: ctx.user })),
-  getRandomGenerationData: publicProcedure
-    .use(isFlagProtected('imageGeneration'))
-    .query(() => getRandomGenerationData()),
   checkResourcesCoverage: publicProcedure
     .input(checkResourcesCoverageSchema)
     .query(({ input }) => checkResourcesCoverage(input)),
-  getGenerationData: publicProcedure
-    .input(getGenerationDataSchema)
-    .query(({ input }) => getGenerationData(input)),
 });
