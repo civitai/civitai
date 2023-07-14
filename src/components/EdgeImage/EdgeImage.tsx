98f67e864d19ebde7ac06253352d1907d36b46e4
Briant Diehl <bkdiehl@gmail.com>
2023-07-14 13:52:04 -0600

    revert changes to edgeImage

--- src/components/EdgeImage/EdgeImage.tsx
+++ src/components/EdgeImage/EdgeImage.tsx
@@ -10,87 +10,49 @@ export type EdgeImageProps = EdgeUrlProps &
     'src' | 'srcSet' | 'ref' | 'width' | 'height' | 'loading' | 'metadata'
   >;
 
-// export function EdgeImage({
-//   src,
-//   width,
-//   height,
-//   fit,
-//   anim,
-//   blur,
-//   quality,
-//   gravity,
-//   className,
-//   name,
-//   ...imgProps
-// }: EdgeImageProps) {
-//   const { classes, cx } = useStyles({ maxWidth: width });
-//   const currentUser = useCurrentUser();
+export function EdgeImage({
+  src,
+  width,
+  height,
+  fit,
+  anim,
+  blur,
+  quality,
+  gravity,
+  className,
+  name,
+  ...imgProps
+}: EdgeImageProps) {
+  const { classes, cx } = useStyles({ maxWidth: width });
+  const currentUser = useCurrentUser();
 
-//   if (width) width = Math.min(width, 4096);
-//   if (height) height = Math.min(height, 4096);
-//   const isGif = imgProps.alt?.endsWith('.gif');
-//   anim ??= isGif && currentUser ? (!currentUser.autoplayGifs ? false : undefined) : undefined;
-//   const gamma = anim === false ? 0.99 : undefined;
-//   if (anim && !isGif) anim = undefined;
-//   const optimized = currentUser?.filePreferences?.imageFormat === 'optimized';
+  if (width) width = Math.min(width, 4096);
+  if (height) height = Math.min(height, 4096);
+  const isGif = imgProps.alt?.endsWith('.gif');
+  anim ??= isGif && currentUser ? (!currentUser.autoplayGifs ? false : undefined) : undefined;
+  const gamma = anim === false ? 0.99 : undefined;
+  if (anim && !isGif) anim = undefined;
+  const optimized = currentUser?.filePreferences?.imageFormat === 'optimized';
 
-//   const _src = src
-//     ? getEdgeUrl(src, {
-//         width,
-//         height,
-//         fit,
-//         anim,
-//         blur,
-//         quality,
-//         gravity,
-//         optimized: optimized ? true : undefined,
-//         gamma,
-//         name,
-//       })
-//     : undefined;
+  const _src = src
+    ? getEdgeUrl(src, {
+        width,
+        height,
+        fit,
+        anim,
+        blur,
+        quality,
+        gravity,
+        optimized: optimized ? true : undefined,
+        gamma,
+        name,
+      })
+    : undefined;
 
-//   // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
-//   return <img className={cx(classes.responsive, className)} src={_src} {...imgProps} />;
-// }
+  // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
+  return <img className={cx(classes.responsive, className)} src={_src} {...imgProps} />;
+}
 
 const useStyles = createStyles((_theme, params: { maxWidth?: number }) => ({
   responsive: { width: '100%', height: 'auto', maxWidth: params.maxWidth },
 }));
-
-export const EdgeImage = forwardRef<HTMLImageElement, EdgeImageProps>(
-  (
-    { src, width, height, fit, anim, blur, quality, gravity, className, name, ...imgProps },
-    ref
-  ) => {
-    const { classes, cx } = useStyles({ maxWidth: width });
-    const currentUser = useCurrentUser();
-
-    if (width) width = Math.min(width, 4096);
-    if (height) height = Math.min(height, 4096);
-    const isGif = imgProps.alt?.endsWith('.gif');
-    anim ??= isGif && currentUser ? (!currentUser.autoplayGifs ? false : undefined) : undefined;
-    const gamma = anim === false ? 0.99 : undefined;
-    if (anim && !isGif) anim = undefined;
-    const optimized = currentUser?.filePreferences?.imageFormat === 'optimized';
-
-    const _src = src
-      ? getEdgeUrl(src, {
-          width,
-          height,
-          fit,
-          anim,
-          blur,
-          quality,
-          gravity,
-          optimized: optimized ? true : undefined,
-          gamma,
-          name,
-        })
-      : undefined;
-
-    // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
-    return <img ref={ref} className={cx(classes.responsive, className)} src={_src} {...imgProps} />;
-  }
-);
-
-EdgeImage.displayName = 'EdgeImage';
