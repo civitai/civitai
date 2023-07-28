639e801c6a4f7c07471c29d0fc08b41978e73414
Briant Diehl <bkdiehl@gmail.com>
2023-07-28 12:43:20 -0600

    update migration for image type

--- prisma/migrations/20230728170432_image_type/migration.sql
+++ prisma/migrations/20230728170432_image_type/migration.sql
@@ -2,4 +2,10 @@
 ALTER TABLE "Image" ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}',
 ADD COLUMN     "type" "MediaType" NOT NULL DEFAULT 'image';
 
+UPDATE "Image"
+SET metadata = json_build_object('hash', hash, 'width', width, 'height', height)
+
+UPDATE "Image"
+SET type = 'video'
+WHERE "mimeType" = 'image/gif'
 
