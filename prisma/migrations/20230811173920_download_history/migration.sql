f510c6ae47bb8440c881f5ff30054b9da6fbcf12
Koen <koen@linker.io>
2023-08-11 20:51:27 +0100

    Population script

--- prisma/migrations/20230811173920_download_history/migration.sql
+++ prisma/migrations/20230811173920_download_history/migration.sql
@@ -1,12 +1,8 @@
 -- DropForeignKey
 ALTER TABLE "UserActivity" DROP CONSTRAINT "UserActivity_userId_fkey";
 
-
--- DropTable
-DROP TABLE "UserActivity";
-
--- DropEnum
-DROP TYPE "UserActivityType";
+-- Drop old view
+DROP VIEW "DownloadHistory";
 
 -- CreateTable
 CREATE TABLE "DownloadHistory" (
@@ -14,13 +10,27 @@ CREATE TABLE "DownloadHistory" (
     "modelVersionId" INTEGER NOT NULL,
     "downloadAt" TIMESTAMP(3) NOT NULL,
     "hidden" BOOLEAN NOT NULL DEFAULT false,
-    "modelId" INTEGER,
 
     CONSTRAINT "DownloadHistory_pkey" PRIMARY KEY ("userId","modelVersionId")
 );
 
+-- Populate data
+INSERT INTO "DownloadHistory" ("userId", "modelVersionId", "downloadAt", "hidden")
+SELECT "userId", "modelVersionId", MAX("downloadAt"), MAX(hidden)
+FROM (
+    SELECT
+        "userId",
+        (details ->> 'modelVersionId')::integer as "modelVersionId",
+        "createdAt" AS "downloadAt",
+        "hide" AS "hidden"
+    FROM "UserActivity"
+    WHERE "userId" > 0 AND activity = 'ModelDownload'
+) d
+WHERE "modelVersionId" IS NOT NULL
+GROUP BY "userId", "modelVersionId";
+
 -- CreateIndex
-CREATE INDEX "DownloadHistory_downloadAt_idx" ON "DownloadHistory"("downloadAt");
+CREATE INDEX "DownloadHistory_userId_downloadAt_idx" ON "DownloadHistory"("userId", "downloadAt");
 
 -- AddForeignKey
 ALTER TABLE "DownloadHistory" ADD CONSTRAINT "DownloadHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
@@ -30,3 +40,9 @@ ALTER TABLE "DownloadHistory" ADD CONSTRAINT "DownloadHistory_modelVersionId_fke
 
 -- AddForeignKey
 ALTER TABLE "DownloadHistory" ADD CONSTRAINT "DownloadHistory_modelId_fkey" FOREIGN KEY ("modelId") REFERENCES "Model"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- DropTable
+DROP TABLE "UserActivity";
+
+-- DropEnum
+DROP TYPE "UserActivityType";
