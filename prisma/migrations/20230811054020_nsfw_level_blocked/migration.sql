bc7dced3f584b2263854d37543f4eea830e6244f
Briant Diehl <bkdiehl@gmail.com>
2023-08-16 11:47:06 -0600

    add `commit` to migration

--- prisma/migrations/20230811054020_nsfw_level_blocked/migration.sql
+++ prisma/migrations/20230811054020_nsfw_level_blocked/migration.sql
@@ -1,5 +1,6 @@
 -- AlterEnum
 ALTER TYPE "NsfwLevel" ADD VALUE 'Blocked';
+COMMIT;
 
 -- Update blocked tags
 UPDATE "Tag" SET nsfw = 'Blocked' WHERE name IN ('self injury', 'hanging', 'hate symbols', 'nazi party', 'white supremacy', 'extremist');
