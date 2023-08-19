99cf3908637d56f0ffc9e2984b3c63ce41fc9cb9
Justin Maier <Zipp425@gmail.com>
2023-08-18 19:56:02 -0600

    Remove ctrl+p spotlight trigger

--- src/components/QuickSearch/QuickSearch.tsx
+++ src/components/QuickSearch/QuickSearch.tsx
@@ -212,7 +212,7 @@ function InnerSearch({ children, ...props }: SearchBoxProps & { children: React.
         onQueryChange={handleQueryChange}
         cleanQueryOnClose={false}
         filter={(_, actions) => actions}
-        shortcut={['mod + p', 'mod + k', '/']}
+        shortcut={['mod + k', '/']}
         limit={20}
         styles={(theme) => ({
           inner: { paddingTop: 'var(--mantine-header-height,50px)' },
