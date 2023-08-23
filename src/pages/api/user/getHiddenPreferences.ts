cf1c7c7599cf5bf7b0aee626793f187d33d92447
Briant Diehl <bkdiehl@gmail.com>
2023-08-23 16:16:19 -0600

    change format of file upload key

--- src/pages/api/user/getHiddenPreferences.ts
+++ src/pages/api/user/getHiddenPreferences.ts
@@ -4,14 +4,10 @@ import { getServerAuthSession } from '~/server/utils/get-server-auth-session';
 
 export default PublicEndpoint(async function handler(req, res) {
   const { refresh } = req.query;
-  console.time('get session');
-  console.time('total fetch time');
   const session = await getServerAuthSession({ req, res });
-  console.timeEnd('get session');
   const hiddenPreferences = await getAllHiddenForUser({
     userId: session?.user?.id ?? -1,
     refreshCache: refresh ? Boolean(refresh) : false,
   });
-  console.timeEnd('total fetch time');
   res.status(200).json(hiddenPreferences);
 });
