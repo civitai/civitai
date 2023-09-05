4ef2863283a66d01fd29a07638c9d01bb9aee1f1
manuelurenah <manuel.ureh@hotmail.com>
2023-09-05 16:44:45 -0400

    Fixes not being able to edit articles because of files

--- src/server/services/file.service.ts
+++ src/server/services/file.service.ts
@@ -1,9 +1,15 @@
+import { Prisma } from '@prisma/client';
 import { dbRead } from '../db/client';
 import { GetFilesByEntitySchema } from '~/server/schema/file.schema';
 
-export const getFilesByEntity = ({ id, type }: GetFilesByEntitySchema) => {
-  return dbRead.file.findMany({
+export const getFilesByEntity = async ({ id, type }: GetFilesByEntitySchema) => {
+  const files = await dbRead.file.findMany({
     where: { entityId: id, entityType: type },
     select: { id: true, name: true, url: true, sizeKB: true, metadata: true },
   });
+
+  return files.map(({ metadata, ...file }) => ({
+    ...file,
+    metadata: (metadata as Prisma.JsonObject) ?? {},
+  }));
 };
