f5847d7068c4b7135a12daa0682b5b4320377801
manuelurenah <manuel.ureh@hotmail.com>
2023-09-08 12:26:43 -0400

    Adds ability to like/track bounties

--- src/components/Bounty/bounty.utils.ts
+++ src/components/Bounty/bounty.utils.ts
@@ -6,6 +6,8 @@ import { trpc } from '~/utils/trpc';
 import { useFiltersContext } from '~/providers/FiltersProvider';
 import { removeEmpty } from '~/utils/object-helpers';
 import { MIN_CREATE_BOUNTY_AMOUNT } from '~/server/common/constants';
+import { ToggleUserBountyEngagementsInput } from '~/server/schema/user.schema';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
 
 export const getBountyCurrency = (bounty?: {
   id: number;
@@ -80,4 +82,44 @@ export const getMainBountyAmount = (bounty?: {
   }
 
   return MIN_CREATE_BOUNTY_AMOUNT;
+};
+
+export const useBountyEngagement = ({ bountyId }: { bountyId: number }) => {
+  const currentUser = useCurrentUser();
+  const queryUtils = trpc.useContext();
+
+  const { data: engagements } = trpc.user.getBountyEngagement.useQuery(undefined, {
+    enabled: !!currentUser,
+  });
+
+  const toggleEngagementMutation = trpc.user.toggleBountyEngagement.useMutation({
+    async onMutate({ type, bountyId }) {
+      await queryUtils.user.getBountyEngagement.cancel();
+      await queryUtils.bounty.getById.cancel();
+
+      const previousEngagements = queryUtils.user.getBountyEngagement.getData() ?? {};
+      const previousBounty = queryUtils.bounty.getById.getData({ id: bountyId });
+      const ids = previousEngagements[type] ?? [];
+      const isToggled = !!ids.find((id) => id === bountyId);
+
+      // TODO.bounty: optimistic update current bounty stats
+
+      queryUtils.user.getBountyEngagement.setData(undefined, (old = {}) => ({
+        ...old,
+        [type]: isToggled ? ids.filter((id) => id !== bountyId) : [...ids, bountyId],
+      }));
+
+      return { previousEngagements, previousBounty };
+    },
+    onError: (_error, _variables, context) => {
+      queryUtils.user.getBountyEngagement.setData(undefined, context?.previousEngagements);
+      queryUtils.bounty.getById.setData({ id: bountyId }, context?.previousBounty);
+    },
+  });
+
+  const handleToggle = async ({ type }: ToggleUserBountyEngagementsInput) => {
+    await toggleEngagementMutation.mutateAsync({ bountyId, type });
+  };
+
+  return { engagements, toggle: handleToggle, toggling: toggleEngagementMutation.isLoading };
 };
