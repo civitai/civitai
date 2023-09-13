8ab89d4c187b1776611581ac88d4910e2be116bf
Justin Maier <Zipp425@gmail.com>
2023-09-13 00:43:10 -0600

    Add social homeblock and refine block loading

---
+++ src/components/HomeBlocks/components/SocialBlock.tsx
@@ -0,0 +1,69 @@
+import { createStyles } from '@mantine/core';
+import { useMemo } from 'react';
+import { InstagramEmbed, TwitterEmbed, YouTubeEmbed } from 'react-social-media-embed';
+import { SocialBlockSchema } from '~/server/schema/home-block.schema';
+
+export type SocialBlockProps = {
+  url: string;
+  type: 'ig-reel' | 'ig-post' | 'yt-short' | 'yt-long' | 'tw-post';
+};
+
+const typeHeight: Partial<Record<SocialBlockSchema['type'], number>> = {
+  'ig-reel': 505,
+  'ig-post': 425,
+  'yt-short': 569,
+  'yt-long': 369,
+};
+
+const useStyles = createStyles((theme, { type }: { type: SocialBlockSchema['type'] }) => ({
+  card: {
+    borderRadius: theme.radius.md,
+    overflow: 'hidden',
+    height: typeHeight[type],
+    background: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    iframe: {
+      border: 'none !important',
+    },
+    [theme.fn.largerThan('sm')]: {
+      gridColumn: type === 'yt-long' ? 'span 2' : undefined,
+    },
+  },
+}));
+
+type Props = SocialBlockSchema;
+
+export function SocialBlock({ url, type }: Props) {
+  const { classes } = useStyles({ type });
+  const content = useMemo(() => {
+    if (type === 'ig-reel') return <InstagramReel url={url} />;
+    if (type === 'ig-post') return <InstagramPost url={url} />;
+    if (type === 'yt-short') return <YoutubeShort url={url} />;
+    if (type === 'yt-long') return <Youtube url={url} />;
+    if (type === 'tw-post') return <Tweet url={url} />;
+    return null;
+  }, [type]);
+
+  return <div className={classes.card}>{content}</div>;
+}
+
+function InstagramReel({ url }: { url: string }) {
+  return <InstagramEmbed url={url} width="100%" placeholderDisabled />;
+}
+
+function InstagramPost({ url }: { url: string }) {
+  return <InstagramEmbed url={url} width="100%" placeholderDisabled />;
+}
+
+function YoutubeShort({ url }: { url: string }) {
+  return (
+    <YouTubeEmbed url={url} width="100%" placeholderDisabled height={typeHeight['yt-short']} />
+  );
+}
+
+function Youtube({ url }: { url: string }) {
+  return <YouTubeEmbed url={url} width="100%" placeholderDisabled height={typeHeight['yt-long']} />;
+}
+
+function Tweet({ url }: { url: string }) {
+  return <TwitterEmbed url={url} width="100%" placeholderDisabled />;
+}
