9d24b8dc8e41185d2321d87f1ef5d27aef0d8cfd
Luis Rojas <lrojas94@gmail.com>
2023-09-13 17:03:57 -0400

    Add default entryMode to BenefactorOnly

--- src/server/services/bounty.service.ts
+++ src/server/services/bounty.service.ts
@@ -1,4 +1,4 @@
-import { Currency, ImageIngestionStatus, Prisma, TagTarget } from '@prisma/client';
+import { BountyEntryMode, Currency, ImageIngestionStatus, Prisma, TagTarget } from '@prisma/client';
 import { dbRead, dbWrite } from '../db/client';
 import { GetByIdInput } from '../schema/base.schema';
 import { updateEntityFiles } from './file.service';
@@ -95,6 +95,8 @@ export const createBounty = async ({
     const bounty = await tx.bounty.create({
       data: {
         ...data,
+        // TODO.bounty: Once we support tipping buzz fully, need to re-enable this
+        entryMode: BountyEntryMode.BenefactorsOnly,
         details: (data.details as Prisma.JsonObject) ?? Prisma.JsonNull,
         tags: tags
           ? {
