d8f1b8b83be9b95167b773ec3514c7db2b274afb
Brett Woodward <flipperbw@gmail.com>
2023-09-19 12:36:14 -0400

    move training signals function to the global registrar so it can update even when components are dismounted

--- src/components/Signals/SignalsRegistrar.tsx
+++ src/components/Signals/SignalsRegistrar.tsx
@@ -1,7 +1,9 @@
 import { useImageGenStatusUpdate } from '~/components/ImageGeneration/utils/generationRequestHooks';
+import { useTrainingSignals } from '~/components/Resource/Forms/Training/TrainingCommon';
 
 export function SignalsRegistrar() {
   useImageGenStatusUpdate();
+  useTrainingSignals();
 
   return null;
 }
