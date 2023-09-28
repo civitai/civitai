711ab3ac3f439479ebabe3547d1f4805e97d11dc
Luis Rojas <lrojas94@gmail.com>
2023-09-28 15:34:21 -0400

    use Hash indexes

--- prisma/migrations/20230928151649_collection_item_random_id/migration.sql
+++ prisma/migrations/20230928151649_collection_item_random_id/migration.sql
@@ -5,10 +5,10 @@ ALTER TABLE "Collection" ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}';
 ALTER TABLE "CollectionItem" ADD COLUMN     "randomId" INTEGER;
 
 -- CreateIndex
-CREATE INDEX "CollectionItem_addedById_idx" ON "CollectionItem"("addedById");
+CREATE INDEX "CollectionItem_addedById_idx" ON "CollectionItem" USING HASH ("addedById");
 
 -- CreateIndex
-CREATE INDEX "CollectionItem_imageId_idx" ON "CollectionItem"("imageId");
+CREATE INDEX "CollectionItem_imageId_idx" ON "CollectionItem" USING HASH ("imageId");
 
 -- CreateIndex
-CREATE INDEX "CollectionItem_modelId_idx" ON "CollectionItem"("modelId");
+CREATE INDEX "CollectionItem_modelId_idx" ON "CollectionItem" USING HASH ("modelId");
