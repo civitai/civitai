1454a477a48db9e131a91882dfd6d133decb3371
Justin Maier <Zipp425@gmail.com>
2023-09-29 12:50:22 -0600

    Adjust mime-type handling

--- src/server/common/mime-types.ts
+++ src/server/common/mime-types.ts
@@ -3,6 +3,7 @@ import { MediaType } from '@prisma/client';
 export const MIME_TYPES = {
   // Images
   png: 'image/png',
+  jpg: 'image/jpg',
   jpeg: 'image/jpeg',
   webp: 'image/webp',
 
@@ -23,6 +24,7 @@ export const MIME_TYPES = {
 export const MEDIA_TYPE: Record<string, MediaType> = {
   [MIME_TYPES.png]: MediaType.image,
   [MIME_TYPES.jpeg]: MediaType.image,
+  [MIME_TYPES.jpg]: MediaType.image,
   [MIME_TYPES.webp]: MediaType.image,
   [MIME_TYPES.mp4]: MediaType.video,
   [MIME_TYPES.webm]: MediaType.video,
@@ -30,7 +32,7 @@ export const MEDIA_TYPE: Record<string, MediaType> = {
   [MIME_TYPES.wav]: MediaType.audio,
 } as const;
 
-export const IMAGE_MIME_TYPE = [MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.webp];
+export const IMAGE_MIME_TYPE = [MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.jpg, MIME_TYPES.webp];
 export type IMAGE_MIME_TYPE = (typeof IMAGE_MIME_TYPE)[number];
 export const VIDEO_MIME_TYPE = [MIME_TYPES.mp4, MIME_TYPES.webm];
 export const AUDIO_MIME_TYPE = [MIME_TYPES.mp3, MIME_TYPES.wav];
