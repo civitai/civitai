4f0c8a503a2372fbf708ded879f896463e4ec8e6
Justin Maier <Zipp425@gmail.com>
2023-10-20 20:30:48 -0600

    Allow all origins in dev

--- src/server/createContext.ts
+++ src/server/createContext.ts
@@ -6,6 +6,7 @@ import { BrowsingMode } from '~/server/common/enums';
 import { getServerAuthSession } from '~/server/utils/get-server-auth-session';
 import { Tracker } from './clickhouse/client';
 import requestIp from 'request-ip';
+import { isProd } from '~/env/other';
 
 export const parseBrowsingMode = (
   cookies: Partial<{ [key: string]: string }>,
@@ -35,7 +36,9 @@ export const createContext = async ({
 }) => {
   const session = await getServerAuthSession({ req, res });
   const ip = requestIp.getClientIp(req) ?? '';
-  const acceptableOrigin = origins.some((o) => req.headers.referer?.startsWith(o)) ?? false;
+  const acceptableOrigin = isProd
+    ? origins.some((o) => req.headers.referer?.startsWith(o)) ?? false
+    : true;
   const browsingMode = parseBrowsingMode(req.cookies, session);
   const track = new Tracker(req, res);
   const cache: CacheSettings | null = {
