8f40eb3dfbf595283dae900a484e016a896897bf
Briant Diehl <bkdiehl@gmail.com>
2023-11-09 16:12:41 -0700

    fewer isClient state updates

--- src/hooks/useIsClient.ts
+++ src/hooks/useIsClient.ts
@@ -1,19 +1,3 @@
-import { useEffect, useState, useTransition } from 'react';
-
-/**
- * @see https://usehooks-ts.com/react-hook/use-is-client
- */
-function useIsClient() {
-  const [isClient, setClient] = useState(false);
-  const [, startTransition] = useTransition();
-
-  useEffect(() => {
-    startTransition(() => {
-      setClient(true);
-    });
-  }, []);
-
-  return isClient;
-}
+import { useIsClient } from '~/providers/IsClientProvider';
 
 export default useIsClient;
---
+++ src/providers/IsClientProvider.tsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const IsClientContext = createContext<boolean | null>(null);
+export const useIsClient = () => {
+  const context = useContext(IsClientContext);
+  if (context === null) throw new Error('missing IsClientContext');
+  return context;
+};
+export const IsClientProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isClient, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  return <IsClientContext.Provider value={isClient}>{children}</IsClientContext.Provider>;
+};
