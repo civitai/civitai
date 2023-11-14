a5b87c75bd978e0b95d6d2e97b1797af58574198
Briant Diehl <bkdiehl@gmail.com>
2023-11-14 15:08:25 -0700

    small fix to post details

--- src/components/Post/Detail/PostDetail.tsx
+++ src/components/Post/Detail/PostDetail.tsx
@@ -41,10 +41,11 @@ import { openContext } from '~/providers/CustomModalsProvider';
 import { TipBuzzButton } from '~/components/Buzz/TipBuzzButton';
 import { env } from '~/env/client.mjs';
 import { toStringList } from '~/utils/array-helpers';
+import { useBrowserRouter } from '~/components/BrowserRouter/BrowserRouterProvider';
 
 export function PostDetail({ postId }: { postId: number }) {
   const currentUser = useCurrentUser();
-  const router = useRouter();
+  const { query } = useBrowserRouter();
   const theme = useMantineTheme();
   const { data: post, isLoading: postLoading } = trpc.post.get.useQuery({ id: postId });
   const { images, isLoading: imagesLoading } = useQueryImages({ postId });
@@ -95,7 +96,7 @@ export function PostDetail({ postId }: { postId: number }) {
                   {post.title}
                 </Title>
               )}
-              {router.query.modal && (
+              {query.dialog && (
                 <NavigateBack url="/posts">
                   {({ onClick }) => <CloseButton onClick={onClick} size="lg" ml="auto" />}
                 </NavigateBack>
