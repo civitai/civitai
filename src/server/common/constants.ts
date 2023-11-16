c34236f8564942e59259517791dc6c767b85ad7b
Justin Maier <Zipp425@gmail.com>
2023-11-16 15:24:21 -0700

    Fix incorrect cache ttl

--- src/server/common/constants.ts
+++ src/server/common/constants.ts
@@ -469,12 +469,12 @@ export const STRIPE_PROCESSING_AWAIT_TIME = 20000; // 20s
 export const STRIPE_PROCESSING_CHECK_INTERVAL = 1000; // 1s
 
 export const CacheTTL = {
-  xs: 60 * 60 * 1,
-  sm: 60 * 60 * 3,
-  md: 60 * 60 * 10,
-  lg: 60 * 60 * 30,
-  hour: 60 * 60 * 1,
-  day: 60 * 60 * 24 * 1,
-  week: 60 * 60 * 24 * 7 * 1,
-  month: 60 * 60 * 24 * 30 * 1,
+  xs: 60 * 1,
+  sm: 60 * 3,
+  md: 60 * 10,
+  lg: 60 * 30,
+  hour: 60 * 60,
+  day: 60 * 60 * 24,
+  week: 60 * 60 * 24 * 7,
+  month: 60 * 60 * 24 * 30,
 } as const;
