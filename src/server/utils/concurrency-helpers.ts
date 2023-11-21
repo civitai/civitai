c970bc14cfd45e94cd2b823b70e1a7bc6c7fb972
Justin Maier <Zipp425@gmail.com>
2023-11-20 22:01:42 -0700

    Concurrency generator support

--- src/server/utils/concurrency-helpers.ts
+++ src/server/utils/concurrency-helpers.ts
@@ -1,38 +1,55 @@
-type Task = () => Promise<void>;
-export function limitConcurrency(tasks: Task[], limit: number): Promise<void> {
+export type Task = () => Promise<void>;
+type TaskGenerator = () => Task | null;
+
+function isTaskGenerator(arg: any): arg is TaskGenerator {
+  return typeof arg === 'function';
+}
+
+export function limitConcurrency(
+  tasksOrGenerator: Task[] | TaskGenerator,
+  limit: number
+): Promise<void> {
   return new Promise((resolve, reject) => {
     let active = 0;
-    let finished = 0;
+    let finished = false;
     let index = 0;
+    const isGenerator = isTaskGenerator(tasksOrGenerator);
+    const tasks = isGenerator ? [] : (tasksOrGenerator as Task[]);
+
+    const getNextTask = (): Task | null => {
+      if (isGenerator) return tasksOrGenerator();
+      else {
+        if (index < tasks.length) return tasks[index++];
+        return null;
+      }
+    };
 
     const checkFinished = () => {
-      if (finished === tasks.length) resolve();
+      if (finished && active === 0) resolve();
     };
 
     const run = async () => {
-      if (index === tasks.length) return;
-
-      const currentIndex = index;
-      index++;
+      const task = getNextTask();
+      if (!task) {
+        finished = true;
+        checkFinished();
+        return;
+      }
 
       active++;
       try {
-        await tasks[currentIndex]();
+        await task();
       } catch (error) {
         reject(error);
         return;
+      } finally {
+        active--;
+        checkFinished();
+        if (active < limit && !finished) run(); // Start a new task if we're below the concurrency limit
       }
-      active--;
-      finished++;
-
-      checkFinished();
-
-      if (active < limit) run(); // Start a new task if we're below the concurrency limit
     };
 
     // Start the initial set of tasks
-    for (let i = 0; i < Math.min(limit, tasks.length); i++) {
-      run();
-    }
+    for (let i = 0; i < limit; i++) run();
   });
 }
