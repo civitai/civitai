4013da2a32086ec5509ae6431b277ca91b4678bb
Justin Maier <Zipp425@gmail.com>
2023-11-22 17:51:46 -0700

    Add LCM base model

--- src/server/common/constants.ts
+++ src/server/common/constants.ts
@@ -56,6 +56,7 @@ export const constants = {
   baseModels: [
     'SD 1.4',
     'SD 1.5',
+    'SD 1.5 LCM',
     'SD 2.0',
     'SD 2.0 768',
     'SD 2.1',
@@ -63,6 +64,7 @@ export const constants = {
     'SD 2.1 Unclip',
     'SDXL 0.9',
     'SDXL 1.0',
+    'SDXL 1.0 LCM',
     'SDXL Distilled',
     'Other',
   ],
@@ -134,6 +136,7 @@ export const constants = {
     'DDIM',
     'PLMS',
     'UniPC',
+    'LCM',
   ],
   availableReactions: {
     [ReviewReactions.Like]: '👍',
@@ -242,9 +245,9 @@ export type BaseModel = (typeof constants.baseModels)[number];
 export const baseModelSetTypes = ['SD1', 'SD2', 'SDXL', 'SDXLDistilled'] as const;
 export type BaseModelSetType = (typeof baseModelSetTypes)[number];
 export const baseModelSets: Record<BaseModelSetType, BaseModel[]> = {
-  SD1: ['SD 1.4', 'SD 1.5'],
+  SD1: ['SD 1.4', 'SD 1.5', 'SD 1.5 LCM'],
   SD2: ['SD 2.0', 'SD 2.0 768', 'SD 2.1', 'SD 2.1 768', 'SD 2.1 Unclip'],
-  SDXL: ['SDXL 0.9', 'SDXL 1.0'],
+  SDXL: ['SDXL 0.9', 'SDXL 1.0', 'SDXL 1.0 LCM'],
   SDXLDistilled: ['SDXL Distilled'],
 };
 
@@ -270,6 +273,7 @@ export const baseLicenses: Record<string, LicenseDetails> = {
 export const baseModelLicenses: Record<BaseModel, LicenseDetails | undefined> = {
   'SD 1.4': baseLicenses['openrail'],
   'SD 1.5': baseLicenses['openrail'],
+  'SD 1.5 LCM': baseLicenses['openrail++'],
   'SD 2.0': baseLicenses['openrail'],
   'SD 2.0 768': baseLicenses['openrail'],
   'SD 2.1': baseLicenses['openrail'],
@@ -277,6 +281,7 @@ export const baseModelLicenses: Record<BaseModel, LicenseDetails | undefined> = 
   'SD 2.1 Unclip': baseLicenses['openrail'],
   'SDXL 0.9': baseLicenses['sdxl 0.9'],
   'SDXL 1.0': baseLicenses['openrail++'],
+  'SDXL 1.0 LCM': baseLicenses['openrail++'],
   'SDXL Distilled': baseLicenses['openrail++'],
   Other: undefined,
 };
@@ -307,6 +312,7 @@ export const samplerMap = new Map<Sampler, string[]>([
   ['DDIM', ['ddim']],
   ['PLMS', ['plms']],
   ['UniPC', ['uni_pc', 'uni_pc_bh2']],
+  ['LCM', ['lcm']],
 ]);
 
 export const samplerOffsets = {
@@ -318,6 +324,7 @@ export const samplerOffsets = {
   'DPM++ 2M Karras': 4,
   DPM2: 4,
   'DPM2 a': 4,
+  LCM: 0,
 } as const;
 
 export const generation = {
@@ -417,6 +424,9 @@ export const generationConfig = {
       baseModel: 'SDXL 1.0',
       strength: 1,
     } as Generation.Resource,
+  },
+  LCM: {
+    samplers: ['LCM', 'Euler A'],
   },
 };
 
