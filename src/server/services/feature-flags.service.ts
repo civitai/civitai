e49be3621673e6678050108355fbf7f58f2af19c
Luis Rojas <lrojas94@gmail.com>
2023-12-20 18:11:31 -0400

    Remove clubs from public

--- src/server/services/feature-flags.service.ts
+++ src/server/services/feature-flags.service.ts
@@ -78,8 +78,8 @@ const featureFlags = createFeatureFlags({
     description: `An improved user profile experience to boast around.`,
     availability: ['public'],
   },
-  clubs: ['public'],
-  createClubs: ['public'],
+  clubs: ['mod', 'granted'],
+  createClubs: ['mod', 'granted'],
 });
 export const featureFlagKeys = Object.keys(featureFlags);
 
