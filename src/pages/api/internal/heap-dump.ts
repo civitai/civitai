d3f4b250e09ce4240f10e3551623d091da9d4890
Koen <koen@linker.io>
2024-01-04 23:05:11 +0000

    Fixed heap dump endpoint

--- src/pages/api/internal/heap-dump.ts
+++ src/pages/api/internal/heap-dump.ts
@@ -1,13 +1,12 @@
-import { writeHeapSnapshot } from "node:v8";
-import { NextApiRequest, NextApiResponse } from "next";
-import { JobEndpoint } from "~/server/utils/endpoint-helpers";
-import { createReadStream } from "node:fs";
+import { writeHeapSnapshot } from 'node:v8';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { JobEndpoint } from '~/server/utils/endpoint-helpers';
+import { createReadStream } from 'node:fs';
 
-export default JobEndpoint (
-  async function getHeapDump(req: NextApiRequest, res: NextApiResponse) {
-    const path = writeHeapSnapshot();
-    
-    var readStream = createReadStream(path);
-    res.send(readStream);
-  }
-)
+export default JobEndpoint(async function getHeapDump(req: NextApiRequest, res: NextApiResponse) {
+  const localPath = `/tmp/heapdump-${Date.now()}.heapsnapshot`;
+  writeHeapSnapshot(localPath);
+
+  const readStream = createReadStream(localPath);
+  res.send(readStream);
+});
