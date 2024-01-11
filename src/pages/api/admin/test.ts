a4a6d39e60c7e2c6c392bdc2faa3c8978094e447
Briant Diehl <bkdiehl@gmail.com>
2024-01-10 17:09:09 -0700

    revert changes to test endpoint

--- src/pages/api/admin/test.ts
+++ src/pages/api/admin/test.ts
@@ -1,4 +1,3 @@
-import { ImageIngestionStatus } from '@prisma/client';
 import dayjs from 'dayjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { dbRead } from '~/server/db/client';
@@ -6,7 +5,6 @@ import { eventEngine } from '~/server/events';
 import ncmecCaller from '~/server/http/ncmec/ncmec.caller';
 import { getTopContributors } from '~/server/services/buzz.service';
 import { WebhookEndpoint } from '~/server/utils/endpoint-helpers';
-import { signalClient } from '~/utils/signal-client';
 
 export default WebhookEndpoint(async function (req: NextApiRequest, res: NextApiResponse) {
   // const teamAccounts = eventEngine.getTeamAccounts('holiday2023');
@@ -21,11 +19,6 @@ export default WebhookEndpoint(async function (req: NextApiRequest, res: NextApi
   //   entityId: 218322,
   //   userId: 969069,
   // });
-  signalClient.send({
-    target: 'image-ingestion:status',
-    data: { imageId: 175719, ingestion: ImageIngestionStatus.Blocked, blockedFor: 'being rude' },
-    userId: 5,
-  });
 
   return res.status(200).json({
     ok: true,
