20bc2365566a34a86da84a75862172915e1d8b74
Justin Maier <Zipp425@gmail.com>
2024-01-19 12:32:26 -0700

    Add reset time to generation error

--- src/server/services/generation/generation.service.ts
+++ src/server/services/generation/generation.service.ts
@@ -439,7 +439,7 @@ export const createGenerationRequest = async ({
   // Handle rate limiting
   if (await generationLimiter.hasExceededLimit(userId.toString()))
     throw throwRateLimitError(
-      `We've noticed an unusual amount of generation from your account. Contact support@civitai.com or come back later.`
+      `We've noticed an unusual amount of generation from your account. Contact support@civitai.com or come back at 12am UTC.`
     );
 
   if (!resources || resources.length === 0) throw throwBadRequestError('No resources provided');
