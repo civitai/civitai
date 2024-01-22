f182a702d6553d022772356db4be0497a55ad732
Briant Diehl <bkdiehl@gmail.com>
2024-01-22 14:51:44 -0700

    add meta tag for potential ad provider

--- src/components/Meta/MetaPWA.tsx
+++ src/components/Meta/MetaPWA.tsx
@@ -4,6 +4,8 @@ export function MetaPWA() {
       <meta name="viewport" content="maximum-scale=1, initial-scale=1, width=device-width" />
       <link rel="manifest" href="/site.webmanifest" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
+      {/* exoclick ad meta tag */}
+      <meta name="6a97888e-site-verification" content="f517e489c958cfe10876b8f7daa294c2"></meta>
       <link
         rel="apple-touch-startup-image"
         href="/images/splash/apple-splash-2048-2732.jpg"
