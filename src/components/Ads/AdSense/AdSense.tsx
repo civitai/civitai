835da85699444973c6e16de9f3da7f4c71170092
Briant Diehl <bkdiehl@gmail.com>
2024-01-29 14:24:31 -0700

    fix type issues

--- src/components/Ads/AdSense/AdSense.tsx
+++ src/components/Ads/AdSense/AdSense.tsx
@@ -13,31 +13,29 @@ const debouncer = createDebouncer(50);
 const emitter = new EventEmitter<{ serve: undefined }>();
 emitter.on('serve', () =>
   debouncer(() => {
-    (window.AdProvider = window.AdProvider || []).push({ serve: {} });
+    (window.adsbygoogle = window.adsbygoogle || []).push({ serve: {} });
   })
 );
 
 export function AdSenseAd() {
-  const { adSenseReady } = useAdsContext();
-  const hasRunRef = useRef(false);
-  useEffect(() => {
-    if (adSenseReady && !hasRunRef.current) {
-      hasRunRef.current = true;
-      emitter.emit('serve', undefined);
-    }
-  }, [adSenseReady]);
+  // const { adSenseReady } = useAdsContext();
+  // const hasRunRef = useRef(false);
+  // useEffect(() => {
+  //   if (adSenseReady && !hasRunRef.current) {
+  //     hasRunRef.current = true;
+  //     emitter.emit('serve', undefined);
+  //   }
+  // }, [adSenseReady]);
 
   return (
-    <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-6320044818993728"
-        data-ad-slot="2186801716"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-        data-adtest="on"
-      ></ins>
-    </>
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-6320044818993728"
+      data-ad-slot="2186801716"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+      data-adtest="on"
+    ></ins>
   );
 }
