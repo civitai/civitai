7cb52f86f61f2baba5efb6d3208dffa9def1a1ab
Justin Maier <Zipp425@gmail.com>
2024-02-16 15:44:49 -0700

    Add Builds menu item

--- src/components/HomeContentToggle/HomeContentToggle.tsx
+++ src/components/HomeContentToggle/HomeContentToggle.tsx
@@ -9,12 +9,14 @@ import {
   Text,
   ThemeIcon,
   createStyles,
+  Badge,
 } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 import {
   IconCalendar,
   IconCategory,
   IconClubs,
+  IconCpu,
   IconFileText,
   IconHome,
   IconLayoutList,
@@ -29,7 +31,12 @@ import { useFeatureFlags } from '~/providers/FeatureFlagsProvider';
 import { containerQuery } from '~/utils/mantine-css-helpers';
 import { getDisplayName } from '~/utils/string-helpers';
 
-const homeOptions = {
+type HomeOption = {
+  url: string;
+  icon: (props: TablerIconsProps) => JSX.Element;
+  highlight?: boolean;
+};
+const homeOptions: Record<string, HomeOption> = {
   home: {
     url: '/',
     icon: (props: TablerIconsProps) => <IconHome {...props} />,
@@ -66,7 +73,12 @@ const homeOptions = {
     url: '/clubs',
     icon: (props: TablerIconsProps) => <IconClubs {...props} />,
   },
-} as const;
+  builds: {
+    url: '/builds',
+    icon: (props: TablerIconsProps) => <IconCpu {...props} />,
+    highlight: true,
+  },
+};
 type HomeOptions = keyof typeof homeOptions;
 
 const useStyles = createStyles<string, { hideActive?: boolean }>((_, params) => ({
@@ -146,6 +158,11 @@ export function HomeContentToggle({ size, sx, ...props }: Props) {
             <Text size="sm" transform="capitalize" inline>
               {key}
             </Text>
+            {value.highlight && (
+              <Badge color="yellow" variant="filled" size="sm" radius="xl">
+                New
+              </Badge>
+            )}
           </Group>
         </Anchor>
       </Link>
@@ -229,10 +246,21 @@ export function HomeTabs({ sx, ...tabProps }: HomeTabProps) {
     .map(([key, value]) => (
       <Link key={key} href={value.url} passHref>
         <Anchor variant="text" onClick={() => set(key as HomeOptions)}>
-          <Tabs.Tab value={key} icon={value.icon({ size: 16 })}>
-            <Text className={classes.tabLabel} inline>
-              {getDisplayName(key)}
-            </Text>
+          <Tabs.Tab
+            value={key}
+            icon={value.icon({ size: 16 })}
+            pr={value.highlight ? 10 : undefined}
+          >
+            <Group spacing={4}>
+              <Text className={classes.tabLabel} inline>
+                {getDisplayName(key)}
+              </Text>
+              {value.highlight && (
+                <Badge color="yellow" variant="filled" size="sm" radius="xl">
+                  New
+                </Badge>
+              )}
+            </Group>
           </Tabs.Tab>
         </Anchor>
       </Link>
