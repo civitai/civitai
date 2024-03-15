828e1107a480067a29e5b77d0a5a2b789c2fa430
Luis Rojas <lrojas94@gmail.com>
2024-03-15 15:03:47 -0400

    Add new Civitai Vault landing page

--- src/pages/product/vault.tsx
+++ src/pages/product/vault.tsx
@@ -1,44 +1,24 @@
 import {
-  Card,
   Container,
   Title,
   Text,
   Button,
   Stack,
-  Center,
-  Loader,
-  Alert,
-  Tabs,
-  List,
-  ThemeIcon,
   Group,
   createStyles,
-  Flex,
+  Image,
+  Box,
+  ThemeIcon,
 } from '@mantine/core';
-import { trpc } from '~/utils/trpc';
-import { createServerSideProps } from '~/server/utils/server-side-helpers';
-import { SubscribeButton } from '~/components/Stripe/SubscribeButton';
-import { PlanCard } from '~/components/Stripe/PlanCard';
-import { ManageSubscriptionButton } from '~/components/Stripe/ManageSubscriptionButton';
-import {
-  IconCalendarDue,
-  IconCircleCheck,
-  IconExclamationMark,
-  IconHeartHandshake,
-} from '@tabler/icons-react';
-import { DonateButton } from '~/components/Stripe/DonateButton';
-import { EdgeMedia } from '~/components/EdgeMedia/EdgeMedia';
-import { PlanBenefitList } from '~/components/Stripe/PlanBenefitList';
-import { joinRedirectReasons, JoinRedirectReason } from '~/utils/join-helpers';
 import { useRouter } from 'next/router';
-import { getFeatureFlags } from '~/server/services/feature-flags.service';
-import { ContainerGrid } from '~/components/ContainerGrid/ContainerGrid';
 import { containerQuery } from '~/utils/mantine-css-helpers';
 import { Meta } from '~/components/Meta/Meta';
 import { NextLink } from '@mantine/next';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
+import { IconCloudPlus, IconDownload, IconMapSearch } from '@tabler/icons-react';
+import { useIsMobile } from '~/hooks/useIsMobile';
 
 export default function CivitaiVault() {
-  const router = useRouter();
   const { classes, cx } = useStyles();
   const currentUser = useCurrentUser();
   const isMember = currentUser?.isMember;
@@ -46,6 +26,7 @@ export default function CivitaiVault() {
     text: isMember ? 'Go to my Vault' : 'Become a member',
     href: isMember ? '/user/vault' : '/pricing',
   };
+  const isMobile = useIsMobile();
 
   return (
     <>
@@ -54,13 +35,11 @@ export default function CivitaiVault() {
         description="Civitai Vault is a place to store your favorite models forever. Even if a model is removed from the site, you can still access it here."
       />
       <Container size="lg" mb="lg">
-        <Stack>
-          <Group>
-            <Stack spacing={4}>
-              <Title align="center" className={classes.title}>
-                Civitai Vault
-              </Title>
-              <Text align="center" className={classes.introText} sx={{ lineHeight: 1.25 }}>
+        <Stack spacing={40}>
+          <Group position="apart">
+            <Stack spacing={12}>
+              <Title className={classes.heroTitle}>Civitai Vault</Title>
+              <Text className={classes.heroText} sx={{ lineHeight: 1.25 }}>
                 ❤️ Civitai Vault is only available to members
               </Text>
             </Stack>
@@ -69,14 +48,85 @@ export default function CivitaiVault() {
               color="blue"
               size="lg"
               radius="xl"
-              fullWidth
               component={NextLink}
               href={buttonData.href}
               rel="nofollow noreferrer"
+              fullWidth={isMobile}
             >
               {buttonData.text}
-            </Button>{' '}
+            </Button>
           </Group>
+          <Box className={classes.gradientContainer}>
+            <Image
+              src="/images/product/vault/lp-main.png"
+              alt="check out the vault"
+              width="100%"
+              height="auto"
+            />
+            <Box className={classes.gradientBox} />
+          </Box>
+          <Stack spacing={12}>
+            <Title className={classes.heading3} order={3}>
+              Save models. Download anytime. Forever
+            </Title>
+            <Text>
+              Vault is a place for all your models. You can download them even after the creator
+              deleted them from Civitai
+            </Text>
+          </Stack>
+          <Stack spacing={60}>
+            <Group noWrap>
+              <ThemeIcon size={72} variant="light" color="green" radius={1000}>
+                <IconCloudPlus size={40} />
+              </ThemeIcon>
+
+              <Stack spacing={0}>
+                <Title className={classes.heading4} order={4}>
+                  Save Models
+                </Title>
+                <Text>
+                  Save any models you like to your vault. You get different vault sizes based on
+                  your membership tier.
+                </Text>
+              </Stack>
+            </Group>
+            <Group noWrap>
+              <ThemeIcon size={72} variant="light" color="blue" radius={1000}>
+                <IconMapSearch size={40} />
+              </ThemeIcon>
+
+              <Stack spacing={0}>
+                <Title className={classes.heading4} order={4}>
+                  Search, filter, add notes
+                </Title>
+                <Text>Have lots of models? Easily find them with searchm, filters, and notes!</Text>
+              </Stack>
+            </Group>
+            <Group noWrap>
+              <ThemeIcon size={72} variant="light" color="green" radius={1000}>
+                <IconDownload size={40} />
+              </ThemeIcon>
+
+              <Stack spacing={0}>
+                <Title className={classes.heading4} order={4}>
+                  Download when you need
+                </Title>
+                <Text>Whenever you need!</Text>
+              </Stack>
+            </Group>
+          </Stack>
+          <Button
+            variant="filled"
+            color="blue"
+            size="lg"
+            radius="xl"
+            component={NextLink}
+            href={buttonData.href}
+            rel="nofollow noreferrer"
+            fullWidth
+          >
+            {buttonData.text}
+          </Button>
         </Stack>
       </Container>
     </>
@@ -86,7 +136,7 @@ export default function CivitaiVault() {
 const useStyles = createStyles((theme) => ({
   cta: {
     height: 52,
-  }
+  },
   heroTitle: {
     fontSize: 32,
     fontWeight: 700,
@@ -99,21 +149,17 @@ const useStyles = createStyles((theme) => ({
     fontSize: 14,
     fontWeight: 500,
   },
-  heading: {
-    fontSize: 24,
+  heading3: {
+    fontSize: 32,
     fontWeight: 700,
     color: theme.colorScheme === 'dark' ? '#fff' : theme.colors.gray[9],
   },
-  copy: {
-    fontSize: 16,
-    fontWeight: 500,
+  heading4: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: theme.colorScheme === 'dark' ? '#fff' : theme.colors.gray[9],
   },
-  videoBorder: {
-    borderRadius: 12,
-    width: '100%',
-    border: '1px solid #2D2E32',
-    borderBottom: 'none',
-  },
+
   gradientContainer: {
     position: 'relative',
     marginBottom: 24,
