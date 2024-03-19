8131c86d1b5bfa9b2210b3333cbcd5d6d43234ac
manuelurenah <manuel.ureh@hotmail.com>
2024-03-19 10:12:54 -0400

    Fixes type issues for mod endpoint

--- src/pages/api/mod/remove-images.ts
+++ src/pages/api/mod/remove-images.ts
@@ -1,24 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
-import { moderateImages } from '~/server/services/user.service';
-import { WebhookEndpoint } from '~/server/utils/endpoint-helpers';
+import { moderateImages } from '~/server/services/image.service';
+import { WebhookEndpoint, handleEndpointError } from '~/server/utils/endpoint-helpers';
+import { numericStringArray } from '~/utils/zod-helpers';
 
 const schema = z.object({
-  imageIds: z.coerce.number().array().optional(),
+  imageIds: numericStringArray(),
 });
 
 export default WebhookEndpoint(async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
-  const { imageIds } = schema.parse(req.body);
-  
-  await moderateImages({
-    ids: imageIds,
-    needsReview: false,
-    reviewAction: 'delete',
-    reviewType: 'blocked'
-  });
+  try {
+    if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
+    const { imageIds } = schema.parse(req.body);
 
-  return res.status(200).json({
-    images: imageIds.length,
-  });
+    await moderateImages({
+      ids: imageIds,
+      needsReview: undefined,
+      reviewAction: 'delete',
+      reviewType: 'blocked',
+    });
+
+    return res.status(200).json({
+      images: imageIds.length,
+    });
+  } catch (e) {
+    return handleEndpointError(res, e);
+  }
 });
