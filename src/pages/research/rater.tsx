c3275ecdaf3306056a14a8e7baafd5e0d50052da
Justin Maier <Zipp425@gmail.com>
2024-03-23 20:19:09 -0600

    Adjust design of rater

--- src/pages/research/rater.tsx
+++ src/pages/research/rater.tsx
@@ -16,6 +16,7 @@ import {
   ActionIcon,
   Tooltip,
   Popover,
+  createStyles,
 } from '@mantine/core';
 import { useListState, useLocalStorage } from '@mantine/hooks';
 import { useEffect, useState } from 'react';
@@ -30,11 +31,10 @@ import {
   IconArrowBackUp,
   IconVolume,
   IconVolumeOff,
-  IconSword,
+  IconFlag,
   IconStarFilled,
   IconHelpHexagon,
 } from '@tabler/icons-react';
-import { InfoPopover } from '~/components/InfoPopover/InfoPopover';
 
 const NsfwLevel = {
   PG: 1,
@@ -48,14 +48,14 @@ const NsfwNumMap = Object.fromEntries(
   Object.entries(NsfwLevel).map(([key, value]) => [value, key])
 );
 
-const keyMap = {
-  PG: '1',
-  PG13: '2',
-  R: '3',
-  X: '4',
-  XXX: '5',
-  Blocked: '6',
-} as const;
+const explanationMap: Record<string, string> = {
+  PG: 'Safe for all ages',
+  PG13: 'Revealing clothing, violence, and light gore',
+  R: 'Adult themes and situations, partial nudity, graphic violence and death',
+  X: 'Graphic nudity, adult objects and settings',
+  XXX: 'Sexual content and activity',
+  Blocked: 'Violates our terms of service',
+};
 
 const LEVEL_BASE_RATINGS = 8; // Base number of ratings needed for the first level
 const LEVEL_GROWTH_RATE = 1.15; // Growth rate for the exponential curve
@@ -88,6 +88,8 @@ const levelSound = '/sounds/level-up.mp3';
 const undoSound = '/sounds/undo.mp3';
 
 export default function Rater() {
+  const { classes } = useStyles();
+
   // Keep track of rater state
   const [muted, setMuted] = useLocalStorage<boolean>({ key: 'rater-muted', defaultValue: false });
   const [isLevelingUp, setIsLevelingUp] = useState(false);
@@ -230,7 +232,7 @@ export default function Rater() {
   }, [image]);
 
   return (
-    <Container size="xl">
+    <Box className={classes.container}>
       {isLevelingUp && (
         <Card
           p="md"
@@ -260,184 +262,218 @@ export default function Rater() {
           </Text>
         </Card>
       )}
-      <Stack spacing="md" w="100%" align="center">
-        <Group align="flex-end" w="100%">
-          <Title order={1} lh={1}>
-            Rater
-          </Title>
-          {progression && (
-            <HoverCard withArrow>
-              <HoverCard.Target>
-                <Badge
-                  size="lg"
-                  sx={(theme) => ({
-                    position: 'relative',
-                    cursor: 'default',
-                    border: `1px solid ${theme.colors.blue[5]}`,
-                    paddingLeft: 5,
-                    backgroundColor:
-                      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[0],
-                    div: {
-                      position: 'absolute',
-                      top: 0,
-                      left: 0,
-                      bottom: 0,
-                      backgroundColor: theme.colors.blue[5],
-                      opacity: 0.3,
-                    },
-                    span: {
-                      display: 'flex',
-                      alignItems: 'center',
-                    },
-                    svg: {
-                      marginRight: 5,
-                    },
-                  })}
-                >
-                  <IconStarFilled strokeWidth={2.5} size={15} />
-                  Level {progression.level + 1}
-                  <Box style={{ width: progression.progress + '%' }} />
-                </Badge>
-              </HoverCard.Target>
-              <HoverCard.Dropdown px="xs" py={3} color="gray">
-                <Stack spacing={0}>
-                  <Group spacing={4}>
-                    <Text size="xs" color="blue.4" weight="bold" tt="uppercase">
-                      next level
-                    </Text>
-                    <Text size="xs" weight={500}>
-                      {progression.ratingsInLevel} / {progression.ratingsForNextLevel}
-                    </Text>
-                  </Group>
-                  <Group spacing={4}>
-                    <Text size="xs" color="blue.4" weight="bold" tt="uppercase">
-                      Total ratings
-                    </Text>
-                    <Text size="xs" weight={500}>
-                      {status?.count ?? 0}
-                    </Text>
-                  </Group>
-                </Stack>
-              </HoverCard.Dropdown>
-            </HoverCard>
-          )}
-          <Group ml="auto" spacing={4}>
-            <Text weight="bold" size="xs">
-              Show:{' '}
-            </Text>
-            {Object.entries(NsfwLevel)
-              .filter(([key]) => key !== 'Blocked')
-              .map(([key, value]) => (
-                <Chip
-                  size="xs"
-                  radius="xs"
-                  checked={(level & value) !== 0}
-                  onChange={() => changeLevel(value)}
-                  key={key}
-                >
-                  {key}
-                </Chip>
-              ))}
-          </Group>
-        </Group>
-        <Box
-          sx={(theme) => ({
-            height: 600,
-            display: 'flex',
-            position: 'relative',
-            img: {
-              height: 'auto',
-              width: 'auto',
-              maxHeight: '100%',
-              maxWidth: '100%',
-              objectFit: 'contain',
-              top: '50%',
-              left: '50%',
-              borderRadius: theme.radius.sm,
-              boxShadow: theme.shadows.md,
-            },
-          })}
+      <Box className={classes.image}>
+        {image ? (
+          <EdgeMedia src={image?.url} width={700} />
+        ) : (
+          <Skeleton height={600} width={600}></Skeleton>
+        )}
+        <Card
+          id="rating"
+          withBorder
+          shadow="sm"
+          radius="sm"
+          className={classes.levelNotice}
+          style={{ display: 'none' }}
         >
-          {image ? (
-            <EdgeMedia src={image?.url} width={700} />
-          ) : (
-            <Skeleton height={600} width={600}></Skeleton>
-          )}
-          <Card
-            id="rating"
-            withBorder
-            shadow="sm"
-            radius="sm"
-            sx={(theme) => ({
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%,-50%)',
-              zIndex: 10,
-              fontSize: 42,
-              opacity: 0.9,
-              minWidth: 50,
-              padding: '2px 10px 6px !important',
-              lineHeight: 1,
-              textAlign: 'center',
-              fontWeight: 500,
-            })}
-            style={{ display: 'none' }}
-          >
-            PG
-          </Card>
-        </Box>
-
-        <Stack spacing="xs" align="center">
-          <Group spacing={4}>
-            <Tooltip label="Undo" position="top" withArrow>
-              <Button onClick={undoRating} variant="default" px="xs" disabled={!prevImages.length}>
-                <IconArrowBackUp />
-              </Button>
-            </Tooltip>
-            <Button.Group>
-              {Object.entries(NsfwLevel).map(([key, value]) => (
+          PG
+        </Card>
+      </Box>
+      <Group align="flex-end" className={classes.rater}>
+        <Title order={1} lh={1}>
+          Rater
+        </Title>
+        {progression && (
+          <HoverCard withArrow>
+            <HoverCard.Target>
+              <Badge size="lg" className={classes.raterBadge}>
+                <IconStarFilled strokeWidth={2.5} size={15} />
+                Level {progression.level + 1}
+                <Box style={{ width: progression.progress + '%' }} />
+              </Badge>
+            </HoverCard.Target>
+            <HoverCard.Dropdown px="xs" py={3} color="gray">
+              <Stack spacing={0}>
+                <Group spacing={4}>
+                  <Text size="xs" color="blue.4" weight="bold" tt="uppercase">
+                    next level
+                  </Text>
+                  <Text size="xs" weight={500}>
+                    {progression.ratingsInLevel} / {progression.ratingsForNextLevel}
+                  </Text>
+                </Group>
+                <Group spacing={4}>
+                  <Text size="xs" color="blue.4" weight="bold" tt="uppercase">
+                    Total ratings
+                  </Text>
+                  <Text size="xs" weight={500}>
+                    {status?.count ?? 0}
+                  </Text>
+                </Group>
+              </Stack>
+            </HoverCard.Dropdown>
+          </HoverCard>
+        )}
+      </Group>
+      <Group ml="auto" spacing={4} className={classes.browsing}>
+        <Text weight="bold" size="xs">
+          Show:{' '}
+        </Text>
+        {Object.entries(NsfwLevel)
+          .filter(([key]) => key !== 'Blocked')
+          .map(([key, value]) => (
+            <Chip
+              size="xs"
+              radius="xs"
+              checked={(level & value) !== 0}
+              onChange={() => changeLevel(value)}
+              key={key}
+            >
+              {key}
+            </Chip>
+          ))}
+      </Group>
+      <Stack spacing="xs" align="center" className={classes.actionBar}>
+        <Group spacing={4}>
+          <Tooltip label="Undo" position="top" withArrow>
+            <Button onClick={undoRating} variant="default" px="xs" disabled={!prevImages.length}>
+              <IconArrowBackUp />
+            </Button>
+          </Tooltip>
+          <Button.Group>
+            {Object.entries(NsfwLevel).map(([key, value]) => (
+              <Tooltip
+                key={key}
+                label={explanationMap[key]}
+                position="top"
+                withArrow
+                openDelay={1000}
+                maw={300}
+                multiline
+              >
                 <Button
                   key={key}
                   variant={(image?.nsfwLevel & value) !== 0 ? 'filled' : 'default'}
                   onClick={() => handleSetLevel(value)}
                 >
-                  {key}
+                  {key === 'Blocked' ? <IconFlag size={18} /> : key}
                 </Button>
-              ))}
-            </Button.Group>
-            <Button onClick={skipImage} variant="default">
-              Skip
-            </Button>
-          </Group>
-          <Group w="100%" spacing={5}>
-            <Text size="xs" mr="auto">
-              <Kbd>1-6</Kbd> to rate, <Kbd>Space</Kbd> to skip, <Kbd>Ctrl</Kbd>+<Kbd>Z</Kbd> to undo
-            </Text>
-            <ActionIcon size="xs" onClick={() => setMuted((x) => !x)} color="dark">
-              {muted ? <IconVolumeOff strokeWidth={2.5} /> : <IconVolume strokeWidth={2.5} />}
-            </ActionIcon>
-            <Popover width={300} withArrow>
-              <Popover.Target>
-                <ActionIcon size="xs" color="dark">
-                  <IconHelpHexagon strokeWidth={2.5} />
-                </ActionIcon>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <Text color="orange" weight={500}>
-                  What is this?
-                </Text>
-                <Text
-                  size="sm"
-                  lh={1.3}
-                >{`We're working on improving our automated content moderation system. We need your help to improve our data! Please assign the rating you think best fits the content`}</Text>
-              </Popover.Dropdown>
-            </Popover>
-          </Group>
-        </Stack>
+              </Tooltip>
+            ))}
+          </Button.Group>
+          <Button onClick={skipImage} variant="default">
+            Skip
+          </Button>
+        </Group>
+        <Group w="100%" spacing={5}>
+          <Text size="xs" mr="auto">
+            <Kbd>1-6</Kbd> to rate, <Kbd>Space</Kbd> to skip, <Kbd>Ctrl</Kbd>+<Kbd>Z</Kbd> to undo
+          </Text>
+          <ActionIcon size="xs" onClick={() => setMuted((x) => !x)} color="dark">
+            {muted ? <IconVolumeOff strokeWidth={2.5} /> : <IconVolume strokeWidth={2.5} />}
+          </ActionIcon>
+          <Popover width={300} withArrow>
+            <Popover.Target>
+              <ActionIcon size="xs" color="dark">
+                <IconHelpHexagon strokeWidth={2.5} />
+              </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown>
+              <Text color="orange" weight={500}>
+                What is this?
+              </Text>
+              <Text
+                size="sm"
+                lh={1.3}
+              >{`We're working on improving our automated content moderation system. We need your help to improve our data! Please assign the rating you think best fits the content`}</Text>
+            </Popover.Dropdown>
+          </Popover>
+        </Group>
       </Stack>
-    </Container>
+    </Box>
   );
-
-  return <div>Rater</div>;
 }
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    position: 'relative',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  rater: {
+    position: 'absolute',
+    top: theme.spacing.xs,
+    left: theme.spacing.xs,
+    zIndex: 10,
+  },
+  raterBadge: {
+    position: 'relative',
+    cursor: 'default',
+    border: `1px solid ${theme.colors.blue[5]}`,
+    paddingLeft: 5,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[0],
+    div: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      backgroundColor: theme.colors.blue[5],
+      opacity: 0.3,
+    },
+    span: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    svg: {
+      marginRight: 5,
+    },
+  },
+  browsing: {
+    position: 'absolute',
+    top: theme.spacing.xs,
+    right: theme.spacing.xs,
+    zIndex: 10,
+  },
+  image: {
+    height: 'calc(100% - 110px)',
+    marginTop: theme.spacing.md,
+    display: 'flex',
+    zIndex: 1,
+    img: {
+      height: 'auto',
+      width: 'auto',
+      maxHeight: '100%',
+      maxWidth: '100%',
+      objectFit: 'contain',
+      top: '50%',
+      left: '50%',
+      borderRadius: theme.radius.md,
+      boxShadow: theme.shadows.md,
+    },
+  },
+  levelNotice: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    zIndex: 10,
+    fontSize: 42,
+    opacity: 0.9,
+    minWidth: 50,
+    padding: '2px 10px 6px !important',
+    lineHeight: 1,
+    textAlign: 'center',
+    fontWeight: 500,
+  },
+  actionBar: {
+    position: 'absolute',
+    bottom: theme.spacing.xs,
+    left: '50%',
+    transform: 'translateX(-50%)',
+    zIndex: 10,
+  },
+}));
+
+setPageOptions(Rater, { withScrollArea: false });
