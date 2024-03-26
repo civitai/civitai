dde7fcca2d008c52108992a8fc2c527d8dd27018
Luis Rojas <lrojas94@gmail.com>
2024-03-26 15:28:52 -0400

    Minor improvements

--- src/pages/user/membership.tsx
+++ src/pages/user/membership.tsx
@@ -162,6 +162,7 @@ export default function UserMembership() {
                                   component: CancelMembershipFeedbackModal,
                                 });
                               }}
+                              closeMenuOnClick={true}
                             >
                               Cancel Membership
                             </Menu.Item>
