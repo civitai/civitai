006eb4c32f7014def8da5962a03fd534dbe810cd
Justin Maier <Zipp425@gmail.com>
2024-04-10 13:10:12 -0600

    Add token invalidation on logout

--- src/server/jobs/cache-cleanup.ts
+++ src/server/jobs/cache-cleanup.ts
@@ -5,7 +5,7 @@ import { CacheTTL } from '~/server/common/constants';
 export const cacheCleanup = createJob('cache-cleanup', '0 */1 * * *', async () => {
   // Clean rate limit keys
   const limitKeys = await redis.sMembers('trpc:limit:keys');
-  const limitCutoff = Date.now() - CacheTTL.day * 1000; // 24 hours
+  const limitCutoff = Date.now() - CacheTTL.day * 1000;
   for (const limitKey of limitKeys) {
     const keys = await redis.hGetAll(limitKey);
     const toRemove = new Set<string>();
@@ -15,4 +15,13 @@ export const cacheCleanup = createJob('cache-cleanup', '0 */1 * * *', async () =
     }
     if (toRemove.size > 0) await redis.hDel(limitKey, [...toRemove]);
   }
+
+  // Clean invalid token ids
+  const invalidTokenIds = await redis.hGetAll('session:invalid-tokens');
+  const toRemove = new Set<string>();
+  const tokenCutoff = Date.now() - CacheTTL.month * 1000;
+  for (const [key, value] of Object.entries(invalidTokenIds)) {
+    if (Number(value) < tokenCutoff) toRemove.add(key);
+  }
+  if (toRemove.size > 0) await redis.hDel('session:invalid-tokens', [...toRemove]);
 });
