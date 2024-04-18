e174d942bc075e6a6c494f3e3d17e15a0b2760e5
Brett Woodward <flipperbw@gmail.com>
2024-04-18 09:56:58 -0400

    removing unnecessary string cast

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -169,7 +169,7 @@ export const getServerSideProps = createServerSideProps({
 
       return {
         redirect: {
-          destination: destination.toString(),
+          destination,
           permanent: false,
         },
       };
