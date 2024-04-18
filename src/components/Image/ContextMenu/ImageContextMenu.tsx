6878e50e37a5c0c1b4cd24dae22ef7e22c25a872
manuelurenah <manuel.ureh@hotmail.com>
2024-04-18 10:43:10 -0400

    Fixes reportCsamImages endpoint

--- src/components/Image/ContextMenu/ImageContextMenu.tsx
+++ src/components/Image/ContextMenu/ImageContextMenu.tsx
@@ -159,7 +159,7 @@ function ImageMenuItems(
     if (!_userId) return;
     if (features.csamReports)
       window.open(`/moderator/csam/${_userId}?imageId=${imageId}`, '_blank');
-    else reportCsamMutation.mutate([imageId]);
+    else reportCsamMutation.mutate({ imageIds: [imageId] });
   };
 
   const { additionalMenuItemsAfter, additionalMenuItemsBefore } = useImageContextMenuContext();
