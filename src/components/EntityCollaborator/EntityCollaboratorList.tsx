b04e2b540f9761bfe2ba28b9d8bd8865ad7fc61e
Luis Rojas <lrojas94@gmail.com>
2024-06-19 19:30:51 -0400

    Add messaging for approved/rejecetd collabs

--- src/components/EntityCollaborator/EntityCollaboratorList.tsx
+++ src/components/EntityCollaborator/EntityCollaboratorList.tsx
@@ -1,4 +1,4 @@
-import { Center, Group, Loader, Stack, Button } from '@mantine/core';
+import { Center, Group, Loader, Stack, Button, Text } from '@mantine/core';
 import { EntityCollaboratorStatus, EntityType } from '@prisma/client';
 import { CreatorCardV2, CreatorCardPropsV2 } from '~/components/CreatorCard/CreatorCard';
 import {
@@ -49,6 +49,13 @@ export const EntityCollaboratorList = ({
 
         return (
           <Stack key={collaborator.user.id} spacing={0}>
+            {collaborator.status !== EntityCollaboratorStatus.Pending && isOwnerOrMod && (
+              <Text size="xs" color="dimmed">
+                {collaborator.status === EntityCollaboratorStatus.Approved
+                  ? 'User Approved Collaboration'
+                  : 'User Rejected Collaboration'}
+              </Text>
+            )}
             <CreatorCardV2 user={collaborator.user} withActions={false} {...creatorCardProps} />
             {showInvitionActions && (
               <Button.Group w="100%">
@@ -85,21 +92,23 @@ export const EntityCollaboratorList = ({
               </Button.Group>
             )}
             {isOwnerOrMod && (
-              <Button
-                onClick={() => {
-                  removeEntityCollaborator({
-                    entityId,
-                    entityType,
-                    targetUserId: collaborator.user.id,
-                  });
-                }}
-                loading={removingEntityCollaborator}
-                compact
-                color="red"
-                mt="xs"
-              >
-                Remove collaborator
-              </Button>
+              <Stack spacing={0}>
+                <Button
+                  onClick={() => {
+                    removeEntityCollaborator({
+                      entityId,
+                      entityType,
+                      targetUserId: collaborator.user.id,
+                    });
+                  }}
+                  loading={removingEntityCollaborator}
+                  compact
+                  color="red"
+                  mt="xs"
+                >
+                  Remove collaborator
+                </Button>
+              </Stack>
             )}
           </Stack>
         );
