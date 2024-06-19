42f380e6f352fe7deef76cf698f1b6655ebfe694
Justin Maier <Zipp425@gmail.com>
2024-06-19 13:03:49 -0600

    Adjust first daily post rewards

--- src/server/controllers/post.controller.ts
+++ src/server/controllers/post.controller.ts
@@ -35,11 +35,7 @@ import {
 } from '~/server/utils/errorHandling';
 import { Context } from '~/server/createContext';
 import { dbRead, dbWrite } from '../db/client';
-import {
-  firstDailyPostReward,
-  firstDailyPostNsfwReward,
-  imagePostedToModelReward,
-} from '~/server/rewards';
+import { firstDailyPostReward, imagePostedToModelReward } from '~/server/rewards';
 import { eventEngine } from '~/server/events';
 import dayjs from 'dayjs';
 import { hasEntityAccess } from '../services/common.service';
@@ -98,10 +94,7 @@ export const createPostHandler = async ({
     });
 
     if (isPublished && !isScheduled) {
-      const reward = Flags.intersects(post.nsfwLevel, sfwBrowsingLevelsFlag)
-        ? firstDailyPostReward
-        : firstDailyPostNsfwReward;
-      await reward.apply(
+      await firstDailyPostReward.apply(
         {
           postId: post.id,
           posterId: post.user.id,
@@ -311,10 +304,7 @@ export const updatePostHandler = async ({
       }
 
       // Give reward for first post of the day
-      const reward = Flags.intersects(updatedPost.nsfwLevel, sfwBrowsingLevelsFlag)
-        ? firstDailyPostReward
-        : firstDailyPostNsfwReward;
-      await reward.apply(
+      await firstDailyPostReward.apply(
         {
           postId: updatedPost.id,
           posterId: updatedPost.userId,
