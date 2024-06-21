3408f28fb9dc1d71474565e4c024e95ed7cbfc78
Justin Maier <Zipp425@gmail.com>
2024-06-21 17:52:19 -0600

    Skip edge cache for announcements

--- src/server/controllers/home-block.controller.ts
+++ src/server/controllers/home-block.controller.ts
@@ -99,7 +99,7 @@ export const getHomeBlocksByIdHandler = async ({
       ...input,
       user: ctx.user,
     });
-    if (homeBlock?.type === 'Announcement') ctx.cache.canCache = false;
+    if (homeBlock?.type === 'Announcement') ctx.cache.skip = true;
 
     if (!homeBlock) {
       throw throwNotFoundError('Home block not found');
