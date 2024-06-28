80b121661da88811694d9d7a01a3c3c972821c5a
Briant Diehl <bkdiehl@gmail.com>
2024-06-28 17:52:48 -0600

    start refactored ResourceSelectModal

--- src/components/Orchestrator/components/ResourceSelectModal.tsx
+++ src/components/Orchestrator/components/ResourceSelectModal.tsx
@@ -1,6 +1,8 @@
 import { Modal } from '@mantine/core';
 import { ModelType } from '@prisma/client';
+import { useMemo } from 'react';
 import { useDialogContext } from '~/components/Dialog/DialogProvider';
+import { useIsMobile } from '~/hooks/useIsMobile';
 import { BaseModel } from '~/server/common/constants';
 import { GenerationResource } from '~/shared/constants/generation.constants';
 
@@ -13,11 +15,18 @@ export function ResourceSelectModal({
 }: {
   title: React.ReactNode;
   onAdd: (resource: GenerationResource) => void;
-  baseModel: BaseModel;
-  modelTypes: ModelType[];
+  baseModel?: BaseModel;
+  modelTypes?: ModelType[];
   canGenerate?: boolean;
 }) {
   const dialog = useDialogContext();
+  const isMobile = useIsMobile();
+
+  const filters = useMemo(() => {
+    const arr: string[] = [];
+    if (canGenerate !== undefined) arr.push(`canGenerate = ${canGenerate}`);
+    // if(baseModel) arr.push()
+  }, []);
 
   return <Modal {...dialog} title={title} size={1200}></Modal>;
 }
