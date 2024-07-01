32518c3c69e012541d0946d11dccbf684734d719
manuelurenah <manuel.ureh@hotmail.com>
2024-07-01 18:16:49 -0400

    Makes reusable middleware

--- src/server/routers/hidden-preferences.router.ts
+++ src/server/routers/hidden-preferences.router.ts
@@ -1,20 +1,13 @@
-import {
-  toggleHiddenSchema,
-  toggleHiddenTagsSchema,
-} from '~/server/schema/user-preferences.schema';
+import { noEdgeCache } from '~/server/middleware.trpc';
+import { toggleHiddenSchema } from '~/server/schema/user-preferences.schema';
 import { getAllHiddenForUser, toggleHidden } from '~/server/services/user-preferences.service';
 import { protectedProcedure, publicProcedure, router } from '~/server/trpc';
 
 export const hiddenPreferencesRouter = router({
   getHidden: publicProcedure
-    .use(async ({ ctx, next }) => {
-      // Prevents edge caching hidden preferences since they're being cache in redis already
-      // NOTE: this is required because this endpoint is being forcefully cache in the browser wihout reason
-      const result = await next();
-      ctx.cache.edgeTTL = 0;
-
-      return result;
-    })
+    // Prevents edge caching hidden preferences since they're being cache in redis already
+    // NOTE: this is required because this endpoint is being forcefully cache in the browser wihout reason
+    .use(noEdgeCache())
     .query(({ ctx }) => getAllHiddenForUser({ userId: ctx.user?.id })),
   toggleHidden: protectedProcedure
     .input(toggleHiddenSchema)
