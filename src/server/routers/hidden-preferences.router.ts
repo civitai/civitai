efa93d644975ccf0c41c3eeb21de32e8c6a02948
manuelurenah <manuel.ureh@hotmail.com>
2024-07-01 17:21:39 -0400

    Prevents edge caching hidden preferences endpoint

--- src/server/routers/hidden-preferences.router.ts
+++ src/server/routers/hidden-preferences.router.ts
@@ -6,7 +6,16 @@ import { getAllHiddenForUser, toggleHidden } from '~/server/services/user-prefer
 import { protectedProcedure, publicProcedure, router } from '~/server/trpc';
 
 export const hiddenPreferencesRouter = router({
-  getHidden: publicProcedure.query(({ ctx }) => getAllHiddenForUser({ userId: ctx.user?.id })),
+  getHidden: publicProcedure
+    .use(async ({ ctx, next }) => {
+      // Prevents edge caching hidden preferences since they're being cache in redis already
+      // NOTE: this is required because this endpoint is being forcefully cache in the browser wihout reason
+      const result = await next();
+      ctx.cache.edgeTTL = 0;
+
+      return result;
+    })
+    .query(({ ctx }) => getAllHiddenForUser({ userId: ctx.user?.id })),
   toggleHidden: protectedProcedure
     .input(toggleHiddenSchema)
     .mutation(({ input, ctx }) => toggleHidden({ ...input, userId: ctx.user.id })),
