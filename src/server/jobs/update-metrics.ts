fc5346f649fd44fcff33c1b1f4ac39192f7bb041
Justin Maier <Zipp425@gmail.com>
2024-07-20 11:08:00 -0600

    Fix metric run issue

--- src/server/jobs/update-metrics.ts
+++ src/server/jobs/update-metrics.ts
@@ -6,16 +6,16 @@ const metricSets = {
   users: [metrics.userMetrics],
   images: [metrics.imageMetrics],
   bounties: [metrics.bountyEntryMetrics, metrics.bountyMetrics],
-  clubs: [
-    // metrics.clubPostMetrics, metrics.clubMetrics // disable clubs
-  ],
+  // clubs: [
+  //   metrics.clubPostMetrics, metrics.clubMetrics // disable clubs
+  // ],
   posts: [metrics.postMetrics],
   tags: [metrics.tagMetrics],
   collections: [metrics.collectionMetrics],
   articles: [metrics.articleMetrics],
-  other: [
-    // metrics.answerMetrics, metrics.questionMetrics // disable questions and answers
-  ],
+  // other: [
+  //   metrics.answerMetrics, metrics.questionMetrics // disable questions and answers
+  // ],
 };
 
 export const metricJobs = Object.entries(metricSets).map(([name, metrics]) =>
