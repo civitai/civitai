c7410eb9a0bf0e0628179c378b5a6929c10925c7
Briant Diehl <bkdiehl@gmail.com>
2024-07-29 11:01:28 -0600

    generate button redirects unauthorized users to login page

--- src/components/Orchestrator/components/GenerateButton.tsx
+++ src/components/Orchestrator/components/GenerateButton.tsx
@@ -2,6 +2,8 @@ import { Button, ButtonProps, Text } from '@mantine/core';
 import { BuzzTransactionButton } from '~/components/Buzz/BuzzTransactionButton';
 import { useGenerationStatus } from '~/components/ImageGeneration/GenerationForm/generation.utils';
 import { useGenerationContext } from '~/components/ImageGeneration/GenerationProvider';
+import { LoginRedirect } from '~/components/LoginRedirect/LoginRedirect';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
 
 export function GenerateButton({
   cost = 0,
@@ -11,21 +13,24 @@ export function GenerateButton({
   onClick,
   ...buttonProps
 }: { cost?: number; loading?: boolean; error?: string; onClick?: () => void } & ButtonProps) {
+  const currentUser = useCurrentUser();
   const status = useGenerationStatus();
   const canGenerate = useGenerationContext((state) => state.canGenerate);
 
   const { size = 'lg' } = buttonProps;
 
-  return !status.charge ? (
-    <Button
-      {...buttonProps}
-      size={size}
-      loading={loading}
-      disabled={!canGenerate}
-      onClick={onClick}
-    >
-      <Text ta="center">{children}</Text>
-    </Button>
+  return !status.charge || !currentUser ? (
+    <LoginRedirect reason="image-gen">
+      <Button
+        {...buttonProps}
+        size={size}
+        loading={loading}
+        disabled={!canGenerate}
+        onClick={onClick}
+      >
+        <Text ta="center">{children}</Text>
+      </Button>
+    </LoginRedirect>
   ) : (
     <BuzzTransactionButton
       {...buttonProps}
