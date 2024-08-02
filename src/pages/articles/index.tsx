43127477901a660faf69018148d737531618a9fe
Brett Woodward <flipperbw@gmail.com>
2024-08-02 16:09:31 -0400

    adjusting margin of announcements so it doesn't push down content

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,5 +1,4 @@
 import { Stack, Title } from '@mantine/core';
-
 import { Announcements } from '~/components/Announcements/Announcements';
 import { setPageOptions } from '~/components/AppLayout/AppLayout';
 import { FeedLayout } from '~/components/AppLayout/FeedLayout';
@@ -7,12 +6,9 @@ import { useArticleQueryParams } from '~/components/Article/article.utils';
 import { ArticleCategories } from '~/components/Article/Infinite/ArticleCategories';
 import { ArticlesInfinite } from '~/components/Article/Infinite/ArticlesInfinite';
 import { MasonryContainer } from '~/components/MasonryColumns/MasonryContainer';
-import { MasonryProvider } from '~/components/MasonryColumns/MasonryProvider';
 import { Meta } from '~/components/Meta/Meta';
 import { env } from '~/env/client.mjs';
-import { constants } from '~/server/common/constants';
 import { createServerSideProps } from '~/server/utils/server-side-helpers';
-import { containerQuery } from '~/utils/mantine-css-helpers';
 
 export const getServerSideProps = createServerSideProps({
   useSession: true,
@@ -39,15 +35,8 @@ export default function ArticlesPage() {
       />
 
       <MasonryContainer>
+        <Announcements />
         <Stack spacing="xs">
-          <Announcements
-            sx={() => ({
-              marginBottom: -35,
-              [containerQuery.smallerThan('md')]: {
-                marginBottom: -5,
-              },
-            })}
-          />
           {query.favorites && <Title>Your Bookmarked Articles</Title>}
           <ArticleCategories />
           <ArticlesInfinite filters={query} />
