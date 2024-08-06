59cefcdfef664db41ea8f8c8c0f4c8444b2c33fb
manuelurenah <manuel.ureh@hotmail.com>
2024-08-06 09:41:14 -0400

    Updates dmp scripts

--- src/components/Ads/AdsProvider.tsx
+++ src/components/Ads/AdsProvider.tsx
@@ -106,33 +106,44 @@ function LoadProviderScript({ provider, onError }: { provider: AdProvider; onErr
           <Script src="https://cmp.uniconsent.com/v2/stub.min.js" />
           <Script src="https://cmp.uniconsent.com/v2/a635bd9830/cmp.js" async />
           <Script
+            id="ads-start"
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.googletag = window.googletag || {};
+                window.googletag.cmd = window.googletag.cmd || [];
+                window.googletag.cmd.push(function () {
+                    window.googletag.pubads().enableAsyncRendering();
+                    window.googletag.pubads().disableInitialLoad();
+                });
+                (adsbygoogle = window.adsbygoogle || []).pauseAdRequests = 1;
+              `,
+            }}
+          />
+          <Script
             id="ads-init"
             type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `
-              console.log('loaded-ads-init');
-              window.googletag = window.googletag || {};
-              window.googletag.cmd = window.googletag.cmd || [];
-              window.googletag.cmd.push(function() {
-                window.googletag.pubads().enableAsyncRendering();
-                window.googletag.pubads().disableInitialLoad();
-              });
-              (adsbygoogle = window.adsbygoogle || []).pauseAdRequests = 1;
+__tcfapi("addEventListener", 2, function(tcData, success) {
+    if (success && tcData.unicLoad  === true) {
+        if(!window._initAds) {
+            window._initAds = true;
+            
+	    var script = document.createElement('script');
+            script.async = true;
+            script.src = '//dsh7ky7308k4b.cloudfront.net/publishers/civitaicom.min.js';
+            document.head.appendChild(script);
 
-              var loadAdScripts = function loadAdScripts() {  
-                var script = document.createElement('script');
-                script.async = true;
-                script.src = '//dsh7ky7308k4b.cloudfront.net/publishers/civitaicom.min.js';
-                document.head.appendChild(script);
-
-                var script = document.createElement('script');
-                script.async = true;
-                script.src = '//btloader.com/tag?o=5184339635601408&upapi=true';
-                document.head.appendChild(script);
-              };
-
-              loadAdScripts();
-            `,
+	    var script = document.createElement('script');
+            script.async = true;
+            script.src = '//btloader.com/tag?o=5184339635601408&upapi=true';
+            document.head.appendChild(script);
+			
+        }
+    }
+});
+              `,
             }}
           />
         </>
