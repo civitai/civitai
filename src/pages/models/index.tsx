dbe1bf78d16be454ddb22058e4eeba38ad2126e4
Luis Rojas <lrojas94@gmail.com>
2024-08-07 16:37:58 -0400

    Add early access button

--- src/pages/models/index.tsx
+++ src/pages/models/index.tsx
@@ -1,4 +1,5 @@
-import { Stack, Title } from '@mantine/core';
+import { Button, Group, Stack, Title } from '@mantine/core';
+import { IconClock } from '@tabler/icons-react';
 import { Announcements } from '~/components/Announcements/Announcements';
 import { setPageOptions } from '~/components/AppLayout/AppLayout';
 import { FeedLayout } from '~/components/AppLayout/FeedLayout';
@@ -9,12 +10,17 @@ import { Meta } from '~/components/Meta/Meta';
 import { ModelsInfinite } from '~/components/Model/Infinite/ModelsInfinite';
 import { useModelQueryParams } from '~/components/Model/model.utils';
 import { env } from '~/env/client.mjs';
+import { useFiltersContext } from '~/providers/FiltersProvider';
 import { PeriodMode } from '~/server/schema/base.schema';
 
 export default function ModelsPage() {
   const { set, view: queryView, ...queryFilters } = useModelQueryParams();
   const { username, query } = queryFilters;
   const periodMode = query ? ('stats' as PeriodMode) : undefined;
+  const { setFilters, earlyAccess } = useFiltersContext((state) => ({
+    setFilters: state.setModelFilters,
+    earlyAccess: state.models.earlyAccess,
+  }));
   if (periodMode) queryFilters.periodMode = periodMode;
 
   return (
@@ -32,7 +38,18 @@ export default function ModelsPage() {
           <IsClient>
             {/* TODO: Bring back something similar in the future once we start selling spots. */}
             {/* <EarlyAccessHighlight /> */}
-            <CategoryTags />
+            <Group spacing="xs">
+              <Button
+                variant={earlyAccess ? 'filled' : 'outline'}
+                color="teal"
+                onClick={() => setFilters({ earlyAccess: !earlyAccess })}
+                compact
+                leftIcon={<IconClock size={16} />}
+              >
+                Early Access
+              </Button>
+              <CategoryTags />
+            </Group>
             <ModelsInfinite filters={queryFilters} showEof showAds />
           </IsClient>
         </Stack>
