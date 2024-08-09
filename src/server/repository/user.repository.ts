aed712314227e1599d782181bb1ce2597255f96c
Briant Diehl <bkdiehl@gmail.com>
2024-08-09 13:49:10 -0600

    check in

--- src/server/repository/user.repository.ts
+++ src/server/repository/user.repository.ts
@@ -2,25 +2,34 @@ import { SqlBool, Expression, InferResult, sql } from 'kysely';
 import { jsonObjectFrom, kyselyDbRead } from '~/server/kysely-db';
 import { ImageRepository } from '~/server/repository/image.repository';
 import { UserCosmeticRepository } from '~/server/repository/user-cosmetic.repository';
-import { UserLinkRepository } from '~/server/repository/user-link.repository';
+import { UserLinkModel, UserLinkRepository } from '~/server/repository/user-link.repository';
 import { UserRankRepository } from '~/server/repository/user-rank.repository';
 import { UserStatRepository } from '~/server/repository/user-stats.repository';
 import { ModelRepository } from '~/server/repository/model.repository';
 
-export type UserBaseModel = InferResult<(typeof UserRepository)['baseUserSelect']>;
+export type UserBaseModel = InferResult<ReturnType<(typeof UserRepository)['baseUserSelect']>>;
 export type UserWithCosmeticModel = InferResult<(typeof UserRepository)['cosmeticUserSelect']>;
 export type UserCreatorModel = InferResult<(typeof UserRepository)['creatorUserSelect']>;
 
+/*
+  query user ids
+  await promise.all([queryUserLinks(userId), queryUserStats(userIds)])
+*/
+
+// TODO - base repository class with redis helpers
+// export const userRepository = new UserRepository({...redis_config})
+
 export class UserRepository {
   // #region [select]
-  private static get baseUserSelect() {
+  private static baseUserSelect(includes: string[] = []) {
     return kyselyDbRead
       .selectFrom('User')
-      .select(['id', 'username', 'deletedAt', 'muted', 'bannedAt', 'createdAt']);
+      .select((eb) => ['id', 'username', 'deletedAt', 'muted', 'bannedAt', 'createdAt'])
+      .$if(includes.includes('email'), (qb) => qb.select('email'));
   }
 
   private static get cosmeticUserSelect() {
-    return this.baseUserSelect.select((eb) => [
+    return this.baseUserSelect().select((eb) => [
       'leaderboardShowcase',
       'image',
       ImageRepository.findOneBaseImageByIdRef(eb.ref('User.profilePictureId')).as('profilePicture'),
@@ -49,12 +58,8 @@ export class UserRepository {
   // #endregion
 
   // #region [main]
-  static async findOneUserCreator(
-    // args: { id?: number; username?: never } | { id?: never; username?: string }
-    args: { id?: number; username?: string }
-  ) {
-    return await this.creatorUserSelect
-
+  static async findOneUseBasic(args: { id?: number; username?: string }, includes?: 'email'[]) {
+    return await this.baseUserSelect(includes)
       .where(({ eb, and }) => {
         const ands: Expression<SqlBool>[] = [];
         if (args.id) ands.push(eb('User.id', '=', args.id));
@@ -70,6 +75,21 @@ export class UserRepository {
     if (args.ids?.length) query = query.where('User.id', 'in', args.ids);
 
     return await query.execute();
+  }
+
+  static async findOneUserCreator(
+    // args: { id?: number; username?: never } | { id?: never; username?: string }
+    args: { id?: number; username?: string }
+  ) {
+    return await this.creatorUserSelect
+
+      .where(({ eb, and }) => {
+        const ands: Expression<SqlBool>[] = [];
+        if (args.id) ands.push(eb('User.id', '=', args.id));
+        if (args.username) ands.push(eb('User.username', '=', args.username));
+        return and([...ands, eb('User.deletedAt', 'is', null)]);
+      })
+      .executeTakeFirst();
   }
   // #endregion
 }
