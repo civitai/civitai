49772da62d7cccb8dd1c6c1b03c83d29e311de72
Briant Diehl <bkdiehl@gmail.com>
2024-08-22 21:11:17 -0600

    added UserBuzzCache

---
+++ src/server/rewards/user-buzz-cache.ts
@@ -0,0 +1,50 @@
+import { REDIS_KEYS, redis } from '~/server/redis/client';
+
+class UserBuzzCache {
+  private _amount?: number;
+  private _key: string;
+
+  constructor(args: { userId: number; deviceId: string; type: string }) {
+    this._key = getKey(args);
+  }
+
+  get amount() {
+    return new Promise<number>((resolve) => {
+      if (this._amount) resolve(this._amount as number);
+      else
+        redis.hGet(REDIS_KEYS.BUZZ_EVENTS, this._key).then((value) => {
+          const amount = !!value ? Number(value) : 0;
+          this._amount = amount;
+          resolve(amount);
+        });
+    });
+  }
+
+  async update(amount: number) {
+    const end = new Date();
+    end.setUTCHours(23, 59, 59, 999);
+    this._amount = amount;
+    await redis.packed.hSet(REDIS_KEYS.BUZZ_EVENTS, this._key, String(amount));
+    await setExpiresAt();
+  }
+
+  static async getMany(args: { userId: number; deviceId: string; type: string }[]) {
+    return await redis.packed.mGet(args.map((x) => getKey(x)));
+  }
+
+  static async setMany(args: { userId: number; deviceId: string; type: string; amount: number }[]) {
+    await Promise.all(
+      args.map((x) => redis.packed.hSet(REDIS_KEYS.BUZZ_EVENTS, getKey(x), x.amount))
+    );
+    await setExpiresAt();
+  }
+}
+
+function getKey(args: { userId: number; deviceId: string; type: string }) {
+  return `${args.userId}:${args.deviceId}:${args.type}`;
+}
+
+async function setExpiresAt() {
+  const end = new Date().setUTCHours(24, 0, 0, 0);
+  await redis.expireAt(REDIS_KEYS.BUZZ_EVENTS, Math.floor(end / 1000));
+}
