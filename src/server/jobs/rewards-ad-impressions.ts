bb77fa13e641416b5a13c04ad757305f4c320f89
Briant Diehl <bkdiehl@gmail.com>
2024-08-23 14:59:06 -0600

    added concurrency limits and buzz transactions to rewards-ad-impressions

--- src/server/jobs/rewards-ad-impressions.ts
+++ src/server/jobs/rewards-ad-impressions.ts
@@ -1,25 +1,22 @@
 import { chunk } from 'lodash-es';
-import { v4 as uuid } from 'uuid';
-import { z } from 'zod';
 import { clickhouse } from '~/server/clickhouse/client';
-import { NotificationCategory } from '~/server/common/enums';
-import { dbWrite } from '~/server/db/client';
 import { createJob, getJobDate } from '~/server/jobs/job';
-import { userMultipliersCache } from '~/server/redis/caches';
-import { redis, REDIS_KEYS } from '~/server/redis/client';
+import { logToAxiom } from '~/server/logging/client';
 import { BuzzEventsCache } from '~/server/rewards/buzz-events-cache';
-import { CreateBuzzTransactionInput } from '~/server/schema/buzz.schema';
-import { createNotification } from '~/server/services/notification.service';
+import { CreateBuzzTransactionInput, TransactionType } from '~/server/schema/buzz.schema';
+import { createBuzzTransactionMany } from '~/server/services/buzz.service';
 import { limitConcurrency } from '~/server/utils/concurrency-helpers';
 
 const type = 'ad-impression';
 const cap = 100;
+const hourlyCap = 15;
 const buzzPerAd = 0.5;
 
 export const rewardsAbusePrevention = createJob('rewards-ad-impressions', '0 * * * *', async () => {
-  const [lastRun, setLastRun] = await getJobDate('rewards-ad-impressions', new Date('8/22/2024'));
+  try {
+    const [lastRun, setLastRun] = await getJobDate('rewards-ad-impressions', new Date('8/22/2024'));
 
-  const results = await clickhouse?.$query<Impression>(`
+    const results = await clickhouse?.$query<Impression>(`
     SELECT
     userId,
     deviceId,
@@ -31,30 +28,48 @@ export const rewardsAbusePrevention = createJob('rewards-ad-impressions', '0 * *
     GROUP BY userId, deviceId;
   `);
 
-  if (results) {
-    const cachedAmounts = await BuzzEventsCache.getMany(
-      results.map(({ userId, deviceId }) => ({ userId, deviceId, type }))
-    );
+    if (results) {
+      const cachedAmounts = await BuzzEventsCache.getMany(
+        results.map(({ userId, deviceId }) => ({ userId, deviceId, type }))
+      );
 
-    const transactions = results
-      .map(({ userId, totalAdImpressions, deviceId }, i) => {
-        const adImpressionAmount = Math.floor(totalAdImpressions * buzzPerAd);
-        const cachedAmount = cachedAmounts[i];
-        const remaining = cap - cachedAmount;
-        const amount = Math.min(adImpressionAmount, remaining);
+      const transactions = results
+        .map(({ userId, totalAdImpressions, deviceId }, i): Transaction => {
+          const adImpressionAmount = Math.floor(totalAdImpressions * buzzPerAd);
+          const cachedAmount = cachedAmounts[i];
+          const remaining = cap - cachedAmount;
+          const amount = Math.min(adImpressionAmount, remaining, hourlyCap);
 
-        return {
-          fromAccountId: 0,
-          toAccountId: userId,
-          toAccountType: 'user:generation',
-          amount,
-          deviceId,
-        };
-      })
-      .filter((x) => x.amount > 0);
+          return {
+            fromAccountId: 0,
+            toAccountId: userId,
+            toAccountType: 'user:generation',
+            amount,
+            deviceId,
+            type: TransactionType.Reward,
+            externalTransactionId: `${userId}:${deviceId}:${type}:${lastRun.getTime()}`,
+          };
+        })
+        .filter((x) => x.amount > 0);
+
+      const tasks = chunk(transactions, 500).map((chunk) => async () => {
+        await createBuzzTransactionMany(chunk);
+        await BuzzEventsCache.incrManyBy(
+          chunk.map((transaction) => ({
+            ...transaction,
+            type,
+            userId: transaction.toAccountId!,
+          }))
+        );
+      });
+
+      await limitConcurrency(tasks, 3);
+    }
+
+    await setLastRun();
+  } catch (e) {
+    logToAxiom({ name: 'rewards-ad-impressions', type: 'error', message: (e as any).message });
   }
-
-  await setLastRun();
 });
 
 type Impression = {
@@ -68,4 +83,5 @@ type Impression = {
 type Transaction = CreateBuzzTransactionInput & {
   fromAccountId: number;
   externalTransactionId: string;
+  deviceId: string;
 };
