d03579a96ac25ea1db0efa93335862415932e216
manuelurenah <manuel.ureh@hotmail.com>
2024-08-27 11:06:37 -0400

    Color correction when displaying buzz notification

--- src/utils/notifications.tsx
+++ src/utils/notifications.tsx
@@ -80,7 +80,7 @@ export function showBuzzNotification({
     message: (
       <Group spacing={4}>
         {/* @ts-ignore: ignoring ts error cause `transparent` works on variant */}
-        <ThemeIcon color="yellow.4" variant="transparent">
+        <ThemeIcon color={notificationProps.color ?? 'yellow.4'} variant="transparent">
           <IconBolt size={18} fill="currentColor" />
         </ThemeIcon>
         {message}
