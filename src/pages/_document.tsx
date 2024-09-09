16b60345b5051379435ff48c54e007e857395f6b
manuelurenah <manuel.ureh@hotmail.com>
2024-09-09 18:56:24 -0400

    Updates collection index when updating collection plus removes turnstile script globally

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import { createGetInitialProps } from '@mantine/next';
-import { DEFAULT_SCRIPT_ID, SCRIPT_URL } from '@marsidev/react-turnstile';
 import Document, { Html, Main, NextScript, Head } from 'next/document';
-import Script from 'next/script';
 
 const getInitialProps = createGetInitialProps();
 
@@ -15,7 +13,6 @@ export default class _Document extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script id={DEFAULT_SCRIPT_ID} src={SCRIPT_URL} strategy="beforeInteractive" />
         </body>
       </Html>
     );
