999c19c47b351203e96c602a0de3f641b7f83be2
Briant Diehl <bkdiehl@gmail.com>
2024-09-25 16:43:47 -0600

    page-view tracking now tracking correct page

--- src/components/TrackView/TrackPageView.tsx
+++ src/components/TrackView/TrackPageView.tsx
@@ -1,42 +1,101 @@
-/*
-  TODO -
-  1. determine if ads are loaded on the page
-  2. determine page visit duration
-*/
-
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useAdUnitLoadedStore } from '~/components/Ads/AdsProvider';
 import { useBrowserRouter } from '~/components/BrowserRouter/BrowserRouterProvider';
+import { removeEmpty } from '~/utils/object-helpers';
 
 export function TrackPageView() {
-  const router = useRouter();
   const browserRouter = useBrowserRouter();
   const pathname = browserRouter.asPath.split('?')[0];
 
+  const timeRef = useRef<{ visible: number; hidden?: number }[]>([{ visible: Date.now() }]);
+  const durationRef = useRef(0);
+  const ref = useRef<{ value: string; prev: string | null }>({
+    value: pathname,
+    prev: null,
+  });
+
   useEffect(() => {
-    useAdUnitLoadedStore.setState({});
-    const mountTime = Date.now();
-    function trackPageView() {
-      const duration = Date.now() - mountTime;
-      if (duration < 1000) return;
+    const pushState = history.pushState;
+    const replaceState = history.replaceState;
 
-      const ads = Object.keys(useAdUnitLoadedStore.getState()).length > 0;
+    function getDuration() {
+      return timeRef.current.reduce(
+        (acc, { visible, hidden = Date.now() }) => acc + (hidden - visible),
+        0
+      );
+    }
 
-      fetch('/api/page-view', {
-        method: 'post',
-        keepalive: true,
-        body: JSON.stringify({ duration, ads }),
+    function updateRef(value: string) {
+      const current = ref.current.value;
+      if (value !== current) {
+        durationRef.current = getDuration();
+        timeRef.current = [{ visible: Date.now() }];
+        ref.current = {
+          value: value,
+          prev: current,
+        };
+      }
+    }
+
+    function updateRefFromHistoryAction(url: string | URL | null | undefined) {
+      if (!url) return;
+      const obj = new URL(url, location.origin);
+      updateRef(obj.pathname);
+    }
+
+    function popstate() {
+      updateRef(location.pathname);
+    }
+
+    function visibilityChange() {
+      if (document.visibilityState === 'visible') timeRef.current.push({ visible: Date.now() });
+      else timeRef.current[timeRef.current.length - 1].hidden = Date.now();
+    }
+
+    function beforeUnload() {
+      trackPageView({
+        path: location.pathname,
+        duration: getDuration(),
       });
     }
 
-    window.addEventListener('beforeunload', trackPageView);
-    return () => {
-      trackPageView();
-      window.removeEventListener('beforeunload', trackPageView);
+    document.addEventListener('visibilitychange', visibilityChange);
+    window.addEventListener('popstate', popstate);
+    window.addEventListener('beforeunload', beforeUnload);
+    history.replaceState = function (data, unused, url) {
+      updateRefFromHistoryAction(url);
+      return replaceState.apply(history, [data, unused, url]);
     };
-    //
-  }, [router.pathname, pathname]);
+    history.pushState = function (data, unused, url) {
+      updateRefFromHistoryAction(url);
+      return pushState.apply(history, [data, unused, url]);
+    };
 
-  return null;
+    return function () {
+      history.pushState = pushState;
+      history.replaceState = replaceState;
+      window.removeEventListener('popstate', popstate);
+      window.removeEventListener('beforeunload', beforeUnload);
+      document.removeEventListener('visibilitychange', visibilityChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    const path = ref.current.prev;
+    if (path) trackPageView({ path, duration: durationRef.current });
+  }, [pathname]);
+}
+
+function trackPageView({ path, duration }: { path: string; duration: number }) {
+  if (duration < 1000) return;
+
+  const ads = Object.keys(useAdUnitLoadedStore.getState()).length > 0;
+
+  fetch('/api/page-view', {
+    method: 'post',
+    keepalive: true,
+    body: JSON.stringify(removeEmpty({ duration, ads: ads ? true : undefined, path })),
+  });
+
+  useAdUnitLoadedStore.setState({});
 }
