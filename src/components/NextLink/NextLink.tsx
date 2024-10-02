c1a4d6a9841d574c9e4b214a6db30080e479ddd5
Briant Diehl <bkdiehl@gmail.com>
2024-10-02 00:16:27 -0600

    fix type errors

--- src/components/NextLink/NextLink.tsx
+++ src/components/NextLink/NextLink.tsx
@@ -1,5 +1,15 @@
 import Link, { LinkProps } from 'next/link';
 
-export function NextLink({children, ...props}: LinkProps & {children?: React.ReactNode;}) {
-  return <Link prefetch={false} {...props} >{children}</Link>
+export function NextLink({
+  children,
+  ...props
+}: LinkProps & { children?: React.ReactNode } & Omit<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    keyof LinkProps
+  >) {
+  return (
+    <Link prefetch={false} {...props}>
+      {children}
+    </Link>
+  );
 }
