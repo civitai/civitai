4a4f50f09ecb585d9f50e1e97372b61326ccee94
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2024-10-04 15:39:40 -0400

    [CU-8689pphvt] Adds content ingestion for models (#1397)
    * Adds service and webhook to ingest model content
    
    * Adds migration and adjustments to allow model ingestion
    
    * Adds reports page for mods
    
    * Adds example env vars
    
    * Fixes after merging with main
    
    * 5.0.139
    
    * Adjusts moderator/models page
    
    * Fix images API endpoint not supporting postId properly
    
    * 5.0.143
    
    * Adds a quick way for mods to update a flagged model while resolving
    
    * Prevents awaiting model ingestion
    
    * Allows sorting flagged models
    
    ---------
    
    Co-authored-by: Luis Rojas <lrojas94@gmail.com>

--- src/server/services/model-flag.service.ts
+++ src/server/services/model-flag.service.ts
@@ -1,13 +1,124 @@
-import { dbWrite } from '~/server/db/client';
-import { hasNsfwWords } from '~/utils/metadata/audit';
+import { ModelFlagStatus, Prisma } from '@prisma/client';
+import { dbRead, dbWrite } from '~/server/db/client';
+import { GetByIdInput } from '~/server/schema/base.schema';
+import { GetFlaggedModelsInput, ModelScanResult } from '~/server/schema/model-flag.schema';
+import { trackModActivity } from '~/server/services/moderator.service';
+import { getPagedData } from '~/server/utils/pagination-helpers';
 
-export async function upsertModelFlag({ modelId, ...data }: { modelId: number; name?: string }) {
-  const nameNsfw = hasNsfwWords(data.name);
-  if (!nameNsfw) return;
+export async function upsertModelFlag({
+  modelId,
+  scanResult,
+  details,
+}: {
+  modelId: number;
+  poiName?: string;
+  scanResult?: {
+    poi: boolean;
+    nsfw: boolean;
+    minor: boolean;
+    triggerWords: boolean;
+    poiName: boolean;
+  };
+  details?: MixedObject;
+}) {
+  const isFlagged = scanResult && Object.values(scanResult).some((flag) => flag);
+  if (!isFlagged) return null;
 
-  await dbWrite.$executeRaw`
-    INSERT INTO "ModelFlag" ("modelId", "nameNsfw")
-    VALUES (${modelId}, ${nameNsfw})
-    ON CONFLICT ("modelId") DO UPDATE SET "nameNsfw" = EXCLUDED."nameNsfw";
+  const [modelFlag] = await dbWrite.$queryRaw<
+    {
+      modelId: number;
+      poi: boolean;
+      nsfw: boolean;
+      minor: boolean;
+      triggerWords: boolean;
+      poiName: string | null;
+      status: ModelFlagStatus;
+    }[]
+  >`
+    INSERT INTO "ModelFlag" ("modelId", "poi", "nsfw", "minor", "triggerWords", "poiName", "status", "details")
+    VALUES (
+      ${modelId},
+      ${scanResult?.poi ?? false},
+      ${scanResult?.nsfw ?? false},
+      ${scanResult?.minor ?? false},
+      ${scanResult?.triggerWords ?? false},
+      ${scanResult?.poiName ?? false},
+      ${Prisma.sql`${ModelFlagStatus.Pending}::"ModelFlagStatus"`},
+      ${details ? Prisma.sql`${JSON.stringify(details)}::jsonb` : Prisma.JsonNull}
+    )
+    ON CONFLICT ("modelId") DO UPDATE
+      SET "poi" = EXCLUDED."poi",
+        "nsfw" = EXCLUDED."nsfw",
+        "minor" = EXCLUDED."minor",
+        "triggerWords" = EXCLUDED."triggerWords",
+        "poiName" = EXCLUDED."poiName",
+        "status" = EXCLUDED."status",
+        "details" = EXCLUDED."details"
+    RETURNING *;
   `;
+
+  return modelFlag;
+}
+
+export function getFlaggedModels(input: GetFlaggedModelsInput) {
+  return getPagedData(input, async ({ skip, take, sort = [], ...rest }) => {
+    const [flaggedModels, count] = await dbRead.$transaction([
+      dbRead.modelFlag.findMany({
+        where: { status: ModelFlagStatus.Pending },
+        take,
+        skip,
+        select: {
+          modelId: true,
+          poi: true,
+          nsfw: true,
+          triggerWords: true,
+          minor: true,
+          details: true,
+          poiName: true,
+          model: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              nsfw: true,
+              poi: true,
+              minor: true,
+              // These are needed to comply with upsert schema
+              status: true,
+              uploadType: true,
+              type: true,
+            },
+          },
+        },
+        orderBy: [
+          ...sort.map(({ id, desc }) => ({ [id]: desc ? 'desc' : 'asc' })),
+          { createdAt: 'desc' },
+        ],
+      }),
+      dbRead.modelFlag.count({ where: { status: ModelFlagStatus.Pending } }),
+    ]);
+
+    return {
+      items: flaggedModels.map(({ details, ...model }) => {
+        const parsedDetails = details as ModelScanResult['llm_interrogation'];
+
+        return {
+          ...model,
+          details: parsedDetails,
+        };
+      }),
+      count,
+    };
+  });
+}
+
+export async function resolveFlaggedModel({ id, userId }: GetByIdInput & { userId: number }) {
+  const updated = await dbWrite.modelFlag.update({
+    where: { modelId: id },
+    data: { status: ModelFlagStatus.Resolved },
+  });
+
+  await trackModActivity(userId, { entityType: 'model', entityId: id, activity: 'moderateFlag' });
+
+  return updated;
 }
