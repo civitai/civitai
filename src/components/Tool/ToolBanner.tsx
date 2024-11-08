310df176edf21218962a36bc402e3b19a8857535
Briant Diehl <bkdiehl@gmail.com>
2024-11-08 15:43:40 -0700

    fix bad merge

--- src/components/Tool/ToolBanner.tsx
+++ src/components/Tool/ToolBanner.tsx
@@ -80,7 +80,7 @@ export function ToolBanner({
                   >
                     Generate
                   </Button>
-                )} */}
+                )}
               </div>
             </div>
           </div>
