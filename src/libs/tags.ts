92dbe9e1eb0b7b478b1e7aaafba7567b1bbf44da
Justin Maier <Zipp425@gmail.com>
2024-11-11 14:27:55 -0700

    Add filter for failed rekognition tagging

--- src/libs/tags.ts
+++ src/libs/tags.ts
@@ -50,5 +50,13 @@ export function getTagDisplayName(name: string) {
 
 export const tagsNeedingReview = ['child', 'teen', 'baby', 'girl', 'boy'];
 export const tagsToIgnore: Partial<Record<TagSource, string[]>> = {
-  Rekognition: ['baby', 'emaciated bodies', 'weapons'],
+  Rekognition: [
+    'baby',
+    'emaciated bodies',
+    'weapons',
+    // Adding these since Rekognition seems to be tagging anything red...
+    'hate symbols',
+    'nazi party',
+    'white supremacy',
+  ],
 };
