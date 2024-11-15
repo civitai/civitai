cabed29711c647bc36ab35374a58057af8811c87
Briant Diehl <bkdiehl@gmail.com>
2024-11-15 11:45:41 -0700

    fix server to client seed changing when it shouldn't

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 type AppContext = { seed: number };
 const Context = createContext<AppContext | null>(null);
@@ -11,5 +11,11 @@ export function AppProvider({
   children,
   ...appContext
 }: { children: React.ReactNode } & AppContext) {
-  return <Context.Provider value={appContext}>{children}</Context.Provider>;
+  const [seed, setSeed] = useState(appContext.seed);
+
+  useEffect(() => {
+    if (!seed && appContext.seed) setSeed(appContext.seed);
+  }, []);
+
+  return <Context.Provider value={{ seed }}>{children}</Context.Provider>;
 }
