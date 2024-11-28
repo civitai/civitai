3bc5cd284c80d35532267425150a94fdb3335662
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2024-11-28 17:44:23 -0400

    Adds civitai merch section in shop (#1518)
    * Adds civitai merch section in shop
    
    * Updates shop nav item

--- src/components/Challenges/ChallengeIndicator.tsx
+++ src/components/Challenges/ChallengeIndicator.tsx
@@ -1,6 +1,7 @@
 import { ActionIcon, Indicator } from '@mantine/core';
 import { IconTrophy } from '@tabler/icons-react';
 import clsx from 'clsx';
+import { useEffect } from 'react';
 import { useQueryCurrentChallenge } from '~/components/Challenges/challenge.utils';
 import { ChallengeInvitation } from '~/components/Challenges/ChallengeInvitation';
 import { dialogStore } from '~/components/Dialog/dialogStore';
@@ -12,8 +13,7 @@ export function ChallengeIndicator() {
   const [dismissed, setDismissed] = useStorage({
     type: 'localStorage',
     key: storageKey,
-    defaultValue:
-      typeof window !== 'undefined' ? window?.localStorage?.getItem(storageKey) === 'true' : false,
+    defaultValue: true,
     getInitialValueInEffect: true,
   });
 
@@ -23,6 +23,14 @@ export function ChallengeIndicator() {
       props: { onClose: () => setDismissed(true) },
     });
   };
+  useEffect(() => {
+    if (challenge?.articleId) {
+      const localDismissed = window.localStorage.getItem(storageKey) === 'true';
+      if (localDismissed !== dismissed) {
+        setDismissed(localDismissed);
+      }
+    }
+  }, [challenge?.articleId, storageKey]);
 
   return (
     <Indicator color="red" size={12} disabled={dismissed} dot inline>
