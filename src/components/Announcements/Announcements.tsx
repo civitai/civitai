f206d9bdbb1e727829de9b3f5f645068b3fbed9e
Briant Diehl <bkdiehl@gmail.com>
2024-12-03 14:39:36 -0700

    simplify announcements component logic

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -1,40 +1,12 @@
-import {
-  dismissAnnouncements,
-  useAnnouncementsStore,
-} from '~/components/Announcements/announcements.utils';
-import React, { useEffect, useMemo } from 'react';
+import { useGetAnnouncements } from '~/components/Announcements/announcements.utils';
+import React from 'react';
 import { Announcement } from '~/components/Announcements/Announcement';
-import { trpc } from '~/utils/trpc';
 import { Carousel } from '@mantine/carousel';
 
 export function Announcements() {
-  const { data } = trpc.announcement.getAnnouncements.useQuery();
-  const dismissed = useAnnouncementsStore((state) => state.dismissed);
+  const { data } = useGetAnnouncements();
 
-  useEffect(() => {
-    if (!data?.length) return;
-    const legacyKeys = Object.keys(localStorage).filter((key) => key.startsWith('announcement-'));
-    const legacyIds = legacyKeys.map((key) => Number(key.replace('announcement-', '')));
-    const announcementIds = data.map(({ id }) => id);
-
-    // remove legacy announcment id storage
-    if (legacyIds) {
-      dismissAnnouncements(legacyIds.filter((id) => announcementIds.includes(id)));
-      for (const key of legacyKeys) {
-        localStorage.removeItem(key);
-      }
-    }
-
-    // remove old announcementIds from storage
-    if (dismissed.some((id) => !announcementIds.includes(id))) {
-      dismissAnnouncements(dismissed.filter((id) => announcementIds.includes(id)));
-    }
-  }, [data]); // eslint-disable-line
-
-  const announcements = useMemo(
-    () => data?.filter((announcement) => !dismissed.includes(announcement.id)) ?? [],
-    [data, dismissed]
-  );
+  const announcements = data.filter((x) => !x.dismissed);
 
   if (!announcements.length) return null;
 
