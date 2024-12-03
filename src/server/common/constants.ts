3b906ed8a1c564bc6b51b3e2dab03be6058ff8d0
Briant Diehl <bkdiehl@gmail.com>
2024-12-02 17:16:56 -0700

    more ads on post detail page

--- src/server/common/constants.ts
+++ src/server/common/constants.ts
@@ -404,7 +404,7 @@ export const POST_TAG_LIMIT = 5;
 export const CAROUSEL_LIMIT = 20;
 export const DEFAULT_EDGE_IMAGE_WIDTH = 450;
 export const MAX_ANIMATION_DURATION_SECONDS = 30;
-export const MAX_POST_IMAGES_WIDTH = 700;
+export const MAX_POST_IMAGES_WIDTH = 800;
 
 export type BaseModelType = (typeof constants.baseModelTypes)[number];
 
