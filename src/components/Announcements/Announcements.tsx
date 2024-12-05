de4b77e077e8d0eb4b482bd75528bad37b3b1823
Briant Diehl <bkdiehl@gmail.com>
2024-12-05 15:18:43 -0700

    adjust announcements carousel to properly adjust when resizable sidebar opens

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -3,10 +3,14 @@ import React, { useRef } from 'react';
 import { Announcement } from '~/components/Announcements/Announcement';
 import { Carousel } from '@mantine/carousel';
 import autoplay from 'embla-carousel-autoplay';
+import { useContainerWidth } from '~/components/ContainerProvider/ContainerProvider';
+import { useDebouncedValue } from '@mantine/hooks';
 
 export function Announcements() {
   const autoplayRef = useRef(autoplay({ delay: 10000 }));
   const { data } = useGetAnnouncements();
+  const width = useContainerWidth();
+  const widthDebounced = useDebouncedValue(width, 50);
 
   const announcements = data.filter((x) => !x.dismissed);
 
@@ -17,6 +21,7 @@ export function Announcements() {
     // eslint-disable-next-line tailwindcss/no-custom-classname
     <div className="announcements peer container mb-3">
       <Carousel
+        key={`${widthDebounced}`}
         withIndicators={announcements.length > 1}
         withControls={false}
         slideGap="md"
