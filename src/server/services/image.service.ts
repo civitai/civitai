b72096ea389bc2da0ffcc66eb0b074495cbd2958
Brett Woodward <flipperbw@gmail.com>
2024-12-11 13:16:00 -0500

    remove manual filter button

--- src/server/services/image.service.ts
+++ src/server/services/image.service.ts
@@ -1681,6 +1681,7 @@ async function getImagesFromSearch(input: ImageSearchInput) {
     if (!hideAutoResources) {
       versionFilters.push(makeMeiliImageSearchFilter('modelVersionIds', `IN [${modelVersionId}]`));
     }
+    // TODO re-enable after backfilling
     // if (!hideManualResources) {
     //   versionFilters.push(
     //     makeMeiliImageSearchFilter('modelVersionIdsManual', `IN [${modelVersionId}]`)
