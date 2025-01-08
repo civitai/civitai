8660e212e707d4557fcd2594b65fe0d434d3edc5
Briant Diehl <bkdiehl@gmail.com>
2025-01-08 11:12:52 -0700

    return correct error messages on image upload failure

--- src/server/services/orchestrator/imageUpload.ts
+++ src/server/services/orchestrator/imageUpload.ts
@@ -1,6 +1,6 @@
 import { invokeImageUploadStepTemplate } from '@civitai/client';
 import { createOrchestratorClient } from '~/server/services/orchestrator/common';
-import { throwBadRequestError } from '~/server/utils/errorHandling';
+import { throwAuthorizationError, throwBadRequestError } from '~/server/utils/errorHandling';
 
 export async function imageUpload({ sourceImage, token }: { sourceImage: string; token: string }) {
   const client = createOrchestratorClient(token);
@@ -13,10 +13,20 @@ export async function imageUpload({ sourceImage, token }: { sourceImage: string;
   });
 
   if (!data) {
-    if (error.status === 400) {
-      const messages = (error as any).error.messages;
-      throw throwBadRequestError(messages.join('\n'));
-    } else throw throwBadRequestError(error.title);
+    const messages = (error as any).error?.messages;
+    switch (error.status) {
+      case 400:
+        throw throwBadRequestError(messages ?? error.detail);
+      case 401:
+        throw throwAuthorizationError(messages ?? error.detail);
+      default:
+        throw new Error(messages ?? error.detail);
+    }
+
+    // if (error.status === 400) {
+    //   const messages = (error as any).error.messages;
+    //   throw throwBadRequestError(messages.join('\n'));
+    // } else throw throwBadRequestError(error.title);
   }
 
   return data;
