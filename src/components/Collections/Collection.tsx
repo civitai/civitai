2d94af211f2720c0a5d0adf0ddd5643373d6b257
Briant Diehl <bkdiehl@gmail.com>
2025-01-13 15:23:12 -0700

    fix type errors

--- src/components/Collections/Collection.tsx
+++ src/components/Collections/Collection.tsx
@@ -278,7 +278,8 @@ const ImageCollection = ({
         );
       }}
       additionalMenuItemsAfter={(image) => {
-        const isOwnerOrMod = permissions?.manage || currentUser?.id === collection.user.id || currentUser?.isModerator;
+        const isOwnerOrMod =
+          permissions?.manage || currentUser?.id === collection.user.id || currentUser?.isModerator;
         return (
           <>
             {isOwnerOrMod && (
@@ -780,10 +781,11 @@ export function Collection({
                                       : 'red';
 
                                   const label = capitalize(status.toLowerCase());
+                                  const entryCount = entryCountDetails[status];
 
-                                  return entryCountDetails[status]
+                                  return entryCount
                                     ? {
-                                        value: (entryCountDetails[status] / totalEntries) * 100,
+                                        value: (entryCount / totalEntries) * 100,
                                         color,
                                         // label,
                                         tooltip: `${label}: ${entryCountDetails[status]}`,
