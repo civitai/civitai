c9c367a11e389407d04b7a52f7f2065286c7a568
Luis Rojas <lrojas94@gmail.com>
2025-01-20 16:50:26 -0400

    Add concurrency, update side B

--- src/pages/api/meili-to-meili-migration.ts
+++ src/pages/api/meili-to-meili-migration.ts
@@ -1,47 +1,67 @@
 import { MeiliSearch } from 'meilisearch';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { updateDocs } from '~/server/meilisearch/client';
+import { limitConcurrency, sleep, Task } from '~/server/utils/concurrency-helpers';
 import { PublicEndpoint } from '~/server/utils/endpoint-helpers';
-import { sleep } from '~/utils/errorHandling';
 
 export default PublicEndpoint(async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     const source = new MeiliSearch({
-      host: '/',
+      host: '',
       apiKey: '',
     });
     const target = new MeiliSearch({
-      host: '/',
+      host: '',
       apiKey: '',
     });
 
     const index = 'metrics_images_v1';
 
-    let from = 1735731876028;
+    let cursor = 0;
+    const batchSize = 50000;
+    const endCursor = cursor + batchSize;
 
-    while (true) {
-      const { hits } = await source.index(index).search('', {
-        offset: 0,
-        limit: 50000,
-        filter: `sortAtUnix > ${from}`,
-        sort: ['sortAt:asc'],
+    const tasks: Task[] = [];
+    while (cursor < endCursor) {
+      const start = cursor;
+      let end = start + batchSize;
+      if (end > endCursor) end = endCursor;
+      tasks.push(async () => {
+        const { hits } = await source.index(index).search('', {
+          offset: 0,
+          limit: batchSize + 1,
+          filter: `id >= ${start} AND id < ${end}`,
+          sort: ['id:asc'],
+        });
+
+        if (hits.length === 0) {
+          return;
+        }
+
+        try {
+          console.log('Updating documents :: ', hits.length, { start, end });
+
+          await updateDocs({
+            indexName: index,
+            documents: hits,
+            client: target,
+          });
+
+          await target.index(index).updateDocuments(hits);
+
+          console.log('Updated documents :: ', { start, end });
+
+          await sleep(1000);
+        } catch (e) {
+          console.error('Error updating documents :: ', e, { start, end });
+        }
       });
 
-      if (hits.length === 0) {
-        break;
-      }
-
-      const documents = hits.map((hit) => {
-        // lastId = Math.max(lastId, hit.id);
-        from = Math.max(from, hit.sortAtUnix);
-        return hit;
-      });
-
-      console.log('Updating documents :: ', documents.length, from);
-
-      await target.index(index).updateDocuments(documents);
-
-      await sleep(1000);
+      cursor = end;
     }
+
+    await limitConcurrency(tasks, 5);
+
     return res.json({ success: true });
   } catch (e) {
     console.error('Error :: ', e);
