4bb35bb41cceb5f29b0525631ba754043d7416b6
Luis Rojas <lrojas94@gmail.com>
2025-01-20 19:12:52 -0400

    Avoid prefetching collection status for images without session

--- src/pages/images/[imageId].tsx
+++ src/pages/images/[imageId].tsx
@@ -1,21 +1,26 @@
+import { NotFound } from '~/components/AppLayout/NotFound';
+import { Page } from '~/components/AppLayout/Page';
+import { useBrowserRouter } from '~/components/BrowserRouter/BrowserRouterProvider';
+import { ImageDetailProvider } from '~/components/Image/Detail/ImageDetailProvider';
+import { ImageDetail2 } from '~/components/Image/DetailV2/ImageDetail2';
+import { imagesQueryParamSchema } from '~/components/Image/image.utils';
 import { createServerSideProps } from '~/server/utils/server-side-helpers';
 import { isNumber } from '~/utils/type-guards';
-import { ImageDetailProvider } from '~/components/Image/Detail/ImageDetailProvider';
-import { imagesQueryParamSchema } from '~/components/Image/image.utils';
-import { useBrowserRouter } from '~/components/BrowserRouter/BrowserRouterProvider';
-import { NotFound } from '~/components/AppLayout/NotFound';
-import { ImageDetail2 } from '~/components/Image/DetailV2/ImageDetail2';
-import { Page } from '~/components/AppLayout/Page';
 
 export const getServerSideProps = createServerSideProps({
   useSSG: true,
-  resolver: async ({ ctx, ssg }) => {
+  useSession: true,
+  resolver: async ({ ctx, ssg, session }) => {
     const params = (ctx.params ?? {}) as { imageId: string };
     const id = Number(params.imageId);
     if (!isNumber(id)) return { notFound: true };
 
     await ssg?.image.get.prefetch({ id });
-    await ssg?.image.getContestCollectionDetails.prefetch({ id });
+
+    if (session) {
+      await ssg?.image.getContestCollectionDetails.prefetch({ id });
+    }
+
     await ssg?.hiddenPreferences.getHidden.prefetch();
   },
 });
