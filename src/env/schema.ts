a6ebf64d1146379d29c9a3c3529a21c2b9ad02d6
Briant Diehl <bkdiehl@gmail.com>
2025-01-29 15:30:36 -0700

    ORCHESTRATOR_EXPERIMENTAL env var default set to false

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -93,7 +93,7 @@ export const serverSchema = z.object({
   ORCHESTRATOR_ENDPOINT: isProd ? z.string().url() : z.string().url().optional(),
   ORCHESTRATOR_MODE: z.string().default('dev'),
   ORCHESTRATOR_ACCESS_TOKEN: z.string().default(''),
-  ORCHESTRATOR_EXPERIMENTAL: booleanString().default(true),
+  ORCHESTRATOR_EXPERIMENTAL: booleanString().default(false),
   AXIOM_TOKEN: z.string().optional(),
   AXIOM_ORG_ID: z.string().optional(),
   AXIOM_DATASTREAM: z.string().optional(),
