aff4a2fd85b5c8d58534912ab36097943b94fa66
manuelurenah <manuel.ureh@hotmail.com>
2025-03-12 15:20:46 -0400

    Deprecates logToDb method and replace with logToAxiom

--- src/pages/api/upload/abort.ts
+++ src/pages/api/upload/abort.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerAuthSession } from '~/server/utils/get-server-auth-session';
 import { abortMultipartUpload, getS3Client } from '~/utils/s3-utils';
-import { logToDb } from '~/utils/logging';
 import { UploadType } from '~/server/common/enums';
+import { logToAxiom } from '~/server/logging/client';
 
 const upload = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerAuthSession({ req, res });
@@ -15,7 +15,7 @@ const upload = async (req: NextApiRequest, res: NextApiResponse) => {
   const { bucket, key, type, uploadId } = req.body;
   const s3 = type === UploadType.Image ? getS3Client('image') : undefined;
   const result = await abortMultipartUpload(bucket, key, uploadId, s3);
-  await logToDb('s3-upload-abort', { userId, type, key, uploadId });
+  await logToAxiom({ name: 's3-upload-abort', userId, type, key, uploadId });
 
   res.status(200).json(result);
 };
