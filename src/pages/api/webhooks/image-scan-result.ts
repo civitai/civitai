33a80478151917fc58fa8d70cae27fdb029b2e87
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2025-03-17 17:52:59 -0400

    Adds moderation rules when scanning models and images (#1655)
    * Adds moderation rules when scanning models and images
    
    * Makes adjustments based on review
    
    * Simplifies applyModRules on image scan
    
    * Fixes issue with content regex rule
    
    * More adjustments after review
    
    * Cleanup
    
    * Displays rule reason in moderation queue
    
    * Adjusts search index update when applying mod rules to images

--- src/pages/api/webhooks/image-scan-result.ts
+++ src/pages/api/webhooks/image-scan-result.ts
@@ -5,19 +5,38 @@ import { env } from '~/env/server';
 import { tagsNeedingReview as minorTags, tagsToIgnore } from '~/libs/tags';
 import { clickhouse } from '~/server/clickhouse/client';
 import { constants } from '~/server/common/constants';
-import { NsfwLevel, SearchIndexUpdateQueueAction, SignalMessages } from '~/server/common/enums';
+import {
+  BlockedReason,
+  NotificationCategory,
+  NsfwLevel,
+  SearchIndexUpdateQueueAction,
+  SignalMessages,
+} from '~/server/common/enums';
 import { dbRead, dbWrite } from '~/server/db/client';
 import { logToAxiom } from '~/server/logging/client';
 import { tagIdsForImagesCache } from '~/server/redis/caches';
 import { scanJobsSchema } from '~/server/schema/image.schema';
-import { imagesMetricsSearchIndex, imagesSearchIndex } from '~/server/search-index';
+import { ImageMetadata, VideoMetadata } from '~/server/schema/media.schema';
+import {
+  getImagesModRules,
+  getTagNamesForImages,
+  queueImageSearchIndexUpdate,
+} from '~/server/services/image.service';
+import { createNotification } from '~/server/services/notification.service';
 import { updatePostNsfwLevel } from '~/server/services/post.service';
 import { getTagRules } from '~/server/services/system-cache';
 import { insertTagsOnImageNew } from '~/server/services/tagsOnImageNew.service';
 import { deleteUserProfilePictureCache } from '~/server/services/user.service';
 import { WebhookEndpoint } from '~/server/utils/endpoint-helpers';
+import { evaluateRules } from '~/server/utils/mod-rules';
 import { getComputedTags } from '~/server/utils/tag-rules';
-import { ImageIngestionStatus, TagSource, TagTarget, TagType } from '~/shared/utils/prisma/enums';
+import {
+  ImageIngestionStatus,
+  ModerationRuleAction,
+  TagSource,
+  TagTarget,
+  TagType,
+} from '~/shared/utils/prisma/enums';
 import {
   auditMetaData,
   getTagsFromPrompt,
@@ -29,7 +48,7 @@ import { signalClient } from '~/utils/signal-client';
 
 const REQUIRED_SCANS = 2;
 
-const tagCache: Record<string, { id: number; blocked?: true; ignored?: true }> = {};
+const localTagCache: Record<string, { id: number; blocked?: true; ignored?: true }> = {};
 
 enum Status {
   Success = 0,
@@ -297,7 +316,7 @@ async function handleSuccess({
   const tagsToFind: string[] = [];
   let hasBlockedTag = false;
   for (const tag of tags) {
-    const cachedTag = tagCache[tag.tag];
+    const cachedTag = localTagCache[tag.tag];
     if (!cachedTag) tagsToFind.push(tag.tag);
     else {
       tag.id = cachedTag.id;
@@ -314,13 +333,13 @@ async function handleSuccess({
 
     // Cache found tags and add ids to tags
     for (const tag of foundTags) {
-      tagCache[tag.name] = { id: tag.id };
-      if (tag.nsfwLevel === NsfwLevel.Blocked) tagCache[tag.name].blocked = true;
-      if (shouldIgnore(tag.name, source)) tagCache[tag.name].ignored = true;
+      localTagCache[tag.name] = { id: tag.id };
+      if (tag.nsfwLevel === NsfwLevel.Blocked) localTagCache[tag.name].blocked = true;
+      if (shouldIgnore(tag.name, source)) localTagCache[tag.name].ignored = true;
     }
 
     for (const tag of tags) {
-      const cachedTag = tagCache[tag.tag];
+      const cachedTag = localTagCache[tag.tag];
       if (!cachedTag) continue;
       tag.id = cachedTag.id;
       if (!cachedTag.ignored && cachedTag.blocked) hasBlockedTag = true;
@@ -345,7 +364,7 @@ async function handleSuccess({
       select: { id: true, name: true, nsfwLevel: true },
     });
     for (const tag of newFoundTags) {
-      tagCache[tag.name] = { id: tag.id };
+      localTagCache[tag.name] = { id: tag.id };
       const match = tags.find((x) => x.tag === tag.name);
       if (match) match.id = tag.id;
     }
@@ -509,6 +528,11 @@ async function handleSuccess({
       if (ingestion === 'Scanned') {
         // Clear cached image tags after completing scans
         await tagIdsForImagesCache.refresh(id);
+        const appliedRule = await applyModerationRules({
+          ...image,
+          prompt,
+          metadata: image.metadata as ImageMetadata | VideoMetadata,
+        });
 
         const imageMetadata = image.metadata as Prisma.JsonObject | undefined;
         const isProfilePicture = imageMetadata?.profilePicture === true;
@@ -520,18 +544,12 @@ async function handleSuccess({
         if (image.postId) await updatePostNsfwLevel(image.postId);
 
         // Update search index
-        await imagesSearchIndex.queueUpdate([
-          {
-            id,
-            action: SearchIndexUpdateQueueAction.Update,
-          },
-        ]);
-        await imagesMetricsSearchIndex.queueUpdate([
-          {
-            id,
-            action: SearchIndexUpdateQueueAction.Update,
-          },
-        ]);
+        const action =
+          appliedRule?.action === ModerationRuleAction.Block
+            ? SearchIndexUpdateQueueAction.Delete
+            : SearchIndexUpdateQueueAction.Update;
+
+        await queueImageSearchIndexUpdate({ ids: [id], action });
       }
     }
 
@@ -597,4 +615,72 @@ function processHiveTags(tags: IncomingTag[]) {
     results.push(tag);
   }
   return results;
+}
+
+type IngestedImage = {
+  id: number;
+  userId: number;
+  prompt: string | null;
+  metadata?: ImageMetadata | VideoMetadata | null;
+};
+
+async function applyModerationRules(image: IngestedImage) {
+  const imageModRules = await getImagesModRules();
+  const tags = await getTagNamesForImages([image.id]);
+
+  if (imageModRules.length) {
+    const appliedRule = evaluateRules(imageModRules, { ...image, tags: tags[image.id] });
+
+    if (appliedRule) {
+      const data =
+        appliedRule.action === ModerationRuleAction.Block
+          ? {
+              ingestion: ImageIngestionStatus.Blocked,
+              nsfwLevel: NsfwLevel.Blocked,
+              blockedFor: BlockedReason.Moderated,
+              needsReview: null,
+            }
+          : appliedRule.action === ModerationRuleAction.Hold
+          ? { needsReview: 'modRule' }
+          : undefined;
+      if (data)
+        await dbWrite.image.update({
+          where: { id: image.id },
+          data: {
+            ...data,
+            metadata: { ...image.metadata, ruleId: appliedRule.id, ruleReason: appliedRule.reason },
+          },
+        });
+
+      // Send notification to user if auto blocked
+      if (appliedRule.action === ModerationRuleAction.Block) {
+        await createNotification({
+          category: NotificationCategory.System,
+          key: `image-block:${image.id}`,
+          type: 'system-message',
+          userId: image.userId,
+          details: {
+            message: `One of your images has been blocked due to a moderation rule violation${
+              appliedRule.reason ? ` by the following reason: ${appliedRule.reason}` : ''
+            }. If you believe this is a mistake, you can appeal this decision.`,
+            url: `/images/${image.id}`,
+          },
+        }).catch((error) =>
+          logToAxiom({
+            name: 'image-scan-result',
+            type: 'error',
+            message: 'Could not create notification when blocking image',
+            data: {
+              imageId: image.id,
+              error: error.message,
+              cause: error.cause,
+              stack: error.stack,
+            },
+          })
+        );
+      }
+
+      return appliedRule;
+    }
+  }
 }
