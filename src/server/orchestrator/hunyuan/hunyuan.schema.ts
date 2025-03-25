e7dbbc6179b8d42c936782dd2187d7709e732a6e
Briant Diehl <bkdiehl@gmail.com>
2025-03-25 13:34:36 -0600

    update sizes

--- src/server/orchestrator/hunyuan/hunyuan.schema.ts
+++ src/server/orchestrator/hunyuan/hunyuan.schema.ts
@@ -7,19 +7,27 @@ import {
 } from '~/server/orchestrator/infrastructure/base.schema';
 import { numberEnum } from '~/utils/zod-helpers';
 
-export const hunyuanAspectRatios = ['16:9', '4:3', '1:1', '3:4', '9:16'] as const;
+export const hunyuanAspectRatios = ['2:1', '4:3', '1:1', '3:4', '1:2'] as const;
 export const hunyuanDuration = [3, 5] as const;
 
 const hunyuanAspectRatioMap: Record<
   (typeof hunyuanAspectRatios)[number],
   { width: number; height: number }
 > = {
-  '16:9': { width: 1280, height: 768 },
-  '4:3': { width: 1280, height: 960 },
-  '1:1': { width: 1280, height: 1280 },
-  '3:4': { width: 960, height: 1280 },
-  '9:16': { width: 768, height: 1280 },
+  '2:1': { width: 896, height: 448 },
+  '4:3': { width: 768, height: 576 },
+  '1:1': { width: 704, height: 704 },
+  '3:4': { width: 576, height: 768 },
+  '1:2': { width: 448, height: 896 },
 };
+
+// {
+//   '16:9': { width: 720, height: 768 },
+//   '4:3': { width: 720, height: 960 },
+//   '1:1': { width: 720, height: 720 },
+//   '3:4': { width: 960, height: 720 },
+//   '9:16': { width: 768, height: 720 },
+// };
 
 const baseHunyuanSchema = z.object({
   engine: z.literal('hunyuan'),
