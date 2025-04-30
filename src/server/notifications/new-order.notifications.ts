dbddbca60d1d9ea93ee4ea3de15c12e3b2d42535
manuelurenah <manuel.ureh@hotmail.com>
2025-04-30 18:02:47 -0400

    Adjusts notifications

--- src/server/notifications/new-order.notifications.ts
+++ src/server/notifications/new-order.notifications.ts
@@ -3,28 +3,28 @@ import { createNotificationProcessor } from '~/server/notifications/base.notific
 
 export const knightsNewOrderNotifications = createNotificationProcessor({
   'new-order-smite-received': {
-    displayName: 'Knights of New: you got a smite!',
+    displayName: 'Knights of New Order: you got a smite!',
     prepareMessage: () => ({
       message:
-        'Oh no! You got a smite because one of your ratings did not meet the guidelines. Jump back in to cleanse the smite as soon as possible!',
+        'Knights of New Order: Oh no! You got a smite because one of your ratings did not meet the guidelines. Jump back in to cleanse the smite as soon as possible!',
       url: '/games/knights-of-new-order',
     }),
     category: NotificationCategory.Other,
   },
   'new-order-smite-cleansed': {
-    displayName: 'Knights of New: your smite was cleansed!',
+    displayName: 'Knights of New Order: your smite was cleansed!',
     category: NotificationCategory.Other,
     prepareMessage: ({ details }) => ({
-      message: `One of your smites was cleansed with the following reason: ${details.cleansedReason}`,
+      message: `Knights of New Order: One of your smites was cleansed with the following reason: ${details.cleansedReason}`,
       url: '/games/knights-of-new-order',
     }),
   },
   'new-order-game-over': {
-    displayName: 'Knights of New: Game Over 💀',
+    displayName: 'Knights of New Order: Game Over 💀',
     category: NotificationCategory.Other,
     prepareMessage: () => ({
       message:
-        'You just received your last smite and lost all your health! You career will be reset.',
+        'Knights of New Order: You just received your last smite and lost all your health! You career will be reset.',
       url: '/games/knights-of-new-order',
     }),
   },
