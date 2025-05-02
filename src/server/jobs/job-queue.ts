09a171452d2e73e5fa38653f0c57e8ec2c8b3f32
Briant Diehl <bkdiehl@gmail.com>
2025-05-01 21:38:54 -0600

    fix type error

--- src/server/jobs/job-queue.ts
+++ src/server/jobs/job-queue.ts
@@ -75,7 +75,7 @@ const updateNsfwLevelJob = createJob('update-nsfw-levels', '*/1 * * * *', async 
     bountyEntryIds,
     modelVersionIds,
     modelIds,
-    // collectionIds: [],
+    collectionIds: [],
   });
 
   await dbWrite.jobQueue.deleteMany({
