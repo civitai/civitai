855a708595abdb44858763513ee93ad70813fd70
Luis Rojas <lrojas94@gmail.com>
2025-05-02 15:06:44 -0400

    Assume default browsing level to be X when not found

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,20 +1,16 @@
 // import { chunk } from 'lodash-es';
 // import { NextApiRequest, NextApiResponse } from 'next';
 // import { IMAGES_SEARCH_INDEX, METRICS_IMAGES_SEARCH_INDEX } from '~/server/common/constants';
-// import { dbWrite } from '~/server/db/client';
+// import { BlockedReason } from '~/server/common/enums';
+// import { dbRead, dbWrite } from '~/server/db/client';
 // import { metricsSearchClient, searchClient, updateDocs } from '~/server/meilisearch/client';
 // import { limitConcurrency, Task } from '~/server/utils/concurrency-helpers';
 // import { PublicEndpoint } from '~/server/utils/endpoint-helpers';
-// import ids from './test.json';
 
 // export default PublicEndpoint(async function (req: NextApiRequest, res: NextApiResponse) {
 //   try {
 //     // Now batch the update task:
-//     console.log('Updating POI images:', ids.length);
-//     // return res.status(200).json({
-//     //   status: 'ok',
-//     //   ids: [...new Set(ids)],
-//     // });
+//     const ids = [];
 
 //     const batches = chunk(ids, 10000);
 //     const updateTasks: Task[] = [];
@@ -22,21 +18,21 @@
 //       updateTasks.push(async () => {
 //         console.log('Updating POI images:', batch.length);
 //         console.log('search index', IMAGES_SEARCH_INDEX);
-//         await updateDocs({
-//           documents: batch.map((id) => ({
-//             id,
-//             poi: true,
-//           })),
-//           indexName: IMAGES_SEARCH_INDEX,
-//           client: searchClient,
-//           batchSize: 10000,
-//         });
+//         // await updateDocs({
+//         //   documents: batch.map((id) => ({
+//         //     id,
+//         //     minor: true,
+//         //   })),
+//         //   indexName: IMAGES_SEARCH_INDEX,
+//         //   client: searchClient,
+//         //   batchSize: 10000,
+//         // });
 
 //         console.log('metrics index', METRICS_IMAGES_SEARCH_INDEX);
 //         await updateDocs({
 //           documents: batch.map((id) => ({
 //             id,
-//             poi: true,
+//             blockedFor: BlockedReason.AiNotVerified,
 //           })),
 //           indexName: METRICS_IMAGES_SEARCH_INDEX,
 //           client: metricsSearchClient,
@@ -49,7 +45,8 @@
 //             id: { in: batch },
 //           },
 //           data: {
-//             poi: true,
+//             ingestion: 'Blocked',
+//             blockedFor: BlockedReason.AiNotVerified,
 //           },
 //         });
 //         console.log('Done');
