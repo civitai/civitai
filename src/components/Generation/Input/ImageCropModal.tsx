319a7bcebc28f3aa16c9c9771ef79896286676ef
Briant Diehl <bkdiehl@gmail.com>
2025-05-08 17:41:21 -0600

    image cropper readonly preview works

--- src/components/Generation/Input/ImageCropModal.tsx
+++ src/components/Generation/Input/ImageCropModal.tsx
@@ -1,8 +1,8 @@
 import { Modal, SegmentedControl } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { useDialogContext } from '~/components/Dialog/DialogProvider';
-import Cropper from 'react-easy-crop';
-import { Point, Area } from 'react-easy-crop/types';
+import Cropper, { getInitialCropFromCroppedAreaPercentages } from 'react-easy-crop';
+import { Point, Area, MediaSize } from 'react-easy-crop/types';
 
 type ImageProps = { url: string; width: number; height: number; label?: string };
 
@@ -26,44 +26,74 @@ export function ImageCropModal({ images }: { images: ImageProps[] }) {
 }
 
 type CroppedImageProps = ImageProps & {
-  x: number;
-  y: number;
   zoom: number;
   crop: Point;
+  croppedAreaPixels: Area | null;
+  croppedArea: Area | null;
 };
 
 export function ImageCropperTest({ images }: { images: ImageProps[] }) {
   const [selected, setSelected] = useState('0');
-  const [state, setState] = useState(
-    images.map((image) => ({ ...image, x: 0, y: 0, zoom: 1, crop: { x: 0, y: 0 } }))
+  const [state, setState] = useState<CroppedImageProps[]>(
+    images.map((image) => ({
+      ...image,
+      x: 0,
+      y: 0,
+      zoom: 1,
+      crop: { x: 0, y: 0 },
+      croppedArea: null,
+      croppedAreaPixels: null,
+    }))
   );
   const selectedIndex = Number(selected);
   const image = state[selectedIndex];
   const aspect = state[0].width / state[0].height;
 
-  function handleCropComplete(croppedAreaPixels: Area, crop: Point, zoom: number) {
+  function handleCropComplete(
+    croppedArea: Area,
+    croppedAreaPixels: Area,
+    crop: Point,
+    zoom: number
+  ) {
     setState((state) => {
-      state[selectedIndex] = { ...state[selectedIndex], ...croppedAreaPixels, crop, zoom };
+      state[selectedIndex] = {
+        ...state[selectedIndex],
+        croppedArea,
+        croppedAreaPixels,
+        crop,
+        zoom,
+      };
       return [...state];
     });
   }
 
   return (
-    <div className="flex flex-1 flex-col gap-3 pb-12">
-      <SegmentedControl
-        onChange={setSelected}
-        value={selected}
-        data={images.map(({ label }, index) => ({
-          label: label ?? `Image ${index + 1}`,
-          value: `${index}`,
-        }))}
-      />
-      <ImageCropper
-        key={selectedIndex}
-        {...image}
-        aspect={aspect}
-        onCropComplete={handleCropComplete}
-      />
+    <div className="flex gap-3">
+      <div className="flex flex-1 flex-col gap-3">
+        <SegmentedControl
+          onChange={setSelected}
+          value={selected}
+          data={images.map(({ label }, index) => ({
+            label: label ?? `Image ${index + 1}`,
+            value: `${index}`,
+          }))}
+        />
+        <div className="relative aspect-square">
+          <ImageCropper
+            key={selectedIndex}
+            {...image}
+            aspect={aspect}
+            onCropComplete={handleCropComplete}
+          />
+        </div>
+      </div>
+      <div className="flex w-32 flex-col gap-3">
+        {state.map((image, index) => (
+          <div key={index} className="relative aspect-square">
+            <ImageCropper {...image} aspect={aspect} readonly />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
@@ -74,41 +104,70 @@ function ImageCropper({
   height,
   label,
   aspect,
-  x,
-  y,
+  croppedArea: initialCroppedArea,
+  croppedAreaPixels: initialCroppedAreaPixels,
   zoom: initialZoom,
   crop: initialCrop,
   onCropComplete,
+  readonly,
+  minZoom = 1,
+  maxZoom = 3,
 }: CroppedImageProps & {
   aspect: number;
-  onCropComplete?: (croppedAreaPixels: Area, crop: Point, zoom: number) => void;
+  onCropComplete?: (croppedArea: Area, croppedAreaPixels: Area, crop: Point, zoom: number) => void;
+  readonly?: boolean;
+  minZoom?: number;
+  maxZoom?: number;
 }) {
   const [crop, setCrop] = useState<Point>(initialCrop);
   const [zoom, setZoom] = useState(initialZoom);
+  const [cropSize, setCropSize] = useState({ width: 0, height: 0 });
+  const [mediaSize, setMediaSize] = useState<MediaSize | null>();
 
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(initialCroppedAreaPixels);
+  const [croppedArea, setCroppedArea] = useState<Area | null>(initialCroppedArea);
 
   function handleCropComplete(croppedArea: Area, croppedAreaPixels: Area) {
+    setCroppedArea(croppedArea);
     setCroppedAreaPixels(croppedAreaPixels);
   }
 
   useEffect(() => {
-    if (croppedAreaPixels) {
-      onCropComplete?.(croppedAreaPixels, crop, zoom);
+    if (croppedArea && croppedAreaPixels && !readonly) {
+      onCropComplete?.(croppedArea, croppedAreaPixels, crop, zoom);
     }
-  }, [croppedAreaPixels]);
+  }, [croppedArea, croppedAreaPixels, readonly]);
+
+  useEffect(() => {
+    if (readonly && initialCroppedArea && mediaSize) {
+      const { crop, zoom } = getInitialCropFromCroppedAreaPercentages(
+        initialCroppedArea,
+        mediaSize,
+        0,
+        cropSize,
+        minZoom,
+        maxZoom
+      );
+      setCrop(crop);
+      setZoom(zoom);
+    }
+  }, [initialCroppedArea, readonly, mediaSize]);
 
   return (
-    <div className="relative aspect-square">
-      <Cropper
-        image={url}
-        crop={crop}
-        zoom={zoom}
-        aspect={aspect}
-        onCropChange={setCrop}
-        onCropComplete={handleCropComplete}
-        onZoomChange={setZoom}
-      />
-    </div>
+    <Cropper
+      image={url}
+      crop={crop}
+      zoom={zoom}
+      aspect={aspect}
+      onCropChange={!readonly ? setCrop : () => undefined}
+      onZoomChange={!readonly ? setZoom : undefined}
+      onCropComplete={!readonly ? handleCropComplete : undefined}
+      onMediaLoaded={setMediaSize}
+      setCropSize={setCropSize}
+      minZoom={minZoom}
+      maxZoom={maxZoom}
+      // initialCroppedAreaPercentages={initialCroppedArea ?? undefined}
+      // initialCroppedAreaPixels={initialCroppedAreaPixels ?? undefined}
+    />
   );
 }
