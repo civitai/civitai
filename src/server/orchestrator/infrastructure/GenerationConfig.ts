39bd47fad0283fd811d2d4ca5549e0e5c54f4167
Briant Diehl <bkdiehl@gmail.com>
2025-05-12 17:08:10 -0600

    remove old code

--- src/server/orchestrator/infrastructure/GenerationConfig.ts
+++ src/server/orchestrator/infrastructure/GenerationConfig.ts
@@ -1,43 +1,5 @@
 import { VideoGenInput } from '@civitai/client';
 import { RefinementCtx, z } from 'zod';
-import {
-  GenerationType,
-  OrchestratorEngine,
-} from '~/server/orchestrator/infrastructure/base.enums';
-
-// interface IVideoGenerationConfig<TSchema extends z.AnyZodObject> {
-//   subType: GenerationType;
-//   engine: OrchestratorEngine;
-//   metadataDisplayProps: Array<keyof z.output<TSchema>>;
-//   schema: TSchema;
-// }
-
-// export class VideoGenerationConfig<TSchema extends z.AnyZodObject = z.AnyZodObject>
-//   implements IVideoGenerationConfig<TSchema>
-// {
-//   constructor(args: IVideoGenerationConfig<TSchema>) {
-//     this.subType = args.subType;
-//     this.engine = args.engine;
-//     this.metadataDisplayProps = args.metadataDisplayProps;
-//     this.schema = args.schema;
-//     this.key = `${args.engine}-${args.subType}`;
-//   }
-
-//   type = 'video';
-//   subType: GenerationType;
-//   engine: OrchestratorEngine;
-//   metadataDisplayProps: Array<keyof z.output<TSchema>>;
-//   schema: TSchema;
-//   key: string;
-// }
-
-// export class ImageGenerationConfig<TSchema extends z.AnyZodObject = z.AnyZodObject> {
-//   constructor() {}
-//   type = 'image';
-//   subType: GenerationType;
-//   schema: TSchema;
-//   key: string;
-// }
 
 type VideoGenProcesses = 'txt2vid' | 'img2vid';
 type WithSubType = { process: VideoGenProcesses };
