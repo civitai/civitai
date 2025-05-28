fadd96fe8803fa668164aa5e86049b67d7c8fc4c
Luis Rojas <lrojas94@gmail.com>
2025-05-28 18:25:25 -0400

    Protect coinbase purchase endpoint

--- src/server/routers/coinbase.router.ts
+++ src/server/routers/coinbase.router.ts
@@ -1,10 +1,11 @@
 import { getStatus, createBuzzOrderHandler } from '~/server/controllers/coinbase.controller';
 import { createBuzzChargeSchema } from '~/server/schema/coinbase.schema';
-import { protectedProcedure, router } from '~/server/trpc';
+import { isFlagProtected, protectedProcedure, router } from '~/server/trpc';
 
 export const coinbaseRouter = router({
   getStatus: protectedProcedure.query(getStatus),
   createBuzzOrder: protectedProcedure
     .input(createBuzzChargeSchema)
+    .use(isFlagProtected('coinbasePayments'))
     .mutation(createBuzzOrderHandler),
 });
