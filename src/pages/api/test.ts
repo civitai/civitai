e0c448a95c081b8ba50547440b3a024a512894bf
Luis Rojas <lrojas94@gmail.com>
2025-05-28 18:09:19 -0400

    Remove test endpoint

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,30 +1,30 @@
-import Decimal from 'decimal.js';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { MODELS_SEARCH_INDEX } from '~/server/common/constants';
-import { SearchIndexUpdateQueueAction } from '~/server/common/enums';
-import coinbaseCaller from '~/server/http/coinbase/coinbase.caller';
-import nowpaymentsCaller from '~/server/http/nowpayments/nowpayments.caller';
-import { searchClient } from '~/server/meilisearch/client';
-import { modelsSearchIndex } from '~/server/search-index';
-import { PublicEndpoint } from '~/server/utils/endpoint-helpers';
-import { sleep } from '~/utils/errorHandling';
+// import Decimal from 'decimal.js';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { MODELS_SEARCH_INDEX } from '~/server/common/constants';
+// import { SearchIndexUpdateQueueAction } from '~/server/common/enums';
+// import coinbaseCaller from '~/server/http/coinbase/coinbase.caller';
+// import nowpaymentsCaller from '~/server/http/nowpayments/nowpayments.caller';
+// import { searchClient } from '~/server/meilisearch/client';
+// import { modelsSearchIndex } from '~/server/search-index';
+// import { PublicEndpoint } from '~/server/utils/endpoint-helpers';
+// import { sleep } from '~/utils/errorHandling';
 
-export default PublicEndpoint(async function (req: NextApiRequest, res: NextApiResponse) {
-  const test = await coinbaseCaller.createCharge({
-    name: 'Test Charge',
-    description: 'This is a test charge',
-    pricing_type: 'fixed_price',
-    local_price: {
-      amount: new Decimal(10.0).toString(),
-      currency: 'USD',
-    },
-    metadata: {
-      areU: 'kidding',
-      internalOrderId: 'test-order-123',
-      userId: 1,
-      buzzAmount: 100,
-    },
-  });
+// export default PublicEndpoint(async function (req: NextApiRequest, res: NextApiResponse) {
+//   const test = await coinbaseCaller.createCharge({
+//     name: 'Test Charge',
+//     description: 'This is a test charge',
+//     pricing_type: 'fixed_price',
+//     local_price: {
+//       amount: new Decimal(10.0).toString(),
+//       currency: 'USD',
+//     },
+//     metadata: {
+//       areU: 'kidding',
+//       internalOrderId: 'test-order-123',
+//       userId: 1,
+//       buzzAmount: 100,
+//     },
+//   });
 
-  res.status(200).json(test);
-});
+//   res.status(200).json(test);
+// });
