e33f5ceceff6f7c978882df51474131a37eb8287
Brett Woodward <flipperbw@gmail.com>
2025-06-03 11:32:13 -0400

    re-enable model api query with meili

--- src/pages/api/v1/model-versions/by-hash/index.ts
+++ src/pages/api/v1/model-versions/by-hash/index.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
-
 import { isProd } from '~/env/other';
 import { prepareModelVersionResponse } from '~/pages/api/v1/model-versions/[id]';
 import { dbRead } from '~/server/db/client';
