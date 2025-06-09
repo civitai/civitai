8dd9ec7a2ad86928f59f9d31aba9bacfadb5689b
manuelurenah <manuel.ureh@hotmail.com>
2025-06-09 14:15:56 -0400

    Fixes notifications and navigation progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -76,6 +76,8 @@ import '~/styles/globals.css';
 import '@mantine/core/styles.layer.css';
 import '@mantine/dates/styles.layer.css';
 import '@mantine/dropzone/styles.layer.css';
+import '@mantine/notifications/styles.layer.css';
+import '@mantine/nprogress/styles.layer.css';
 import '@mantine/tiptap/styles.layer.css';
 
 dayjs.extend(duration);
