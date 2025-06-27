5aefe69628f0c5676c2650bac269fb0ef6f0d69b
Brett Woodward <flipperbw@gmail.com>
2025-06-27 11:39:16 -0400

    add job for refreshing auction cache

---
+++ src/server/jobs/refresh-auction-cache.ts
@@ -0,0 +1,27 @@
+import { SearchIndexUpdateQueueAction } from '~/server/common/enums';
+import { dbWrite } from '~/server/db/client';
+import { createJob } from '~/server/jobs/job';
+import { modelVersionResourceCache } from '~/server/redis/caches';
+import { modelsSearchIndex } from '~/server/search-index';
+import { homeBlockCacheBust } from '~/server/services/home-block-cache.service';
+import { resourceDataCache } from '~/server/services/model-version.service';
+import { bustFeaturedModelsCache } from '~/server/services/model.service';
+import { bustOrchestratorModelCache } from '~/server/services/orchestrator/models';
+import { HomeBlockType } from '~/shared/utils/prisma/enums';
+
+export const refreshAuctionCache = createJob('refresh-auction-cache', '6 0 * * *', async () => {
+  const data = await dbWrite.coveredCheckpoint.findMany();
+
+  if (!data.length) return;
+
+  const versionIds = data.map((c) => c.version_id);
+
+  await modelsSearchIndex.updateSync(
+    data.map((c) => ({ id: c.model_id, action: SearchIndexUpdateQueueAction.Update }))
+  );
+  await bustFeaturedModelsCache();
+  await homeBlockCacheBust(HomeBlockType.FeaturedModelVersion, 'default');
+  await resourceDataCache.bust(versionIds);
+  await modelVersionResourceCache.bust(versionIds);
+  await bustOrchestratorModelCache(versionIds);
+});
