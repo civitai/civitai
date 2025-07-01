929a290d4bb4fbd105a2a91e9990a8c6b9df9fb4
Brett Woodward <flipperbw@gmail.com>
2025-07-01 15:19:53 -0400

    change followed-by to be event-based notification

--- src/server/notifications/follow.notifications.ts
+++ src/server/notifications/follow.notifications.ts
@@ -1,28 +1,21 @@
 import { NotificationCategory } from '~/server/common/enums';
 import { createNotificationProcessor } from '~/server/notifications/base.notifications';
 
+export type NotifDetailsFollowedBy = {
+  username: string | null;
+  // userId: number;
+};
+
 export const followNotifications = createNotificationProcessor({
-  // Moveable
   'followed-by': {
-    displayName: 'New followers',
+    displayName: 'New follower',
     category: NotificationCategory.Update,
-    prepareMessage: ({ details }) => ({
-      message: `${details.username} has followed you!`,
-      url: `/user/${details.username}`,
-    }),
-    prepareQuery: ({ lastSent }) => `
-      SELECT
-        CONCAT('followed-by:',ue."userId",':',ue."targetUserId") "key",
-        ue."targetUserId" as "userId",
-        'followed-by' as "type",
-        jsonb_build_object(
-          'userId', u.id,
-          'username', u.username
-        ) "details"
-      FROM "UserEngagement" ue
-      JOIN "User" u ON u.id = ue."userId"
-      WHERE ue.type = 'Follow' AND ue."createdAt" > '${lastSent}'
-      AND NOT EXISTS (SELECT 1 FROM "UserNotificationSettings" WHERE "userId" = ue."targetUserId" AND type = 'followed-by')
-    `,
+    prepareMessage: ({ details }) => {
+      const det = details as NotifDetailsFollowedBy;
+      return {
+        message: `${det.username ?? 'A user'} has followed you!`,
+        url: det.username ? `/user/${det.username}` : undefined,
+      };
+    },
   },
 });
