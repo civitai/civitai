fcbf96817bbfed8345373899a1f00af0d227ce79
Brett Woodward <flipperbw@gmail.com>
2025-07-02 13:46:12 -0400

    baseModelType non-nullable, fix tag service

--- src/components/Ads/AdsProvider.tsx
+++ src/components/Ads/AdsProvider.tsx
@@ -1,14 +1,14 @@
-import React, { createContext, useContext, useEffect } from 'react';
-import { useCurrentUser } from '~/hooks/useCurrentUser';
-import Script from 'next/script';
-import { useFeatureFlags } from '~/providers/FeatureFlagsProvider';
-import { useBrowsingSettings } from '~/providers/BrowserSettingsProvider';
-import { useSignalContext } from '~/components/Signals/SignalsProvider';
-import { useDeviceFingerprint } from '~/providers/ActivityReportingProvider';
-import { adUnitsLoaded } from '~/components/Ads/ads.utils';
-import { create } from 'zustand';
-import { isDev } from '~/env/other';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
+import React, { createContext, useContext, useEffect } from 'react';
+import { create } from 'zustand';
+import { adUnitsLoaded } from '~/components/Ads/ads.utils';
+import { useSignalContext } from '~/components/Signals/SignalsProvider';
+import { isDev } from '~/env/other';
+import { useCurrentUser } from '~/hooks/useCurrentUser';
+import { useDeviceFingerprint } from '~/providers/ActivityReportingProvider';
+import { useBrowsingSettings } from '~/providers/BrowserSettingsProvider';
+import { useFeatureFlags } from '~/providers/FeatureFlagsProvider';
 
 declare global {
   interface Window {
