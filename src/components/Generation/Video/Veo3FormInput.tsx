aa20a79f04da79fe91f82409d875a88773d4ee94
Briant Diehl <bkdiehl@gmail.com>
2025-07-11 15:48:22 -0600

    added veo3 images input

--- src/components/Generation/Video/Veo3FormInput.tsx
+++ src/components/Generation/Video/Veo3FormInput.tsx
@@ -8,14 +8,21 @@ import InputSeed from '~/components/ImageGeneration/GenerationForm/InputSeed';
 import { InputSegmentedControl, InputSwitch, InputTextArea } from '~/libs/form';
 import {
   getVeo3Checkpoint,
+  getVeo3IsFastMode,
   removeVeo3CheckpointFromResources,
   veo3AspectRatios,
   veo3Duration,
   veo3ModelOptions,
   veo3Models,
+  veo3StandardId,
 } from '~/server/orchestrator/veo3/veo3.schema';
 import clsx from 'clsx';
 import type { ResourceInput } from '~/server/orchestrator/infrastructure/base.schema';
+import {
+  InputSourceImageUploadMultiple,
+  SourceImageUploadMultiple,
+} from '~/components/Generation/Input/SourceImageUploadMultiple';
+import { useEffect } from 'react';
 
 export function Veo3FormInput() {
   const form = useFormContext();
@@ -23,24 +30,50 @@ export function Veo3FormInput() {
   const isTxt2Vid = process === 'txt2vid';
   const resources = form.watch('resources') as ResourceInput[] | null;
   const checkpoint = getVeo3Checkpoint(resources);
+  const isFastMode = getVeo3IsFastMode(checkpoint.id);
+
+  useEffect(() => {
+    if (isFastMode) form.setValue('process', 'txt2vid');
+  }, [isFastMode]);
+
+  useEffect(() => {
+    if (process === 'img2vid' && isFastMode) setCheckpoint(veo3StandardId);
+  }, [process]);
+
+  function setCheckpoint(modelVersionId: number) {
+    const resourcesWithoutModel = removeVeo3CheckpointFromResources(resources);
+    const checkpoint = veo3Models.find((x) => x.id === modelVersionId);
+    if (checkpoint) form.setValue('resources', [checkpoint, ...resourcesWithoutModel]);
+  }
 
   return (
     <>
       {/* <InputVideoProcess name="process" />
       {process === 'img2vid' && (
-        <InputSourceImageUpload name="sourceImage" warnOnMissingAiMetadata />
+        <div className="-mx-2">
+          <InputSourceImageUploadMultiple
+            name="images"
+            max={1}
+            warnOnMissingAiMetadata
+            aspect="video"
+          >
+            {(previewItems) => (
+              <div className="mx-auto max-w-80">
+                {previewItems.map((item, i) => (
+                  <SourceImageUploadMultiple.Image key={i} index={i} {...item} />
+                ))}
+                <SourceImageUploadMultiple.Dropzone />
+              </div>
+            )}
+          </InputSourceImageUploadMultiple>
+        </div>
       )} */}
       <div className="flex flex-col gap-2">
         <Input.Label>Model</Input.Label>
         <SegmentedControl
           data={veo3ModelOptions}
           value={checkpoint.id.toString()}
-          onChange={(value) => {
-            const modelId = Number(value);
-            const resourcesWithoutModel = removeVeo3CheckpointFromResources(resources);
-            const model = veo3Models.find((x) => x.id === modelId);
-            if (model) form.setValue('resources', [model, ...resourcesWithoutModel]);
-          }}
+          onChange={(value) => setCheckpoint(Number(value))}
         ></SegmentedControl>
       </div>
       <InputTextArea
