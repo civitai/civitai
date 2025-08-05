53f814b5200885cca993a5b794e293d9d943322a
manuelurenah <manuel.ureh@hotmail.com>
2025-08-05 12:27:11 -0400

    Adds unique index

--- prisma/migrations/20250801214540_restricted_images_by_base_model_view/migration.sql
+++ prisma/migrations/20250801214540_restricted_images_by_base_model_view/migration.sql
@@ -5,5 +5,5 @@ JOIN "ModelVersion" mv ON mv.id = irn."modelVersionId"
 WHERE mv."baseModel" IN ('SDXL Turbo', 'SVD', 'Stable Cascade', 'SD 3', 'SD 3.5', 'SD 3.5 Medium', 'SD 3.5 Large', 'SD 3.5 Large Turbo')
 WITH DATA;
 
-CREATE INDEX CONCURRENTLY idx_restricted_images_by_base_model_imageid 
+CREATE UNIQUE INDEX CONCURRENTLY idx_restricted_images_by_base_model_imageid 
 ON "RestrictedImagesByBaseModel" ("imageId");
--- src/server/services/image.service.ts
+++ src/server/services/image.service.ts
@@ -5044,6 +5044,7 @@ export async function createImageResources({
       )
     );
 
+    console.log('INSERTING IMAGE RESOURCES');
     await dbClient.$queryRaw`
       INSERT INTO "ImageResourceNew" ("imageId", "modelVersionId", strength, detected)
       VALUES ${values}
