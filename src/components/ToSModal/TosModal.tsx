d72b52914db36b13cf0903f3500922d531b391a0
manuelurenah <manuel.ureh@hotmail.com>
2025-09-12 12:51:19 -0400

    Fixes TosModal max-height

--- src/components/ToSModal/TosModal.tsx
+++ src/components/ToSModal/TosModal.tsx
@@ -12,6 +12,7 @@ import {
   Title,
 } from '@mantine/core';
 import { useState } from 'react';
+import { useElementSize } from '@mantine/hooks';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
@@ -40,6 +41,9 @@ export default function TosModal({
   });
   const queryUtils = trpc.useUtils();
 
+  const { ref: headerRef, height: headerHeight } = useElementSize();
+  const { ref: footerRef, height: footerHeight } = useElementSize();
+
   const updateUserSettings = trpc.user.setSettings.useMutation({
     async onSuccess(res) {
       queryUtils.user.getSettings.setData(undefined, (old) => ({ ...old, ...res }));
@@ -51,6 +55,15 @@ export default function TosModal({
       });
     },
   });
+
+  const getScrollAreaMaxHeight = () => {
+    const elementGaps = 16 * 3;
+    const dividerHeight = 2;
+    const reservedSpace = headerHeight + footerHeight + elementGaps + dividerHeight + 32;
+    console.log('Reserved space:', reservedSpace);
+
+    return Math.max(210, reservedSpace);
+  };
 
   const handleConfirm = async () => {
     if (!acceptedCoC) return;
@@ -74,18 +87,18 @@ export default function TosModal({
         <Stack gap="md">
           {data?.title && (
             <>
-              <Stack gap={0}>
+              <Stack ref={headerRef} gap={0}>
                 <Title order={2}>{data?.title}</Title>
                 {data.lastmod ? (
                   <Text size="sm" c="dimmed">
-                    Last modified: {formatDate(data.lastmod, undefined, true)}
+                    Last modifie: {formatDate(data.lastmod, undefined, true)}
                   </Text>
                 ) : null}
               </Stack>
               <Divider mx="-lg" />
             </>
           )}
-          <ScrollArea.Autosize mah={500}>
+          <ScrollArea.Autosize mah={`calc(90dvh - ${getScrollAreaMaxHeight()}px`}>
             <Stack>
               <CustomMarkdown
                 // allowedElements={['p', 'a', 'strong', 'h1', 'h2', 'ul', 'ol', 'li']}
@@ -98,24 +111,26 @@ export default function TosModal({
               </CustomMarkdown>
             </Stack>
           </ScrollArea.Autosize>
-          <Checkbox
-            checked={acceptedCoC}
-            onChange={(event) => setAcceptedCoC(event.currentTarget.checked)}
-            label="I have read and agree to the Terms of Service"
-            size="sm"
-          />
-          <Group ml="auto">
-            <Button onClick={handleClose} color="gray" disabled={updateUserSettings.isLoading}>
-              Go back
-            </Button>
-            <Button
-              onClick={handleConfirm}
-              disabled={!acceptedCoC}
-              loading={updateUserSettings.isLoading || loading}
-            >
-              Accept
-            </Button>
-          </Group>
+          <Stack ref={footerRef} gap="md">
+            <Checkbox
+              checked={acceptedCoC}
+              onChange={(event) => setAcceptedCoC(event.currentTarget.checked)}
+              label="I have read and agree to the Terms of Service"
+              size="sm"
+            />
+            <Group ml="auto">
+              <Button onClick={handleClose} color="gray" disabled={updateUserSettings.isLoading}>
+                Go back
+              </Button>
+              <Button
+                onClick={handleConfirm}
+                disabled={!acceptedCoC}
+                loading={updateUserSettings.isLoading || loading}
+              >
+                Accept
+              </Button>
+            </Group>
+          </Stack>
         </Stack>
       )}
     </Modal>
