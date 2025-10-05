d4a1bc628284f9f7fd846a44726973c5049b0489
Luis Rojas <lrojas94@gmail.com>
2025-10-04 20:07:31 -0400

    Invalidate civitai session user when refreshing the session

--- src/server/services/user.service.ts
+++ src/server/services/user.service.ts
@@ -111,6 +111,7 @@ import type {
   UserSettingsSchema,
   UserTier,
 } from './../schema/user.schema';
+import { invalidateCivitaiUser } from '~/server/services/orchestrator/civitai';
 // import { createFeaturebaseToken } from '~/server/featurebase/featurebase';
 
 export const getUserCreator = async ({
@@ -880,7 +881,9 @@ export const getSessionUser = async ({ userId, token }: { userId?: number; token
         : undefined,
     // feedbackToken,
   };
+
   await redis.packed.set(cacheKey, sessionUser, { EX: CacheTTL.hour * 4 });
+  await invalidateCivitaiUser({ userId });
 
   return sessionUser;
 };
