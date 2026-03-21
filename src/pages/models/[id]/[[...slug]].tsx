7264408ac5f7c7563ac87bc86e876968de54b164
Briant Diehl <bkdiehl@gmail.com>
2026-03-20 19:12:50 -0600

    fix model page with chinese/japanese characters not loading

--- src/pages/models/[id]/[[...slug]].tsx
+++ src/pages/models/[id]/[[...slug]].tsx
@@ -240,7 +240,7 @@ export const getServerSideProps = createServerSideProps({
       if (model) {
         const correctSlug = slugit(model.name);
         const currentSlug = params.slug?.join('/');
-        if (currentSlug !== correctSlug) {
+        if (correctSlug && currentSlug !== correctSlug) {
           const queryString = modelVersionId ? `?modelVersionId=${modelVersionId}` : '';
           return {
             redirect: {
