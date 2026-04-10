c51d875d16864faf7ebe732debfceb37909f8c83
Briant Diehl <bkdiehl@gmail.com>
2026-04-10 13:19:41 -0600

    fix migration

--- prisma/migrations/20260410122239_add_account_type_to_bid/migration.sql
+++ prisma/migrations/20260410122239_add_account_type_to_bid/migration.sql
@@ -1,12 +1,10 @@
 -- AlterTable: Add accountType to Bid
 ALTER TABLE "Bid" ADD COLUMN "accountType" TEXT NOT NULL DEFAULT 'yellow';
 
--- Update unique constraint on Bid to include accountType
-ALTER TABLE "Bid" DROP CONSTRAINT "Bid_auctionId_userId_entityId_key";
-ALTER TABLE "Bid" ADD CONSTRAINT "Bid_auctionId_userId_entityId_accountType_key"
-  UNIQUE ("auctionId", "userId", "entityId", "accountType");
+-- Update unique index on Bid to include accountType
+DROP INDEX "Bid_auctionId_userId_entityId_key";
+CREATE UNIQUE INDEX "Bid_auctionId_userId_entityId_accountType_key" ON "Bid"("auctionId", "userId", "entityId", "accountType");
 
--- Update unique constraint on BidRecurring to include accountType
-ALTER TABLE "BidRecurring" DROP CONSTRAINT "BidRecurring_auctionBaseId_userId_entityId_key";
-ALTER TABLE "BidRecurring" ADD CONSTRAINT "BidRecurring_auctionBaseId_userId_entityId_accountType_key"
-  UNIQUE ("auctionBaseId", "userId", "entityId", "accountType");
+-- Update unique index on BidRecurring to include accountType
+DROP INDEX "BidRecurring_auctionBaseId_userId_entityId_key";
+CREATE UNIQUE INDEX "BidRecurring_auctionBaseId_userId_entityId_accountType_key" ON "BidRecurring"("auctionBaseId", "userId", "entityId", "accountType");
