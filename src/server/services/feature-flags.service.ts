8ed526be0fb4854458c866f98e582227ab4bf49f
manuelurenah <manuel.ureh@hotmail.com>
2026-06-17 11:40:53 -0400

    fix(feature-flags): update strikes availability from 'dev' to 'mod'

--- src/server/services/feature-flags.service.ts
+++ src/server/services/feature-flags.service.ts
@@ -204,7 +204,7 @@ const featureFlags = createFeatureFlags({
   comicCreator: { availability: ['mod'], fliptKey: 'comic-creator' },
   licensingFee: { availability: ['user'], fliptKey: 'licensing-fee' },
   liveMetrics: { availability: ['mod'], fliptKey: 'live-metrics' },
-  strikes: ['dev', 'granted'],
+  strikes: ['mod', 'granted'],
   prepaidBuzzTransactions: { availability: ['mod'], fliptKey: 'prepaid-buzz-transactions' },
   userPaymentConfiguration: {
     availability: ['granted'],
